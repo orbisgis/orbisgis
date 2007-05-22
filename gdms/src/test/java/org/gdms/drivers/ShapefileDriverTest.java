599caed0fae9bd862608bd4a1f001c371953e984
thomas <thomas@irstv>
2007-05-22 10:32:20 +0000

    urbsat : first import

--- gdms/src/test/java/org/gdms/drivers/ShapefileDriverTest.java
+++ gdms/src/test/java/org/gdms/drivers/ShapefileDriverTest.java
@@ -21,6 +21,7 @@ import com.hardcode.driverManager.DriverLoadException;
 
 public class ShapefileDriverTest extends TestCase {
 	private DataSourceFactory dsf = new DataSourceFactory();
+
 	// SEE THE GT BUG REPORT :
 	// http://jira.codehaus.org/browse/GEOT-1268
 
