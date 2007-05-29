fb5d9cd0a036ca01c1b39df18f8059b4de824ded
sebastien <sebastien@irstv>
2007-05-29 09:12:55 +0000

    Fonctions test pour l'implementation des fonctions Enveloppe et Proportion, donnant le rapport surface batiment sur surface totale, la réutilisation de la fonction enveloppe dans la fonction proportion Doit absolument etre faite dans la version future.

---
+++ urbsat/src/test/java/org/urbsat/function/Eclatement.java
@@ -0,0 +1,81 @@
+package org.urbsat.function;
+
+import java.util.ArrayList;
+
+import org.gdms.data.values.Value;
+import org.gdms.data.values.ValueFactory;
+import org.gdms.sql.function.Function;
+import org.gdms.sql.function.FunctionException;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
+
+public class Eclatement implements Function{
+private Value result = null;
+	
+	private int constante = 12;
+	
+	public Function cloneFunction() {
+		
+		return new Eclatement();
+	}
+
+	public Value evaluate(Value[] args) throws FunctionException {
+		String ts = args[0].toString();
+		Geometry geom = null;
+		try {
+			geom = new WKTReader().read(ts);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		System.out.println(geom);
+		ArrayList<LineString> ls = new ArrayList<LineString>();
+		Polygon polybuilding = (Polygon) geom;
+
+		LineString lsBuilding = (LineString) polybuilding
+				.getBoundary();
+		// System.out.println(lsBuilding);
+		Coordinate[] tab = lsBuilding.getCoordinates();
+		// faire les linestring
+		GeometryFactory geomf = new GeometryFactory();
+		int i = 0;
+		boolean boucle = true;
+		while (boucle) {
+			Coordinate[] tab2 = new Coordinate[2];
+			tab2[0] = tab[i];
+			tab2[1] = tab[i + 1];
+			LineString hey3 = geomf.createLineString(tab2);
+			ls.add(hey3);
+			i++;
+			try {
+				tab[i + 1] = tab[i + 1];
+			} catch (java.lang.ArrayIndexOutOfBoundsException e) {
+				boucle = false;
+			}
+		}
+	
+		return ValueFactory.createValue(ls.get(0));
+	}
+	
+	public String getName() {
+		
+		return "Eclatement";
+	}
+
+	public int getType(int[] types) {
+		return types[0];
+	}
+
+	public boolean isAggregate() {
+		
+		return false;
+	}
+
+}
---
+++ urbsat/src/test/java/org/urbsat/function/ReadExecuteEclatement.java
@@ -0,0 +1,100 @@
+package org.urbsat.function;
+
+import java.io.File;
+
+import org.gdms.data.DataSource;
+import org.gdms.data.DataSourceFactory;
+import org.gdms.data.ExecutionException;
+import org.gdms.data.NoSuchTableException;
+import org.gdms.data.StatusCheckDecorator;
+import org.gdms.data.SyntaxException;
+import org.gdms.driver.DriverException;
+import org.gdms.spatial.SpatialDataSource;
+import org.gdms.spatial.SpatialDataSourceDecorator;
+import org.gdms.sql.function.FunctionManager;
+
+import com.hardcode.driverManager.DriverLoadException;
+
+public class ReadExecuteEclatement {
+
+	static DataSourceFactory dsf = new DataSourceFactory();
+
+	static DataSource ds1 = null;
+
+	
+
+	static String ds1Name;
+
+
+
+	public static void main(String[] args) throws Exception {
+
+		Long beginTime = System.currentTimeMillis();
+		
+		FunctionManager.addFunction(new Eclatement());
+		
+
+		File src1 = new File(
+				"../../datas2tests/shp/mediumshape2D/landcover2000.shp");
+		
+		ds1 = dsf.getDataSource(src1);
+		ds1Name = ds1.getName();
+				
+		testEclatement();
+
+		System.out.printf("=> %d ms\n", System.currentTimeMillis() - beginTime);
+
+	}
+
+	private static void testEclatement() throws Exception {
+
+		ds1.beginTrans();
+		String sqlQuery = "select Eclatement(the_geom) from " + ds1Name  + " where gid=1;";
+		DataSource result = dsf.executeSQL(sqlQuery);
+		System.out.println("test1");
+		displayValue(result);
+		
+		
+	
+	}
+
+	
+	public static void displayValue(DataSource result2)
+	throws DriverException {
+		
+		result2.beginTrans();
+		
+		
+		for (int i = 0; i < result2.getFieldNames().length; i++) {
+				
+				System.out.println(result2.getAsString());
+				System.out.println(result2.getFieldNames()[i]);
+				
+				System.out.println(result2.getFieldValue(0, 5));
+				
+				System.out.println(i);
+			
+		}
+
+		result2.rollBackTrans();
+	}
+	
+
+	public static void displayGeometry(SpatialDataSource spatialds2)
+			throws DriverException {
+
+		spatialds2.beginTrans();
+
+		for (int i = 0; i < spatialds2.getRowCount(); i++) {
+
+			if (spatialds2.getGeometry(i).isEmpty()) {
+
+			} else {
+				System.out.println(spatialds2.getGeometry(i).toString());
+			}
+		}
+
+		spatialds2.rollBackTrans();
+
+	}
+}
