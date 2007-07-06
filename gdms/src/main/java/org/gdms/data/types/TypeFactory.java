6f10682356c50097a7c3106ea475a8f9c090e1b2
ebocher <ebocher@irstv>
2007-07-06 07:03:48 +0000

    A IsSpatial method for Samuel newbie

--- gdms/src/main/java/org/gdms/data/types/TypeFactory.java
+++ gdms/src/main/java/org/gdms/data/types/TypeFactory.java
@@ -1,5 +1,9 @@
 package org.gdms.data.types;
 
+import org.gdms.data.DataSource;
+import org.gdms.data.metadata.Metadata;
+import org.gdms.driver.DriverException;
+
 public class TypeFactory {
 	public static Type createType(final int typeCode)
 			throws InvalidTypeException {
@@ -41,5 +45,22 @@ public class TypeFactory {
 					typeCode, constraintNames);
 			return typeDef.createType(constraints);
 		}
+	}
+
+	public static boolean IsSpatial(DataSource ds) throws DriverException {
+
+		ds.open();
+		Metadata m = ds.getMetadata();
+		boolean isSpatial = false;
+		for (int i = 0; i < m.getFieldCount(); i++) {
+			if (m.getFieldType(i).getTypeCode() == Type.GEOMETRY) {
+				isSpatial = true;
+				break;
+			}
+		}
+		ds.cancel();
+
+		return isSpatial;
+
 	}
 }
