d31404d1d0d8e34fb77d13fa80c088b97dfbaeb1
thomas <thomas@irstv>
2007-09-11 12:35:15 +0000

    DataSourceFactory modification (Solene Val Driver)

--- gdms/src/main/java/org/gdms/data/DataSourceFactory.java
+++ gdms/src/main/java/org/gdms/data/DataSourceFactory.java
@@ -29,6 +29,7 @@ import org.gdms.driver.h2.H2spatialDriver;
 import org.gdms.driver.hsqldb.HSQLDBDriver;
 import org.gdms.driver.shapefile.ShapefileDriver;
 import org.gdms.driver.solene.CirDriver;
+import org.gdms.driver.solene.ValDriver;
 import org.gdms.sql.instruction.Adapter;
 import org.gdms.sql.instruction.CreateAdapter;
 import org.gdms.sql.instruction.CustomAdapter;
@@ -876,6 +877,7 @@ public class DataSourceFactory {
 			dm.registerDriver(ShapefileDriver.DRIVER_NAME,
 					ShapefileDriver.class);
             dm.registerDriver(CirDriver.DRIVER_NAME, CirDriver.class);
+            dm.registerDriver(ValDriver.DRIVER_NAME, ValDriver.class);
 			dm.registerDriver(HSQLDBDriver.DRIVER_NAME, HSQLDBDriver.class);
 			dm.registerDriver(H2spatialDriver.DRIVER_NAME,
 					H2spatialDriver.class);
