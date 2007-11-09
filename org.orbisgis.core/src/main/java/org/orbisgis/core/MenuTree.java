443e78bcac2152b5f9c09dcfa12f8e4d6c46eeb2
fernando <fernando@irstv>
2007-11-09 13:46:48 +0000

    Fixed bug when adding a menu before its parent

--- org.orbisgis.core/src/main/java/org/orbisgis/core/MenuTree.java
+++ org.orbisgis.core/src/main/java/org/orbisgis/core/MenuTree.java
@@ -17,6 +17,10 @@ public class MenuTree {
 	}
 
 	public void addMenu(Menu menu) {
+		addMenu(menu, true);
+	}
+
+	private void addMenu(Menu menu, boolean addUnlinked) {
 		if (menu.getParent() == null) {
 			root.addChild(menu);
 		} else {
@@ -26,17 +30,13 @@ public class MenuTree {
 			}
 			Menu parent = getNode(root, menu.getParent());
 			if (parent == null) {
-				unlinkedMenus.add(menu);
+				if (addUnlinked) {
+					unlinkedMenus.add(menu);
+				}
 			} else {
+				unlinkedMenus.remove(menu);
 				parent.addChild(menu);
 			}
-		}
-
-		ArrayList<Menu> previousUnlinkedMenus = unlinkedMenus;
-		unlinkedMenus = new ArrayList<Menu>();
-
-		for (int i = 0; i < previousUnlinkedMenus.size(); i++) {
-			addMenu(previousUnlinkedMenus.get(i));
 		}
 	}
 
@@ -56,12 +56,24 @@ public class MenuTree {
 	}
 
 	public JMenuItem[] getJMenus() {
-		Menu[] childs = root.getChilds();
-		JMenuItem[] ret = new JMenuItem[childs.length];
-		for (int i = 0; i < ret.length; i++) {
-			ret[i] = childs[i].getJMenuItem(al);
-		}
+		int lastSize;
+		do {
+			lastSize = unlinkedMenus.size();
+			for (int i = unlinkedMenus.size() - 1; i >= 0; i--) {
+				addMenu(unlinkedMenus.get(i), false);
+			}
+		} while (lastSize != unlinkedMenus.size());
+		if (unlinkedMenus.isEmpty()) {
+			Menu[] childs = root.getChilds();
+			JMenuItem[] ret = new JMenuItem[childs.length];
+			for (int i = 0; i < ret.length; i++) {
+				ret[i] = childs[i].getJMenuItem(al);
+			}
 
-		return ret;
+			return ret;
+		} else {
+			throw new IllegalStateException("There are unlinked menus:"
+					+ unlinkedMenus.get(0).getId());
+		}
 	}
 }
