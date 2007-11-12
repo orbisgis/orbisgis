27749b912512df6d92265e00600b4464f74b315e
fernando <fernando@irstv>
2007-11-12 15:42:54 +0000

    Added a commented test that fails

--- org.orbisgis.geocatalog/src/test/java/org/orbisgis/geocatalog/CatalogTest.java
+++ org.orbisgis.geocatalog/src/test/java/org/orbisgis/geocatalog/CatalogTest.java
@@ -13,11 +13,11 @@ public class CatalogTest extends TestCase {
 		Catalog cat = new Catalog();
 		OrbisgisCore.getDSF().registerDataSource("source",
 				new FileSourceDefinition("a.csv"));
-		assertTrue(cat.getTreeModel().getRoot().getChildAt(0).getName()
-				.equals("source"));
+		assertTrue(cat.getTreeModel().getRoot().getChildAt(0).getName().equals(
+				"source"));
 		OrbisgisCore.getDSF().getSourceManager().rename("source", "source2");
-		assertTrue(cat.getTreeModel().getRoot().getChildAt(0).getName()
-				.equals("source2"));
+		assertTrue(cat.getTreeModel().getRoot().getChildAt(0).getName().equals(
+				"source2"));
 		OrbisgisCore.getDSF().remove("source2");
 		assertTrue(cat.getTreeModel().getRoot().getChildCount() == 0);
 	}
@@ -31,5 +31,8 @@ public class CatalogTest extends TestCase {
 		res.setName("source2");
 		assertTrue(OrbisgisCore.getDSF().exists("source2"));
 		assertTrue(!OrbisgisCore.getDSF().exists("source"));
+		model.removeNode(res);
+		// TODO remove when solved
+		// assertTrue(!OrbisgisCore.getDSF().exists("source2"));
 	}
 }
