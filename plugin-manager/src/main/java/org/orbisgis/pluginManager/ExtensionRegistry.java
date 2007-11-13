1d3e6a9c2aef764bcdc51e7cff8f21ad4489ab04
fernando <fernando@irstv>
2007-11-13 13:54:38 +0000

    Removed the ExtensionRunner concept

--- plugin-manager/src/main/java/org/orbisgis/pluginManager/ExtensionRegistry.java
+++ plugin-manager/src/main/java/org/orbisgis/pluginManager/ExtensionRegistry.java
@@ -2,12 +2,10 @@ package org.orbisgis.pluginManager;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
 class ExtensionRegistry implements IExtensionRegistry {
 
-	HashMap<String, ArrayList<Extension>> extensionsByPoint = new HashMap<String, ArrayList<Extension>>();
-	HashMap<String, IExtensionRunner> extensionRunner = new HashMap<String, IExtensionRunner>();
+	private HashMap<String, ArrayList<Extension>> extensionsByPoint = new HashMap<String, ArrayList<Extension>>();
 
 	public ExtensionRegistry(ArrayList<Extension> extensions) {
 
@@ -31,31 +29,4 @@ class ExtensionRegistry implements IExtensionRegistry {
 		}
 		return extensions.toArray(emptySet);
 	}
-
-	public void registerExtensionRunnerInstance(IExtensionRunner runner) {
-		Iterator<ArrayList<Extension>> exts = extensionsByPoint.values()
-				.iterator();
-		while (exts.hasNext()) {
-			ArrayList<Extension> array = exts.next();
-			for (Extension extension : array) {
-				String className = runner.getClass().getCanonicalName();
-				if (className.equals(extension.getRunnerClass())) {
-					extensionRunner.put(extension.getId(), runner);
-				}
-			}
-		}
-	}
-
-	public void executeExtension(String extensionId, String param) {
-		IExtensionRunner er = extensionRunner.get(extensionId);
-		if (er != null) {
-			er.execute(param);
-		} else {
-			throw new IllegalStateException("No runner instance has been"
-					+ " registered. The instance that manages "
-					+ "the extensions should be registered with "
-					+ "registerRunnerInstance() method");
-		}
-	}
-
 }
--- plugin-manager/src/main/java/org/orbisgis/pluginManager/IExtensionRegistry.java
+++ plugin-manager/src/main/java/org/orbisgis/pluginManager/IExtensionRegistry.java
@@ -11,22 +11,4 @@ public interface IExtensionRegistry {
 	 */
 	Extension[] getExtensions(String extensionPointId);
 
-	/**
-	 * Executes the extension with the specified id using the specified
-	 * parameter. The actual meaning of the execution is extension point
-	 * dependent. To successfully execute an extension, the instance that
-	 * manages it have to be registered before with registerRunnerInstance()
-	 *
-	 * @param extensionId
-	 * @param param
-	 */
-	void executeExtension(String extensionId, String param);
-
-	/**
-	 * Registers the instance that manages the extension executions
-	 *
-	 * @param runner
-	 */
-	public void registerExtensionRunnerInstance(IExtensionRunner runner);
-
 }
