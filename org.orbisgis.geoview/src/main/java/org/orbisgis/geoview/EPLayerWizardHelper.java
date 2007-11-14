adbd4049086ad4e5b2db37fa7a107f2bc1b57a07
fernando <fernando@irstv>
2007-11-14 14:34:31 +0000

    NewLayerWizard extension point helper

---
+++ org.orbisgis.geoview/src/main/java/org/orbisgis/geoview/EPLayerWizardHelper.java
@@ -0,0 +1,60 @@
+package org.orbisgis.geoview;
+
+import java.util.ArrayList;
+
+import org.orbisgis.core.ChoosePanel;
+import org.orbisgis.core.wizards.WizardAndId;
+import org.orbisgis.core.wizards.WizardGetter;
+import org.orbisgis.geoview.layerModel.CRSException;
+import org.orbisgis.geoview.layerModel.ILayer;
+import org.sif.UIFactory;
+import org.sif.UIPanel;
+
+public class EPLayerWizardHelper {
+
+	public static void openWizard(GeoView2D geoview) {
+		ArrayList<WizardAndId<INewLayer>> wizards = getWizards(null);
+		String[] names = new String[wizards.size()];
+		String[] ids = new String[wizards.size()];
+		for (int i = 0; i < names.length; i++) {
+			names[i] = wizards.get(i).getWizard().getName();
+			ids[i] = wizards.get(i).getId();
+		}
+		ChoosePanel cp = new ChoosePanel("Select the resource type", names, ids);
+		boolean accepted = UIFactory.showDialog(cp);
+		if (accepted) {
+			int index = cp.getSelectedIndex();
+			runWizard(geoview, wizards.get(index).getWizard());
+		}
+	}
+
+	private static void runWizard(GeoView2D geoview, INewLayer wizard) {
+		UIPanel[] panels = wizard.getWizardPanels();
+		boolean ok = UIFactory.showDialog(panels);
+		if (ok) {
+			ILayer[] layers = wizard.getLayers();
+			OGMapControlModel model = geoview.getMapModel();
+			ILayer lc = model.getLayers();
+			for (ILayer layer : layers) {
+				try {
+					lc.put(layer);
+				} catch (CRSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static void runWizard(GeoView2D geoview, String wizardId) {
+		ArrayList<WizardAndId<INewLayer>> wizards = getWizards(wizardId);
+		runWizard(geoview, wizards.get(0).getWizard());
+	}
+
+	private static ArrayList<WizardAndId<INewLayer>> getWizards(String id) {
+		WizardGetter<INewLayer> wg = new WizardGetter<INewLayer>(
+				"org.orbisgis.geoview.NewLayerWizard");
+		return wg.getWizards(id);
+	}
+
+}
--- org.orbisgis.geoview/src/main/java/org/orbisgis/geoview/NewLayerAction.java
+++ org.orbisgis.geoview/src/main/java/org/orbisgis/geoview/NewLayerAction.java
@@ -1,21 +1,9 @@
 package org.orbisgis.geoview;
 
-import org.orbisgis.geoview.layerModel.CRSException;
-import org.orbisgis.geoview.layerModel.ILayer;
 
 public class NewLayerAction implements IGeoviewAction {
 
 	public void actionPerformed(GeoView2D geoview) {
-		ILayer[] layers = LayerWizardEP.openWizard(geoview);
-		OGMapControlModel model = geoview.getMapModel();
-		ILayer lc = model.getLayers();
-		for (ILayer layer : layers) {
-			try {
-				lc.put(layer);
-			} catch (CRSException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		EPLayerWizardHelper.openWizard(geoview);
 	}
 }
