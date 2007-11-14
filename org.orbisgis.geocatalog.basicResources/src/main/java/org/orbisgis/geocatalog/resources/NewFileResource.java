3194db9bb1a97a4976ff1781434d569d08ca5662
fernando <fernando@irstv>
2007-11-14 14:32:33 +0000

    Possibility to add resources with special behavior

--- org.orbisgis.geocatalog.basicResources/src/main/java/org/orbisgis/geocatalog/resources/NewFileResource.java
+++ org.orbisgis.geocatalog.basicResources/src/main/java/org/orbisgis/geocatalog/resources/NewFileResource.java
@@ -3,9 +3,7 @@ package org.orbisgis.geocatalog.resources;
 import java.io.File;
 import java.util.ArrayList;
 
-import org.gdms.data.file.FileSourceDefinition;
 import org.orbisgis.core.FileWizard;
-import org.orbisgis.core.OrbisgisCore;
 import org.orbisgis.core.resourceTree.IResource;
 import org.orbisgis.geocatalog.INewResource;
 
@@ -19,10 +17,7 @@ public class NewFileResource extends FileWizard implements INewResource {
 		File[] files = getSelectedFiles();
 		ArrayList<IResource> resources = new ArrayList<IResource>();
 		for (File file : files) {
-			String registerName = OrbisgisCore.registerInDSF(file.getName(),
-					new FileSourceDefinition(file));
-
-			resources.add(new GdmsSource(registerName));
+			resources.add(new FileGdmsSource(file));
 		}
 
 		return resources.toArray(new IResource[0]);
