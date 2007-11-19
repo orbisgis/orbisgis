574c140ee770c0ab950795d32bed34f09bcece1a
thomas <thomas@irstv>
2007-11-19 14:04:56 +0000

    org.urbsat : Explode custom query

--- org.urbsat/src/main/java/org/urbsat/utilities/Explode.java
+++ org.urbsat/src/main/java/org/urbsat/utilities/Explode.java
@@ -1,95 +1,131 @@
 package org.urbsat.utilities;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.gdms.data.DataSource;
 import org.gdms.data.DataSourceFactory;
 import org.gdms.data.ExecutionException;
-import org.gdms.data.object.ObjectSourceDefinition;
+import org.gdms.data.indexes.SpatialIndex;
+import org.gdms.data.metadata.Metadata;
+import org.gdms.data.types.Constraint;
+import org.gdms.data.types.ConstraintNames;
+import org.gdms.data.types.DefaultType;
 import org.gdms.data.types.Type;
-import org.gdms.data.types.TypeFactory;
 import org.gdms.data.values.Value;
 import org.gdms.data.values.ValueFactory;
 import org.gdms.driver.memory.ObjectMemoryDriver;
 import org.gdms.spatial.GeometryValue;
+import org.gdms.spatial.SpatialDataSourceDecorator;
 import org.gdms.sql.customQuery.CustomQuery;
-import org.gdms.sql.function.Function;
-import org.gdms.sql.function.FunctionException;
+import org.gdms.sql.strategies.FirstStrategy;
 
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
 
-// call register('/import/leduc/dev/eclipse/datas2tests/cir/face_unitaire.cir','src');
-// select Explode(ConvertIntoMultiPoint2D(the_geom)) from src;
-
-/*
- call register('/import/leduc/dev/eclipse/datas2tests/shp/smallshape2D/points.shp','src');
-
- select id,AsWKT(the_geom),AsWKT(Explode(ConvertIntoMultiPoint2D(the_geom))) from points;
- */
+//select Explode() from points;
+//select Explode(the_geom) from points;
 
 public class Explode implements CustomQuery {
-	private final static DataSourceFactory dsf = new DataSourceFactory();
-	private static DataSource dataSource;
-	private static long count = 0;
-
-	static {
-		final String uniqueName = "test";
-		ObjectMemoryDriver omd;
-		try {
-			omd = new ObjectMemoryDriver(new String[] { "id", "the_geom" },
-					new Type[] { TypeFactory.createType(Type.LONG),
-							TypeFactory.createType(Type.GEOMETRY) });
-			dsf.getSourceManager().register(uniqueName,
-					new ObjectSourceDefinition(omd));
-			dataSource = dsf.getDataSource(uniqueName);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	public String getName() {
 		return "Explode";
-	}
-
-	public Value evaluate(Value[] args) throws FunctionException {
-		final GeometryValue gv = (GeometryValue) args[0];
-		final Geometry geom = gv.getGeom();
-
-		try {
-			dataSource.open();
-			if (geom instanceof GeometryCollection) {
-				dataSource.insertFilledRow(new Value[] {
-						ValueFactory.createValue(count++), gv });
-			} else {
-				final long nbOfGeometries = geom.getNumGeometries();
-				for (int i = 0; i < nbOfGeometries; i++) {
-					dataSource.insertFilledRow(new Value[] {
-							ValueFactory.createValue(count++),
-							ValueFactory.createValue(geom.getGeometryN(i)) });
-				}
-			}
-			dataSource.commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 
 	public String getDescription() {
 		return "Convert any GeometryCollection into a set of single Geometries";
 	}
 
-	public int getType(int[] paramTypes) {
-		return Type.GEOMETRY;
-	}
-
-	public boolean isAggregate() {
-		// TODO
-		return false;
-	}
-
 	public DataSource evaluate(DataSourceFactory dsf, DataSource[] tables,
 			Value[] values) throws ExecutionException {
-		// TODO Auto-generated method stub
-		return null;
+		if (tables.length != 1) {
+			throw new ExecutionException("Explode only operates on one table");
+		}
+		if (values.length > 1) {
+			throw new ExecutionException(
+					"Explode operates with no more than one value");
+		}
+
+		DataSource resultDs = null;
+		try {
+			final SpatialDataSourceDecorator sds = new SpatialDataSourceDecorator(
+					tables[0]);
+			sds.open();
+			if (1 == values.length) {
+				// if no spatial's field's name is provided, the default (first)
+				// one is arbitrarily choose.
+				final String spatialFieldName = values[0].toString();
+				sds.setDefaultGeometry(spatialFieldName);
+			}
+			final int spatialFieldIndex = sds.getSpatialFieldIndex();
+
+			final Metadata metadata = sds.getMetadata();
+			// a simple :
+			// final ObjectMemoryDriver driver = new
+			// ObjectMemoryDriver(metadata);
+			// is not enough... we also need to remove all pk or unique
+			// constraints !
+			final int fieldCount = metadata.getFieldCount();
+			final Type[] fieldsTypes = new Type[fieldCount];
+			final String[] fieldsNames = new String[fieldCount];
+
+			for (int fieldId = 0; fieldId < fieldCount; fieldId++) {
+				fieldsNames[fieldId] = metadata.getFieldName(fieldId);
+				fieldsTypes[fieldId] = metadata.getFieldType(fieldId);
+
+				if ((null == fieldsTypes[fieldId]
+						.getConstraint(ConstraintNames.PK))
+						|| ((null == fieldsTypes[fieldId]
+								.getConstraint(ConstraintNames.UNIQUE)))) {
+					// let us try to remove the PK/UNIQUE constraint...
+					final List<Constraint> lc = new LinkedList<Constraint>();
+					for (Constraint c : fieldsTypes[fieldId].getConstraints()) {
+						if ((ConstraintNames.PK != c.getConstraintName())
+								&& (ConstraintNames.UNIQUE != c
+										.getConstraintName())) {
+							lc.add(c);
+						}
+					}
+					fieldsTypes[fieldId] = new DefaultType(lc
+							.toArray(new Constraint[0]), fieldsTypes[fieldId]
+							.getDescription(), fieldsTypes[fieldId]
+							.getTypeCode());
+				}
+			}
+			final ObjectMemoryDriver driver = new ObjectMemoryDriver(
+					fieldsNames, fieldsTypes);
+			resultDs = dsf.getDataSource(driver);
+			resultDs.open();
+
+			long nbOfRows = sds.getRowCount();
+			for (long rowIndex = 0; rowIndex < nbOfRows; rowIndex++) {
+				final Value[] fieldsValues = sds.getRow(rowIndex);
+				final Geometry geometry = ((GeometryValue) fieldsValues[spatialFieldIndex])
+						.getGeom();
+
+				if (geometry instanceof GeometryCollection) {
+					final long nbOfGeometries = geometry.getNumGeometries();
+					for (int i = 0; i < nbOfGeometries; i++) {
+						fieldsValues[spatialFieldIndex] = ValueFactory
+								.createValue(geometry.getGeometryN(i));
+						resultDs.insertFilledRow(fieldsValues);
+					}
+
+				} else {
+					resultDs.insertFilledRow(fieldsValues);
+				}
+			}
+
+			resultDs.commit();
+			sds.cancel();
+			// spatial index for the new grid
+
+			dsf.getIndexManager().buildIndex(resultDs.getName(),
+					sds.getDefaultGeometry(), SpatialIndex.SPATIAL_INDEX);
+
+			FirstStrategy.indexes = true;
+		} catch (Exception e) {
+			throw new ExecutionException(e);
+		}
+		return resultDs;
 	}
 }
