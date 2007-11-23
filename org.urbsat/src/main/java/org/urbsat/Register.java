cfb83b52b706c9f28b5e652f18762aa1876de58b
thomas <thomas@irstv>
2007-11-23 15:45:10 +0000

    org.urbsat : new Compacity function

--- org.urbsat/src/main/java/org/urbsat/Register.java
+++ org.urbsat/src/main/java/org/urbsat/Register.java
@@ -9,13 +9,13 @@ import org.urbsat.custom.BuildArea;
 import org.urbsat.custom.BuildLenght;
 import org.urbsat.custom.BuildNumber;
 import org.urbsat.custom.BuildVolume;
-import org.urbsat.custom.Compacity;
 import org.urbsat.custom.FrontalDensity;
 import org.urbsat.custom.LateralDensity;
 import org.urbsat.custom.OldAverageBuildHeight;
 import org.urbsat.custom.StandardDeviationBuildBalanced;
 import org.urbsat.custom.StandardDeviationBuildHeight;
 import org.urbsat.landcoverIndicators.custom.Density;
+import org.urbsat.landcoverIndicators.function.Compacity;
 import org.urbsat.landcoverIndicators.function.MeanSpacingBetweenBuildingsInACell;
 import org.urbsat.utilities.CreateGrid;
 import org.urbsat.utilities.GetZDEM;
@@ -25,7 +25,6 @@ public class Register implements PluginActivator {
 		QueryManager.registerQuery(new CreateGrid());
 		QueryManager.registerQuery(new Density());
 		QueryManager.registerQuery(new BuildNumber());
-		QueryManager.registerQuery(new Compacity());
 		QueryManager.registerQuery(new BuildLenght());
 		QueryManager.registerQuery(new OldAverageBuildHeight());
 		QueryManager.registerQuery(new AverageBuildHeight());
@@ -40,6 +39,7 @@ public class Register implements PluginActivator {
 		QueryManager.registerQuery(new GetZDEM());
 
 		FunctionManager.addFunction(new MeanSpacingBetweenBuildingsInACell());
+		FunctionManager.addFunction(new Compacity());
 	}
 
 	public void stop() throws Exception {
---
+++ org.urbsat/src/main/java/org/urbsat/landcoverIndicators/function/Compacity.java
@@ -0,0 +1,53 @@
+package org.urbsat.landcoverIndicators.function;
+
+import org.gdms.data.types.Type;
+import org.gdms.data.values.Value;
+import org.gdms.data.values.ValueFactory;
+import org.gdms.spatial.GeometryValue;
+import org.gdms.sql.function.Function;
+import org.gdms.sql.function.FunctionException;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+/*
+ * select explode(the_geom) from build where type='bati';
+ * => build1
+ * select compacity(the_geom) from build1;
+ */
+public class Compacity implements Function {
+
+	public Function cloneFunction() {
+		return new Compacity();
+	}
+
+	public Value evaluate(Value[] args) throws FunctionException {
+		final Geometry geomBuild = ((GeometryValue) args[0]).getGeom();
+		final double sBuild = geomBuild.getArea();
+		final double pBuild = geomBuild.getLength();
+		// final double ratioBuild = sBuild / pBuild;
+
+		final double correspondingCircleRadius = Math.sqrt(sBuild / Math.PI);
+		// final double sCircle = sBuild;
+		final double pCircle = 2 * Math.PI * correspondingCircleRadius;
+		// final double ratioCircle = sCircle / pCircle;
+
+		// return ValueFactory.createValue(ratioCircle / ratioBuild);
+		return ValueFactory.createValue(pBuild / pCircle);
+	}
+
+	public String getDescription() {
+		return "Calculate the compacity of each building's geometry : select Compacity(the_geom) from buildings";
+	}
+
+	public String getName() {
+		return "Compacity";
+	}
+
+	public int getType(int[] paramTypes) {
+		return Type.DOUBLE;
+	}
+
+	public boolean isAggregate() {
+		return false;
+	}
+}
