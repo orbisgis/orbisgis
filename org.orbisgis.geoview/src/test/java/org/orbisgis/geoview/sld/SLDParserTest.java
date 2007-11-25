ebdde97d6c63c3890e3a12550302bbfee8aa64f2
ebocher <ebocher@irstv>
2007-11-25 15:40:50 +0000

    SLD parser test

---
+++ org.orbisgis.geoview/src/test/java/org/orbisgis/geoview/sld/SLDParserTest.java
@@ -0,0 +1,93 @@
+package org.orbisgis.geoview.sld;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.orbisgis.geoview.renderer.style.sld.FeatureTypeStyle;
+import org.orbisgis.geoview.renderer.style.sld.Rule;
+import org.orbisgis.geoview.renderer.style.sld.SLDParser;
+import org.orbisgis.geoview.renderer.style.sld.Symbolizer;
+
+import com.ximpleware.EOFException;
+import com.ximpleware.EncodingException;
+import com.ximpleware.EntityException;
+import com.ximpleware.NavException;
+import com.ximpleware.ParseException;
+import com.ximpleware.xpath.XPathEvalException;
+import com.ximpleware.xpath.XPathParseException;
+
+public class SLDParserTest {
+
+	
+	public static void main(String[] args) {
+		
+		//String path = "..//..//datas2tests//sld//densityBySymbols.sld";
+
+		//String path = "..//..//datas2tests//sld//yellowparcels.sld";
+		
+		//String path = "..//..//datas2tests//sld//greenline.sld";
+		
+		String path = "..//..//datas2tests//sld//redRoads.sld";
+
+		
+		try {
+					
+		
+		
+		SLDParser parser = new SLDParser(path);
+
+		
+		parser.read();
+		
+
+		List<FeatureTypeStyle> featureTypeStyles = parser.getFeatureTypeStyles();
+
+		
+		for (int i = 0; i < featureTypeStyles.size(); i++) {
+			
+			int ruleCount = featureTypeStyles.get(i).getRuleCount();
+			
+			System.out.println("Number of rules : " + ruleCount);
+			
+			List<Rule> rules = featureTypeStyles.get(i).getRules();
+						
+			
+			for (int j = 0; j < ruleCount; j++) {
+				
+				System.out.println("Symbolizer type : " + rules.get(j).getSymbolizer().getType());
+				
+			}
+			
+		}
+		
+		
+		
+		
+		} catch (EncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EOFException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EntityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XPathParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XPathEvalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NavException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
