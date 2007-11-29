60ad7970dd671c3c980377c1eae7aead6f209cb1
fernando <fernando@irstv>
2007-11-29 09:36:10 +0000

    Warn if there is some object datasource

--- src/main/java/org/orbisgis/CollectionUtils.java
+++ src/main/java/org/orbisgis/CollectionUtils.java
@@ -12,4 +12,15 @@ public class CollectionUtils {
 		return false;
 	}
 
+	public static String getCommaSeparated(Object[] array) {
+		StringBuilder ret = new StringBuilder("");
+		String separator = "";
+		for (Object object : array) {
+			ret.append(separator).append(object);
+			separator=", ";
+		}
+
+		return ret.toString();
+	}
+
 }
