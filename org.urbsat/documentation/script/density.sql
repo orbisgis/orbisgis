73ea6838a5af14f51bd18d5356a52bea42c77eb4
thomas <thomas@irstv>
2007-11-30 16:39:47 +0000

    /platform/org.urbsat/documentation -> /documentation/urbsat

--- org.urbsat/documentation/script/density.sql
+++
@@ -1,4 +0,0 @@
-select register('c://tmp//grid.shp', 'grid');
-create table grid as select creategrid(500,500) from bati;
-select register('c://tmp//bati_inter.shp', 'bati_inter');
-create table bati_inter as select intersection(a.the_geom,b.the_geom), a.index from grid as a, bati as b where intersects(a.the_geom, b.the_geom);
