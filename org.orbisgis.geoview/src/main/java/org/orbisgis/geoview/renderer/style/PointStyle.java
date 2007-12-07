cb2df030143e7b411874572c005dcf68f4de5b90
fernando <fernando@irstv>
2007-12-07 13:03:52 +0000

    bug fix:symbol rendering

--- org.orbisgis.geoview/src/main/java/org/orbisgis/geoview/renderer/style/PointStyle.java
+++ org.orbisgis.geoview/src/main/java/org/orbisgis/geoview/renderer/style/PointStyle.java
@@ -12,8 +12,8 @@ import org.orbisgis.geoview.renderer.liteShape.LiteShape;
 
 public class PointStyle implements Style{
 
-	
-	
+
+
 	private static String stringFillColor = "#FFFFFF";
 	private static String stringLineColor = "#FFFF00";
 	private int fillOpacity;
@@ -22,31 +22,31 @@ public class PointStyle implements Style{
 	private Color fillColor;
 	static String defaultShape = "circle";
 	static int size = 4;
-	
-	
+
+
 	public PointStyle() {
 		this(defaultShape,size,  stringFillColor,stringLineColor,1 );
-		
+
 	}
 
 	public PointStyle(int size){
 		this(defaultShape,size,  stringFillColor,stringLineColor,1 );
-		
+
 	}
-	
+
 	public  PointStyle (String stringFillColor, String stringLineColor) {
 		this(defaultShape,size,  stringFillColor,stringLineColor,1 );
-		
+
 	}
-	
-	
-	
+
+
+
 	public  PointStyle (String pointShape, String stringFillColor, String stringLineColor) {
 		this(pointShape,size,  stringFillColor,stringLineColor,1 );
-		
+
 	}
-	
-	
+
+
 	public  PointStyle (String pointShape, int pointSize, String stringFillColor, String stringLineColor, int fillOpacity) {
 		this.pointShape =pointShape;
 		this.pointSize= pointSize;
@@ -54,80 +54,80 @@ public class PointStyle implements Style{
 		this.stringLineColor = stringLineColor;
 		this.fillOpacity =fillOpacity;
 	}
-	
-	
+
+
 
 	public Shape getDefaultShape(double x, double y){
-		
+
 		double r = (getSize() / 2d);
-			 
-		
-		return new Ellipse2D.Double(x, y, r, r);
-		
-		
+
+
+		return new Ellipse2D.Double(x-r, y-r, size, size);
+
+
 	}
-	
+
 	public Shape getDefaultShape(double x, double y, float size){
-		
+
 		double r = (size / 2d);
-			 
-		
-		return new Ellipse2D.Double(x, y, r, r);
-		
-		
+
+
+		return new Ellipse2D.Double(x-r, y-r, size, size);
+
+
 	}
-	
-	
-	
+
+
+
 	public Color getFillColor(){
-		
-		
+
+
 		if (stringFillColor.length()>0){
 			return Color.decode(stringFillColor);
 		}
-	
-	
-	
-	return null;	
-	
+
+
+
+	return null;
+
 }
-		
+
 	public Color getLineColor() {
-			
+
 			if (stringLineColor.length()>0){
 				return Color.decode(stringLineColor);
 			}
-		
-		
+
+
 		return null;
 	}
-	
 
-	
+
+
 	public AlphaComposite getAlphaComposite(){
-		
-		
+
+
 			if ((fillOpacity > 0)&& (fillOpacity < 1)){
 				return AlphaComposite.getInstance(AlphaComposite.SRC_OVER, fillOpacity);
 			}
-			
-		
-		
+
+
+
 		return AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1);
-		
-		
+
+
 	}
-	
+
 	public Color getDefaultLineColor() {
-		
+
 		return Color.BLACK;
 	}
-	
+
 	public BasicStroke getBasicStroke() {
 		return null;
 	}
 
-	
+
 	 public void setSize(int size) {
 	        this.size = size;
 	    }
@@ -136,5 +136,5 @@ public class PointStyle implements Style{
 	        return size;
 	    }
 
-	
+
 }
