379846880f3e70675a061f353be84cd047e2effb
fernando <fernando@irstv>
2007-12-19 09:53:32 +0000

    wrong output package for gdms

--- plugin-manager/docs/produceJaxbClasses.sh
+++ plugin-manager/docs/produceJaxbClasses.sh
@@ -5,7 +5,7 @@
 
 schemasAndTargetPackages() {
 	cat <<EOF
-gdms/src/main/resources/source-info-directory.xsd													org.gdms.data.persistence
+gdms/src/main/resources/source-info-directory.xsd													org.gdms.source.directory
 org.orbisgis.core/src/main/resources/org/orbisgis/core/persistence/windows.xsd						org.orbisgis.core.persistence
 org.orbisgis.geocatalog/src/main/resources/org/orbisgis/geocatalog/persistence/catalog.xsd			org.orbisgis.geocatalog.persistence
 org.orbisgis.geoview/src/main/resources/org/orbisgis/geoview/viewContext.xsd						org.orbisgis.geoview.persistence
