0dd57cdb52a493140894306c6f719556bd8db133
thomas <thomas@irstv>
2007-12-19 09:44:06 +0000

    org.orbisgis.geoview : new Activator class + Geomark (removed from org.urbsat)

---
+++ org.orbisgis.geoview/src/main/java/org/orbisgis/geoview/Register.java
@@ -0,0 +1,18 @@
+package org.orbisgis.geoview;
+
+import org.gdms.sql.customQuery.QueryManager;
+import org.orbisgis.geoview.sql.customQuery.Geomark;
+import org.orbisgis.pluginManager.PluginActivator;
+
+public class Register implements PluginActivator {
+	public void start() throws Exception {
+		QueryManager.registerQuery(new Geomark());
+	}
+
+	public void stop() throws Exception {
+	}
+
+	public boolean allowStop() {
+		return true;
+	}
+}
