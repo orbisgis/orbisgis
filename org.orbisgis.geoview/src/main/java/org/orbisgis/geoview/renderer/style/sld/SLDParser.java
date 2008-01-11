7cc5d9b0136dc576c841d21418727851021b4e5c
thomas <thomas@irstv>
2008-01-11 11:50:25 +0000

    org.orbisgis.*, commons: put OrbisGIS file header in java sources

--- org.orbisgis.geoview/src/main/java/org/orbisgis/geoview/renderer/style/sld/SLDParser.java
+++ org.orbisgis.geoview/src/main/java/org/orbisgis/geoview/renderer/style/sld/SLDParser.java
@@ -1,3 +1,41 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at french IRSTV institute and is able
+ * to manipulate and create vectorial and raster spatial information. OrbisGIS
+ * is distributed under GPL 3 licence. It is produced  by the geomatic team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/>, CNRS FR 2488:
+ *    Erwan BOCHER, scientific researcher,
+ *    Thomas LEDUC, scientific researcher,
+ *    Fernando GONZALEZ CORTES, computer engineer.
+ *
+ * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult:
+ *    <http://orbisgis.cerma.archi.fr/>
+ *    <http://sourcesup.cru.fr/projects/orbisgis/>
+ *    <http://listes.cru.fr/sympa/info/orbisgis-developers/>
+ *    <http://listes.cru.fr/sympa/info/orbisgis-users/>
+ *
+ * or contact directly:
+ *    erwan.bocher _at_ ec-nantes.fr
+ *    fergonco _at_ gmail.com
+ *    thomas.leduc _at_ cerma.archi.fr
+ */
 package org.orbisgis.geoview.renderer.style.sld;
 
 import com.ximpleware.*;
