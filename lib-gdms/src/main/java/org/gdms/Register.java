5eac76b80a4c7e5cf702552b10b5facdcb261154
thomas <thomas@irstv>
2008-05-08 10:01:44 +0000

    trk/lib-gdms: D8StrahlerStreamOrder can not be a function

--- lib-gdms/src/main/java/org/gdms/Register.java
+++ lib-gdms/src/main/java/org/gdms/Register.java
@@ -8,6 +8,8 @@ import org.gdms.sql.customQuery.spatial.geometry.topology.ToLineNoder;
 import org.gdms.sql.customQuery.spatial.raster.convert.RasterToPoints;
 import org.gdms.sql.customQuery.spatial.raster.convert.RasterToPolygons;
 import org.gdms.sql.customQuery.spatial.raster.convert.RasterToXYZ;
+import org.gdms.sql.customQuery.spatial.raster.hydrology.D8StrahlerStreamOrder;
+import org.gdms.sql.customQuery.spatial.raster.hydrology.D8ThresholdedWatershed;
 import org.gdms.sql.function.FunctionManager;
 import org.gdms.sql.function.spatial.geometry.extract.ToMultiSegments;
 import org.gdms.sql.function.spatial.geometry.generalize.Generalize;
@@ -16,7 +18,6 @@ import org.gdms.sql.function.spatial.raster.hydrology.D8AllOutlets;
 import org.gdms.sql.function.spatial.raster.hydrology.D8AllWatersheds;
 import org.gdms.sql.function.spatial.raster.hydrology.D8Directions;
 import org.gdms.sql.function.spatial.raster.hydrology.D8SlopesInPercent;
-import org.gdms.sql.function.spatial.raster.hydrology.D8StrahlerStreamOrder;
 import org.gdms.sql.function.spatial.raster.hydrology.D8WatershedFromOutletIndex;
 import org.gdms.sql.function.spatial.raster.utilities.CropRaster;
 import org.gdms.sql.function.spatial.raster.utilities.ToEnvelope;
@@ -24,31 +25,30 @@ import org.orbisgis.pluginManager.PluginActivator;
 
 public class Register implements PluginActivator {
 	public void start() throws Exception {
-
 		FunctionManager.addFunction(ToMultiSegments.class);
 		FunctionManager.addFunction(Generalize.class);
+
 		FunctionManager.addFunction(D8SlopesInPercent.class);
 		FunctionManager.addFunction(D8Directions.class);
 		FunctionManager.addFunction(D8Accumulations.class);
 		FunctionManager.addFunction(D8AllOutlets.class);
 		FunctionManager.addFunction(D8WatershedFromOutletIndex.class);
 		FunctionManager.addFunction(D8AllWatersheds.class);
+
 		FunctionManager.addFunction(CropRaster.class);
 		FunctionManager.addFunction(ToEnvelope.class);
-		
-		FunctionManager.addFunction(D8StrahlerStreamOrder.class);
 
 		QueryManager.registerQuery(new ToLineNoder());
 		QueryManager.registerQuery(new ShortestPath());
 		QueryManager.registerQuery(new BuildTIN());
-		
+
 		QueryManager.registerQuery(new RasterToPoints());
 		QueryManager.registerQuery(new RasterToPolygons());
 		QueryManager.registerQuery(new RasterToXYZ());
 		QueryManager.registerQuery(new PointsToXYZ());
 
-		
-		//QueryManager.registerQuery(new D8ThresholdedWatershed())
+		QueryManager.registerQuery(new D8ThresholdedWatershed());
+		QueryManager.registerQuery(new D8StrahlerStreamOrder());
 	}
 
 	public void stop() throws Exception {
