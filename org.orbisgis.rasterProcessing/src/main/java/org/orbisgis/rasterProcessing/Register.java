dd275dfa1c069b1028189dd95e918d0b4dc57073
fernando <fernando@irstv>
2008-05-16 15:16:31 +0000

    CropRaster available from SQL in rasterProcessing. orbisgis-core depends on 'processing' so all its development can be accessed in both server and desktop applications

--- org.orbisgis.rasterProcessing/src/main/java/org/orbisgis/rasterProcessing/Register.java
+++ org.orbisgis.rasterProcessing/src/main/java/org/orbisgis/rasterProcessing/Register.java
@@ -1,13 +1,15 @@
 package org.orbisgis.rasterProcessing;
 
 import org.gdms.sql.customQuery.QueryManager;
+import org.gdms.sql.function.FunctionManager;
+import org.gdms.sql.function.spatial.raster.utilities.CropRaster;
 import org.orbisgis.pluginManager.PluginActivator;
 import org.orbisgis.rasterProcessing.tin.Generate2DMesh;
 
 public class Register implements PluginActivator {
 	public void start() throws Exception {
 		QueryManager.registerQuery(new Generate2DMesh());
-
+		FunctionManager.addFunction(CropRaster.class);
 	}
 
 	public void stop() throws Exception {
