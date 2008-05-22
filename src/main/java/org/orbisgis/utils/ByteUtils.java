1fcbbe5394a9bffc079492bfcc5993a2dc2f879f
fernando <fernando@irstv>
2008-05-22 13:58:54 +0000

    Byte utilities and tests for GDMS driver

--- src/main/java/org/orbisgis/utils/ByteUtils.java
+++ src/main/java/org/orbisgis/utils/ByteUtils.java
@@ -16,4 +16,71 @@ public class ByteUtils {
 		return b;
 	}
 
+	public static byte[] shortsToBytes(short[] shorts) {
+		byte[] ret = new byte[shorts.length * 2];
+		for (int i = 0; i < shorts.length; i++) {
+			ret[i * 2] = (byte) ((shorts[i] >>> 0) & 0xFF);
+			ret[i * 2 + 1] = (byte) ((shorts[i] >>> 8) & 0xFF);
+		}
+
+		return ret;
+	}
+
+	public static short[] bytesToShorts(byte[] bytes) {
+		short[] ret = new short[bytes.length / 2];
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = (short) (((bytes[i * 2] & 0xff) << 0) + ((bytes[i * 2 + 1] & 0xff) << 8));
+		}
+
+		return ret;
+	}
+
+	public static byte[] intsToBytes(int[] ints) {
+		byte[] ret = new byte[ints.length * 4];
+		for (int i = 0; i < ints.length; i++) {
+			ret[i * 4] = (byte) ((ints[i] >>> 0) & 0xFF);
+			ret[i * 4 + 1] = (byte) ((ints[i] >>> 8) & 0xFF);
+			ret[i * 4 + 2] = (byte) ((ints[i] >>> 16) & 0xFF);
+			ret[i * 4 + 3] = (byte) ((ints[i] >>> 24) & 0xFF);
+		}
+		return ret;
+	}
+
+	public static int[] bytesToInts(byte[] bytes) {
+		int[] ret = new int[bytes.length / 4];
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = ((bytes[i * 4] & 0xff) << 0)
+					+ ((bytes[i * 4 + 1] & 0xff) << 8)
+					+ ((bytes[i * 4 + 2] & 0xff) << 16)
+					+ ((bytes[i * 4 + 3] & 0xff) << 24);
+		}
+
+		return ret;
+	}
+
+	public static byte[] floatsToBytes(float[] floats) {
+		byte[] ret = new byte[floats.length * 4];
+		for (int i = 0; i < floats.length; i++) {
+			int floatAsInt = Float.floatToIntBits(floats[i]);
+			ret[i * 4] = (byte) ((floatAsInt >>> 0) & 0xFF);
+			ret[i * 4 + 1] = (byte) ((floatAsInt >>> 8) & 0xFF);
+			ret[i * 4 + 2] = (byte) ((floatAsInt >>> 16) & 0xFF);
+			ret[i * 4 + 3] = (byte) ((floatAsInt >>> 24) & 0xFF);
+		}
+		return ret;
+	}
+
+	public static float[] bytesToFloats(byte[] bytes) {
+		float[] ret = new float[bytes.length / 4];
+		for (int i = 0; i < ret.length; i++) {
+			int floatAsInt = ((bytes[i * 4 + 3] & 0xff) << 24)
+					+ ((bytes[i * 4 + 2] & 0xff) << 16)
+					+ ((bytes[i * 4 + 1] & 0xff) << 8)
+					+ ((bytes[i * 4] & 0xff) << 0);
+			ret[i] = Float.intBitsToFloat(floatAsInt);
+		}
+
+		return ret;
+	}
+
 }
---
+++ src/test/java/org/orbisgis/ByteUtilsTest.java
@@ -0,0 +1,38 @@
+package org.orbisgis;
+
+import junit.framework.TestCase;
+
+import org.orbisgis.utils.ByteUtils;
+
+public class ByteUtilsTest extends TestCase {
+
+	public void testShortsBytes() throws Exception {
+		short[] shorts = new short[] { 1, 2, 3, 5, 1500, Short.MAX_VALUE };
+		byte[] shortBytes = ByteUtils.shortsToBytes(shorts);
+		short[] shorts2 = ByteUtils.bytesToShorts(shortBytes);
+		assertTrue(shorts.length == shorts2.length);
+		for (int i = 0; i < shorts2.length; i++) {
+			assertTrue(shorts[i] == shorts2[i]);
+		}
+	}
+
+	public void testFloats2Bytes() throws Exception {
+		float[] floats = new float[] { 4f, 2857685.235f, 3f, 356.234f };
+		byte[] floatBytes = ByteUtils.floatsToBytes(floats);
+		float[] floats2 = ByteUtils.bytesToFloats(floatBytes);
+		assertTrue(floats.length == floats2.length);
+		for (int i = 0; i < floats2.length; i++) {
+			assertTrue(floats[i] == floats2[i]);
+		}
+	}
+
+	public void testInts2Bytes() throws Exception {
+		int[] ints = new int[] { 1, 2, 3, 4, 1500 };
+		byte[] intBytes = ByteUtils.intsToBytes(ints);
+		int[] ints2 = ByteUtils.bytesToInts(intBytes);
+		assertTrue(ints.length == ints2.length);
+		for (int i = 0; i < ints2.length; i++) {
+			assertTrue(ints[i] == ints2[i]);
+		}
+	}
+}
