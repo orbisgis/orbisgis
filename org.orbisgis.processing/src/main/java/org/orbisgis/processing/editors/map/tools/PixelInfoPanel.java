a2a4ff2a00f67a2ab8299052a862a77a08efc356
ebocher <ebocher@irstv>
2008-06-02 13:06:43 +0000

    PLAN #4082 : refactoring convention names

---
+++ org.orbisgis.processing/src/main/java/org/orbisgis/processing/editors/map/tools/PixelInfoPanel.java
@@ -0,0 +1,68 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at french IRSTV institute and is able
+ * to manipulate and create vectorial and raster spatial information. OrbisGIS
+ * is distributed under GPL 3 license. It is produced  by the geomatic team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/>, CNRS FR 2488:
+ *    Erwan BOCHER, scientific researcher,
+ *    Thomas LEDUC, scientific researcher,
+ *    Fernando GONZALEZ CORTES, computer engineer.
+ *
+ * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult:
+ *    <http://orbisgis.cerma.archi.fr/>
+ *    <http://sourcesup.cru.fr/projects/orbisgis/>
+ *    <http://listes.cru.fr/sympa/info/orbisgis-developers/>
+ *    <http://listes.cru.fr/sympa/info/orbisgis-users/>
+ *
+ * or contact directly:
+ *    erwan.bocher _at_ ec-nantes.fr
+ *    fergonco _at_ gmail.com
+ *    thomas.leduc _at_ cerma.archi.fr
+ */
+package org.orbisgis.processing.editors.map.tools;
+
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PixelInfoPanel extends JPanel {
+	private JLabel[] valuesLabels;
+
+	public PixelInfoPanel(final String[] labels) {
+		final GridLayout layout = new GridLayout();
+		setLayout(layout);
+		layout.setColumns(2);
+		layout.setRows(labels.length);
+		valuesLabels = new JLabel[labels.length];
+
+		for (int i = 0; i < labels.length; i++) {
+			add(new JLabel(labels[i]));
+			valuesLabels[i] = new JLabel("-");
+			add(valuesLabels[i]);
+		}
+	}
+
+	public void setValues(final Object[] values) {
+		for (int i = 0; i < valuesLabels.length; i++) {
+			valuesLabels[i].setText(values[i].toString());
+		}
+	}
+}
