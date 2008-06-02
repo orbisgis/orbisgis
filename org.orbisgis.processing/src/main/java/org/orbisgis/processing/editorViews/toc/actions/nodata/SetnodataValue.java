0618fa0ad52a59fc480deb09bc94e766c718e677
ebocher <ebocher@irstv>
2008-06-02 13:59:40 +0000

    PLAN #4107 nodatapanel

---
+++ org.orbisgis.processing/src/main/java/org/orbisgis/processing/editorViews/toc/actions/nodata/SetnodataValue.java
@@ -0,0 +1,99 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at french IRSTV institute and is able
+ * to manipulate and create vectorial and raster spatial information. OrbisGIS
+ * is distributed under GPL 3 license. It is produced  by the geomatic team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/>, CNRS FR 2488:
+ *    Erwan BOCHER, scientific researcher,
+ *    Thomas LEDUC, scientific researcher,
+ *    Fernando GONZALEZ CORTES, computer engineer.
+ *
+ * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult:
+ *    <http://orbisgis.cerma.archi.fr/>
+ *    <http://sourcesup.cru.fr/projects/orbisgis/>
+ *    <http://listes.cru.fr/sympa/info/orbisgis-developers/>
+ *    <http://listes.cru.fr/sympa/info/orbisgis-users/>
+ *
+ * or contact directly:
+ *    erwan.bocher _at_ ec-nantes.fr
+ *    fergonco _at_ gmail.com
+ *    thomas.leduc _at_ cerma.archi.fr
+ */
+package org.orbisgis.processing.editorViews.toc.actions.nodata;
+
+import java.io.IOException;
+
+import org.gdms.driver.DriverException;
+import org.grap.model.GeoRaster;
+import org.orbisgis.Services;
+import org.orbisgis.layerModel.ILayer;
+import org.orbisgis.layerModel.MapContext;
+import org.sif.UIFactory;
+import org.sif.multiInputPanel.DoubleType;
+import org.sif.multiInputPanel.MultiInputPanel;
+
+public class SetnodataValue implements
+		org.orbisgis.editorViews.toc.action.ILayerAction {
+
+	
+
+	public boolean accepts(ILayer layer) {
+		try {
+			return layer.isRaster();
+		} catch (DriverException e) {
+			return false;
+		}
+	}
+
+	public boolean acceptsSelectionCount(int selectionCount) {
+		return selectionCount == 1;
+	}
+
+	public void execute(MapContext mapContext, ILayer layer) {
+
+		
+		try {
+			GeoRaster geoRasterSrc = layer.getRaster();
+		
+		
+		final MultiInputPanel mip = new MultiInputPanel("Set nodatavalue");
+		mip.addInput("MinValue", "Min value", new Double(geoRasterSrc.getMin())
+				.toString(), new DoubleType());
+		mip.addInput("MaxValue", "Max value", new Double(geoRasterSrc.getMax())
+				.toString(), new DoubleType());
+		mip.addInput("NodataValue", "Nodata value", new Double(geoRasterSrc
+				.getNoDataValue()).toString(), new DoubleType());
+
+		if (UIFactory.showDialog(mip)) {
+			final double nodata = new Double(mip.getInput("NodataValue"));
+
+			geoRasterSrc.setNodataValue((float) nodata);
+
+		}
+		} catch (DriverException e) {
+			Services.getErrorManager().error(
+					"Cannot read the raster from the layer ", e);
+		} catch (IOException e) {
+			Services.getErrorManager().error(
+					"Cannot compute " + layer.getName(), e);
+		}
+
+	}
+}
