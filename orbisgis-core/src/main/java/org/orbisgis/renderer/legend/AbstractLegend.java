1da011b44964bd01b67f545650689bf98eb9a061
fernando <fernando@irstv>
2008-06-12 10:19:55 +0000

    NPU #4187: Removed methods

--- orbisgis-core/src/main/java/org/orbisgis/renderer/legend/AbstractLegend.java
+++ orbisgis-core/src/main/java/org/orbisgis/renderer/legend/AbstractLegend.java
@@ -6,7 +6,6 @@ import org.gdms.driver.DriverException;
 public abstract class AbstractLegend implements Legend {
 
 	private SpatialDataSourceDecorator sds;
-	private int layer;
 	private String name;
 
 	public void setDataSource(SpatialDataSourceDecorator ds)
@@ -16,18 +15,6 @@ public abstract class AbstractLegend implements Legend {
 
 	public SpatialDataSourceDecorator getDataSource() {
 		return sds;
-	}
-
-	public Legend[] getLegends() {
-		return new Legend[0];
-	}
-
-	public void setLayer(int i) {
-		this.layer = i;
-	}
-
-	protected int getLayer() {
-		return layer;
 	}
 
 	public String getName() {
