d24ffffeef92a3e354ce3de5fb1238c0a386dfcb
fernando <fernando@irstv>
2008-07-27 10:44:32 +0000

    Some improvement to fast booting OG from eclipse

--- plugin-manager/src/main/java/org/orbisgis/pluginManager/EclipseProjectReader.java
+++ plugin-manager/src/main/java/org/orbisgis/pluginManager/EclipseProjectReader.java
@@ -39,6 +39,7 @@ package org.orbisgis.pluginManager;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.ximpleware.EOFException;
 import com.ximpleware.EncodingException;
@@ -49,6 +50,9 @@ import com.ximpleware.xpath.XPathEvalException;
 import com.ximpleware.xpath.XPathParseException;
 
 public class EclipseProjectReader implements PluginClassPathReader {
+
+	private HashMap<File, File[]> pluginDirJars = new HashMap<File, File[]>();
+	private HashMap<File, File[]> pluginDirOutputFolders = new HashMap<File, File[]>();
 
 	private static final String CLASSPATH_CLASSPATHENTRY = "/classpath/classpathentry";
 
@@ -65,161 +69,179 @@ public class EclipseProjectReader implements PluginClassPathReader {
 	}
 
 	public File[] getJars(File pluginDir) {
-		File classpathFile = new File(pluginDir, ".classpath");
-		ArrayList<File> ret = new ArrayList<File>();
-		try {
-			VTD vtd = new VTD(classpathFile);
-			int n = vtd.evalToInt("count(" + CLASSPATH_CLASSPATHENTRY
-					+ "[(@kind='var') or (@kind='lib')])");
-			for (int i = 0; i < n; i++) {
-				String attribute = vtd.getAttribute(CLASSPATH_CLASSPATHENTRY
-						+ "[(@kind='var') or (@kind='lib')][" + (i + 1) + "]",
-						"path");
-				if (attribute.contains("M2_REPO")) {
-					String mavenRepo = System.getProperty("user.home")
-							.replaceAll("\\Q\\\\E", "/")
-							+ "/.m2/repository/";
-					if (!new File(mavenRepo).exists()) {
-						throw new RuntimeException(
-								"Cannot work with a different M2_REPO than default");
+		File[] files = pluginDirJars.get(pluginDir);
+		if (files == null) {
+			File classpathFile = new File(pluginDir, ".classpath");
+			ArrayList<File> ret = new ArrayList<File>();
+			try {
+				VTD vtd = new VTD(classpathFile);
+				int n = vtd.evalToInt("count(" + CLASSPATH_CLASSPATHENTRY
+						+ "[(@kind='var') or (@kind='lib')])");
+				for (int i = 0; i < n; i++) {
+					String attribute = vtd.getAttribute(
+							CLASSPATH_CLASSPATHENTRY
+									+ "[(@kind='var') or (@kind='lib')]["
+									+ (i + 1) + "]", "path");
+					if (attribute.contains("M2_REPO")) {
+						String mavenRepo = System.getProperty("user.home")
+								.replaceAll("\\Q\\\\E", "/")
+								+ "/.m2/repository/";
+						if (!new File(mavenRepo).exists()) {
+							throw new RuntimeException(
+									"Cannot work with a different M2_REPO than default");
+						}
+						attribute = attribute.replaceAll("\\QM2_REPO\\E",
+								mavenRepo);
 					}
-					attribute = attribute
-							.replaceAll("\\QM2_REPO\\E", mavenRepo);
+					ret.add(new File(attribute));
 				}
-				ret.add(new File(attribute));
-			}
-			n = vtd.evalToInt("count(" + CLASSPATH_CLASSPATHENTRY
-					+ "[@kind='src'])");
-			for (int i = 0; i < n; i++) {
-				String attribute = vtd.getAttribute(CLASSPATH_CLASSPATHENTRY
-						+ "[@kind='src'][" + (i + 1) + "]", "path");
-				if (attribute.startsWith("/")) {
-					// remove the '/' from the beginning
-					attribute = attribute.substring(attribute.lastIndexOf('/'));
-					File linkedProject = new File(pluginDir.getParentFile(),
-							attribute);
-					if (!linkedProject.exists()) {
-						throw new RuntimeException(
-								"Cannot find project related to "
-										+ pluginDir.getAbsolutePath()
-										+ ": "
-										+ attribute
-										+ ". They have to be under the same directory.");
-					}
-					File[] jars = getJars(linkedProject);
-					for (File url : jars) {
-						ret.add(url);
+				n = vtd.evalToInt("count(" + CLASSPATH_CLASSPATHENTRY
+						+ "[@kind='src'])");
+				for (int i = 0; i < n; i++) {
+					String attribute = vtd.getAttribute(
+							CLASSPATH_CLASSPATHENTRY + "[@kind='src']["
+									+ (i + 1) + "]", "path");
+					if (attribute.startsWith("/")) {
+						// remove the '/' from the beginning
+						attribute = attribute.substring(attribute
+								.lastIndexOf('/'));
+						File linkedProject = new File(
+								pluginDir.getParentFile(), attribute);
+						if (!linkedProject.exists()) {
+							throw new RuntimeException(
+									"Cannot find project related to "
+											+ pluginDir.getAbsolutePath()
+											+ ": "
+											+ attribute
+											+ ". They have to be under the same directory.");
+						}
+						File[] jars = getJars(linkedProject);
+						for (File url : jars) {
+							ret.add(url);
+						}
 					}
 				}
-			}
 
-			return ret.toArray(new File[0]);
-		} catch (EncodingException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ pluginDir.getAbsolutePath(), e);
-		} catch (EOFException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ pluginDir.getAbsolutePath(), e);
-		} catch (EntityException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ pluginDir.getAbsolutePath(), e);
-		} catch (ParseException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ pluginDir.getAbsolutePath(), e);
-		} catch (IOException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ pluginDir.getAbsolutePath(), e);
-		} catch (XPathParseException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ pluginDir.getAbsolutePath(), e);
-		} catch (XPathEvalException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ pluginDir.getAbsolutePath(), e);
-		} catch (NavException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ pluginDir.getAbsolutePath(), e);
+				files = ret.toArray(new File[0]);
+			} catch (EncodingException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ pluginDir.getAbsolutePath(), e);
+			} catch (EOFException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ pluginDir.getAbsolutePath(), e);
+			} catch (EntityException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ pluginDir.getAbsolutePath(), e);
+			} catch (ParseException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ pluginDir.getAbsolutePath(), e);
+			} catch (IOException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ pluginDir.getAbsolutePath(), e);
+			} catch (XPathParseException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ pluginDir.getAbsolutePath(), e);
+			} catch (XPathEvalException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ pluginDir.getAbsolutePath(), e);
+			} catch (NavException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ pluginDir.getAbsolutePath(), e);
+			}
 		}
+
+		pluginDirJars.put(pluginDir, files);
+		return files;
 	}
 
 	public File[] getOutputFolders(File projectDir) {
-		File classpathFile = new File(projectDir, ".classpath");
-		ArrayList<File> ret = new ArrayList<File>();
-		try {
-			VTD vtd = new VTD(classpathFile);
-			int n = vtd.evalToInt("count(" + CLASSPATH_CLASSPATHENTRY
-					+ "[@kind='output'])");
-			for (int i = 0; i < n; i++) {
-				String attribute = vtd.getAttribute(CLASSPATH_CLASSPATHENTRY
-						+ "[@kind='output'][" + (i + 1) + "]", "path");
-				ret.add(new File(projectDir, attribute));
-			}
+		File[] files = pluginDirOutputFolders.get(projectDir);
+		if (files == null) {
+			File classpathFile = new File(projectDir, ".classpath");
+			ArrayList<File> ret = new ArrayList<File>();
+			try {
+				VTD vtd = new VTD(classpathFile);
+				int n = vtd.evalToInt("count(" + CLASSPATH_CLASSPATHENTRY
+						+ "[@kind='output'])");
+				for (int i = 0; i < n; i++) {
+					String attribute = vtd.getAttribute(
+							CLASSPATH_CLASSPATHENTRY + "[@kind='output']["
+									+ (i + 1) + "]", "path");
+					ret.add(new File(projectDir, attribute));
+				}
 
-			n = vtd.evalToInt("count(" + CLASSPATH_CLASSPATHENTRY
-					+ "[@kind='src'])");
-			for (int i = 0; i < n; i++) {
-				String attribute = vtd.getAttribute(CLASSPATH_CLASSPATHENTRY
-						+ "[@kind='src'][" + (i + 1) + "]", "path");
-				if (attribute.startsWith("/")) {
-					// remove the '/' from the beginning
-					attribute = attribute.substring(attribute.lastIndexOf('/'));
-					File linkedProject = new File(projectDir.getParentFile(),
-							attribute);
-					if (!linkedProject.exists()) {
-						throw new RuntimeException(
-								"Cannot find project related to "
-										+ projectDir.getAbsolutePath()
-										+ ": "
-										+ attribute
-										+ ". They have to be under the same directory.");
-					}
-					File[] outputDirs = getOutputFolders(linkedProject);
-					for (File file : outputDirs) {
-						ret.add(file);
+				n = vtd.evalToInt("count(" + CLASSPATH_CLASSPATHENTRY
+						+ "[@kind='src'])");
+				for (int i = 0; i < n; i++) {
+					String attribute = vtd.getAttribute(
+							CLASSPATH_CLASSPATHENTRY + "[@kind='src']["
+									+ (i + 1) + "]", "path");
+					if (attribute.startsWith("/")) {
+						// remove the '/' from the beginning
+						attribute = attribute.substring(attribute
+								.lastIndexOf('/'));
+						File linkedProject = new File(projectDir
+								.getParentFile(), attribute);
+						if (!linkedProject.exists()) {
+							throw new RuntimeException(
+									"Cannot find project related to "
+											+ projectDir.getAbsolutePath()
+											+ ": "
+											+ attribute
+											+ ". They have to be under the same directory.");
+						}
+						File[] outputDirs = getOutputFolders(linkedProject);
+						for (File file : outputDirs) {
+							ret.add(file);
+						}
 					}
 				}
-			}
 
-			return ret.toArray(new File[0]);
-		} catch (EncodingException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ projectDir.getAbsolutePath(), e);
-		} catch (EOFException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ projectDir.getAbsolutePath(), e);
-		} catch (EntityException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ projectDir.getAbsolutePath(), e);
-		} catch (ParseException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ projectDir.getAbsolutePath(), e);
-		} catch (IOException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ projectDir.getAbsolutePath(), e);
-		} catch (XPathParseException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ projectDir.getAbsolutePath(), e);
-		} catch (XPathEvalException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ projectDir.getAbsolutePath(), e);
-		} catch (NavException e) {
-			throw new RuntimeException(
-					"Cannot understand plugin of type 'eclipse':"
-							+ projectDir.getAbsolutePath(), e);
+				files = ret.toArray(new File[0]);
+			} catch (EncodingException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ projectDir.getAbsolutePath(), e);
+			} catch (EOFException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ projectDir.getAbsolutePath(), e);
+			} catch (EntityException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ projectDir.getAbsolutePath(), e);
+			} catch (ParseException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ projectDir.getAbsolutePath(), e);
+			} catch (IOException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ projectDir.getAbsolutePath(), e);
+			} catch (XPathParseException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ projectDir.getAbsolutePath(), e);
+			} catch (XPathEvalException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ projectDir.getAbsolutePath(), e);
+			} catch (NavException e) {
+				throw new RuntimeException(
+						"Cannot understand plugin of type 'eclipse':"
+								+ projectDir.getAbsolutePath(), e);
+			}
 		}
+
+		pluginDirOutputFolders.put(projectDir, files);
+		return files;
 	}
 }
