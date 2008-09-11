84f473e0bc5f8dab19243895ebda4c6d5c7c0990
fernando <fernando@irstv>
2008-09-11 08:35:30 +0000

    PLAN 4474: Export map action enabled

--- org.orbisgis.core-ui/src/main/java/org/orbisgis/editors/map/actions/ExportMapAsPDF.java
+++ org.orbisgis.core-ui/src/main/java/org/orbisgis/editors/map/actions/ExportMapAsPDF.java
@@ -36,32 +36,20 @@
  */
 package org.orbisgis.editors.map.actions;
 
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
-import javax.swing.JOptionPane;
-
+import org.gdms.driver.DriverException;
 import org.orbisgis.Services;
 import org.orbisgis.editor.IEditor;
 import org.orbisgis.editor.action.IEditorAction;
 import org.orbisgis.editors.map.MapEditor;
-import org.orbisgis.layerModel.ILayer;
 import org.orbisgis.layerModel.MapContext;
+import org.orbisgis.map.export.MapExportManager;
 import org.orbisgis.pluginManager.ui.SaveFilePanel;
-import org.orbisgis.progress.NullProgressMonitor;
-import org.orbisgis.renderer.Renderer;
-import org.orbisgis.renderer.legend.Legend;
 import org.sif.UIFactory;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.PageSize;
-import com.lowagie.text.pdf.PdfContentByte;
-import com.lowagie.text.pdf.PdfTemplate;
-import com.lowagie.text.pdf.PdfWriter;
 import com.vividsolutions.jts.geom.Envelope;
 
 /**
@@ -76,12 +64,28 @@ public class ExportMapAsPDF implements IEditorAction {
 		MapEditor mapEditor = (MapEditor) editor;
 		MapContext mc = (MapContext) editor.getElement().getObject();
 
-		ILayer root = mc.getLayerModel();
+		MapExportManager mem = Services.getService(MapExportManager.class);
 		Envelope envelope = mapEditor.getMapTransform().getAdjustedExtent();
+		final SaveFilePanel outfilePanel = new SaveFilePanel(
+				"org.orbisgis.editors.map.actions.ExportMapAsPDF",
+				"Choose a file format");
+		outfilePanel.addFilter("svg", "Scalable Vector Graphics (*.svg)");
 
-		BufferedImage img = mapEditor.getMapTransform().getImage();
-		save(img, envelope, root);
-
+		if (UIFactory.showDialog(outfilePanel)) {
+			try {
+				mem.exportSVG(mc, new FileOutputStream(outfilePanel
+						.getSelectedFile()), 500, 500, envelope);
+			} catch (UnsupportedEncodingException e) {
+				Services.getErrorManager().error("Cannot export", e);
+			} catch (IllegalArgumentException e) {
+				Services.getErrorManager().error("Cannot export", e);
+			} catch (IOException e) {
+				Services.getErrorManager().error(
+						"Cannot write to the selected file", e);
+			} catch (DriverException e) {
+				Services.getErrorManager().error("Cannot access the map", e);
+			}
+		}
 	}
 
 	public boolean isEnabled(IEditor editor) {
@@ -91,118 +95,5 @@ public class ExportMapAsPDF implements IEditorAction {
 
 	public boolean isVisible(IEditor editor) {
 		return true;
-	}
-
-	/**
-	 * TODO take into account raster with nice resolution
-	 * 
-	 * @param layerPixelEnvelope
-	 * @param envelope
-	 * @param layer
-	 */
-	public void save(BufferedImage img, Envelope envelope, ILayer layer) {
-		int width = img.getWidth();
-		int height = img.getHeight();
-		Document document = new Document(PageSize.A4.rotate());
-
-		try {
-
-			Renderer r = new Renderer();
-
-			final SaveFilePanel outfilePanel = new SaveFilePanel(
-					"org.orbisgis.editors.map.actions.ExportMapAsPDF",
-					"Choose a file format");
-			outfilePanel.addFilter("pdf", "Portable Document Format (*.pdf)");
-
-			if (UIFactory.showDialog(outfilePanel)) {
-				final File savedFile = new File(outfilePanel.getSelectedFile()
-						.getAbsolutePath());
-
-				if (savedFile.getName().toLowerCase().endsWith("pdf")) {
-
-					FileOutputStream fos = new FileOutputStream(savedFile);
-
-					PdfWriter writer = PdfWriter.getInstance(document, fos);
-
-					document.open();
-
-					PdfContentByte cb = writer.getDirectContent();
-
-					PdfTemplate tp = cb.createTemplate(document.getPageSize()
-							.getWidth(), document.getPageSize().getHeight());
-
-					Graphics2D g2d = tp.createGraphicsShapes(width, height);
-
-					r.draw(g2d, width, height, envelope, layer,
-							new NullProgressMonitor());
-					g2d.dispose();
-
-					cb.addTemplate(tp, 0, height);
-					PdfTemplate vectorTP = cb.createTemplate(width, height);
-
-					Graphics2D vectorg2d = vectorTP.createGraphicsShapes(width,
-							height);
-
-					ILayer[] layers = layer.getLayersRecursively();
-
-					for (int i = 0; i < layers.length; i++) {
-						if (layers[i].isVisible()) {
-							if (layers[i].isVectorial()) {
-								Legend[] vectorLegends = layers[i]
-										.getVectorLegend();
-								java.awt.Font font = new java.awt.Font("arial",
-										0, 12);
-								vectorg2d.setFont(font);
-								vectorg2d.drawString(layers[i].getName(), 0, 0);
-								for (int j = 0; j < vectorLegends.length; j++) {
-									Legend vectorLegend = vectorLegends[j];
-									vectorLegend.drawImage(vectorg2d);
-									int[] size = vectorLegend
-											.getImageSize(vectorg2d);
-									vectorg2d.translate(0, size[1]);
-
-								}
-
-							} else if (layers[i].isRaster()) {
-
-								// TODO generate a image for LUT with min and
-								// max
-								/*
-								 * RasterLegend[] rastersLegend =
-								 * layers[i].getRasterLegend();
-								 * 
-								 * for (int j = 0; j < rastersLegend.length;
-								 * j++) {
-								 * 
-								 * final LutDisplay lutDisplay = new
-								 * LutDisplay(rastersLegend[j].getColorModel());
-								 * 
-								 * vectorg2d.drawImage(lutDisplay.getImagePlus().getImage(),0,0,
-								 * null); }
-								 */
-
-							}
-						}
-					}
-					vectorg2d.dispose();
-
-					cb.addTemplate(vectorTP, 0, height);
-
-					JOptionPane.showMessageDialog(null,
-							"The file has been saved.");
-				}
-
-			}
-
-		} catch (FileNotFoundException e) {
-			Services.getErrorManager().error("Cannot write on the disk", e);
-		} catch (DocumentException e) {
-			Services.getErrorManager().error("Cannot write the PDF", e);
-		} catch (Exception e) {
-			Services.getErrorManager().error("Cannot export in PDF", e);
-		}
-
-		document.close();
-
 	}
 }
