0d67ffc53a4df59b52f85605c1f35cb667f01ae9
ebocher <ebocher@irstv>
2008-10-17 08:51:25 +0000

    [#4618] OpenJUMP featuremodel adapter Schema test

--- contrib/src/test/java/org/contrib/model/jump/JUMPModelTest.java
+++ contrib/src/test/java/org/contrib/model/jump/JUMPModelTest.java
@@ -3,6 +3,7 @@ package org.contrib.model.jump;
 import java.io.File;
 
 import org.contrib.model.jump.adapter.FeatureCollectionAdapter;
+import org.contrib.model.jump.model.FeatureSchema;
 import org.gdms.data.DataSource;
 import org.gdms.data.DataSourceFactory;
 import org.gdms.data.SpatialDataSourceDecorator;
@@ -23,9 +24,10 @@ public class JUMPModelTest extends TestCase {
 
 		FeatureCollectionAdapter featureCollectionAdapter = new FeatureCollectionAdapter(
 				sds);
-		
-		assertTrue(featureCollectionAdapter.size()==sds.getRowCount());
-		assertTrue(featureCollectionAdapter.getFeatureSchema().getAttributeCount()==sds.getMetadata().getFieldCount());
+
+		assertTrue(featureCollectionAdapter.size() == sds.getRowCount());
+		assertTrue(featureCollectionAdapter.getFeatureSchema()
+				.getAttributeCount() == sds.getMetadata().getFieldCount());
 
 	}
 
@@ -43,6 +45,27 @@ public class JUMPModelTest extends TestCase {
 				sds.getFullExtent()));
 
 		sds.close();
+
+	}
+
+	public void testSchema() throws Exception {
+
+		DataSource mydata = dsf.getDataSource(new File(path));
+
+		SpatialDataSourceDecorator sds = new SpatialDataSourceDecorator(mydata);
+		sds.open();
+
+		FeatureCollectionAdapter featureCollectionAdapter = new FeatureCollectionAdapter(
+				sds);
+
+		FeatureSchema fs = featureCollectionAdapter.getFeatureSchema();
+
+		for (int i = 0; i < fs.getAttributeCount(); i++) {
+
+			assertTrue(fs.getAttributeName(i).equals(
+					sds.getMetadata().getFieldName(i)));
+
+		}
 
 	}
 
