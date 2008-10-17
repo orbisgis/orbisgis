5afde8fa4d5094ed107ba9e224f3c3791417e2f2
ebocher <ebocher@irstv>
2008-10-17 12:52:36 +0000

    [#4618] OpenJUMP featuremodel adapter spatial index test

--- contrib/src/test/java/org/contrib/model/jump/JUMPModelTest.java
+++ contrib/src/test/java/org/contrib/model/jump/JUMPModelTest.java
@@ -1,13 +1,19 @@
 package org.contrib.model.jump;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import org.contrib.model.jump.adapter.FeatureCollectionAdapter;
 import org.contrib.model.jump.model.Feature;
 import org.contrib.model.jump.model.FeatureSchema;
+import org.contrib.model.jump.model.IndexedFeatureCollection;
 import org.gdms.data.DataSource;
 import org.gdms.data.DataSourceFactory;
 import org.gdms.data.SpatialDataSourceDecorator;
+
+import com.vividsolutions.jts.geom.Geometry;
 
 import junit.framework.TestCase;
 
@@ -88,6 +94,42 @@ public class JUMPModelTest extends TestCase {
 					sds.getGeometry(sds.getDefaultGeometry(), i)));
 
 		}
+
+	}
+
+	public void testIndexedFeatureCollection() throws Exception {
+
+		DataSource mydata = dsf.getDataSource(new File(path));
+
+		SpatialDataSourceDecorator sds = new SpatialDataSourceDecorator(mydata);
+		sds.open();
+
+		FeatureCollectionAdapter featureCollectionAdapter = new FeatureCollectionAdapter(
+				sds);
+
+		IndexedFeatureCollection indexedFeatureCollection = new IndexedFeatureCollection(
+				featureCollectionAdapter);
+
+		Geometry sdsGeom = sds.getGeometry(10);
+
+		System.out.println(sdsGeom);
+	
+		boolean result = false;
+
+		for (Iterator j = indexedFeatureCollection.query(
+				sdsGeom.getEnvelopeInternal()).iterator(); j.hasNext();) {
+			Feature featureResult = (Feature) j.next();
+
+			if (featureResult.getGeometry().equals(sdsGeom)) {
+				result = true;
+
+				System.out.println(featureResult.getGeometry());
+
+			}
+
+		}
+
+		assertTrue(result);
 
 	}
 
