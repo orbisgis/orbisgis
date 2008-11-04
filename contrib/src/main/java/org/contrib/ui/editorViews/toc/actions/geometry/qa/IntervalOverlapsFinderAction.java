c20f652ad90e7db3ddd264853518acfe75b8c2c8
ebocher <ebocher@irstv>
2008-11-04 11:23:02 +0000

    #7 task	Internal gap finder better background job

--- contrib/src/main/java/org/contrib/ui/editorViews/toc/actions/geometry/qa/IntervalOverlapsFinderAction.java
+++ contrib/src/main/java/org/contrib/ui/editorViews/toc/actions/geometry/qa/IntervalOverlapsFinderAction.java
@@ -1,5 +1,8 @@
 package org.contrib.ui.editorViews.toc.actions.geometry.qa;
 
+import java.awt.Color;
+
+import org.contrib.algorithm.qa.InternalGapFinder;
 import org.contrib.model.jump.adapter.FeatureCollectionAdapter;
 import org.contrib.model.jump.adapter.FeatureCollectionDatasourceAdapter;
 import org.contrib.model.jump.adapter.TaskMonitorAdapter;
@@ -15,6 +18,10 @@ import org.orbisgis.editorViews.toc.action.ILayerAction;
 import org.orbisgis.layerModel.ILayer;
 import org.orbisgis.layerModel.LayerException;
 import org.orbisgis.layerModel.MapContext;
+import org.orbisgis.outputManager.OutputManager;
+import org.orbisgis.pluginManager.background.BackgroundJob;
+import org.orbisgis.pluginManager.background.BackgroundManager;
+import org.orbisgis.progress.IProgressMonitor;
 
 import com.vividsolutions.jcs.qa.InternalOverlapFinder;
 
@@ -37,72 +44,121 @@ public class IntervalOverlapsFinderAction implements ILayerAction {
 
 	public void execute(MapContext mapContext, ILayer layer) {
 
-		DataManager dataManager = (DataManager) Services
-				.getService(DataManager.class);
+		BackgroundManager bm = (BackgroundManager) Services
+				.getService(BackgroundManager.class);
+		bm.backgroundOperation(new ExecuteProcessing(mapContext, layer));
 
-		final DataSourceFactory dsf = dataManager.getDSF();
+	}
 
-		FeatureCollection fc = new FeatureCollectionAdapter(
-				new SpatialDataSourceDecorator(layer.getDataSource()));
-		InternalOverlapFinder internalOverlapFinder = new InternalOverlapFinder(
-				fc, new TaskMonitorAdapter());
+	private class ExecuteProcessing implements BackgroundJob {
 
-		internalOverlapFinder.computeOverlaps();
+		private ILayer layer;
 
-		FeatureCollection overlapingFeatures = internalOverlapFinder
-				.getOverlappingFeatures();
+		private MapContext mapContext;
 
-		FeatureCollection overlapIndicators = internalOverlapFinder
-				.getOverlapIndicators();
-
-		FeatureCollection overlapSizeIndicators = internalOverlapFinder
-				.getOverlapSizeIndicators();
-
-		try {
-			ObjectMemoryDriver overlapingFeaturesdriver = new ObjectMemoryDriver(
-					new FeatureCollectionDatasourceAdapter(overlapingFeatures));
-			String overlapingFeatureslayer = dsf.getSourceManager()
-					.nameAndRegister(overlapingFeaturesdriver);
-
-			ObjectMemoryDriver overlapingIndicatorsdriver = new ObjectMemoryDriver(
-					new FeatureCollectionDatasourceAdapter(overlapIndicators));
-			String overlapingIndicatorslayer = dsf.getSourceManager()
-					.nameAndRegister(overlapingIndicatorsdriver);
-
-			ObjectMemoryDriver overlapingSizeIndicatorsdriver = new ObjectMemoryDriver(
-					new FeatureCollectionDatasourceAdapter(
-							overlapSizeIndicators));
-			String overlapingSizeIndicatorslayer = dsf.getSourceManager()
-					.nameAndRegister(overlapingSizeIndicatorsdriver);
-
-			final ILayer overlapingFeaturesLayer = dataManager
-					.createLayer(overlapingFeatureslayer);
-
-			final ILayer overlapingSizeIndicatorsLayer = dataManager
-					.createLayer(overlapingSizeIndicatorslayer);
-			final ILayer overlapingIndicatorsLayer = dataManager
-					.createLayer(overlapingIndicatorslayer);
-			mapContext.getLayerModel().insertLayer(overlapingFeaturesLayer, 0);
-			mapContext.getLayerModel()
-					.insertLayer(overlapingIndicatorsLayer, 0);
-			mapContext.getLayerModel().insertLayer(
-					overlapingSizeIndicatorsLayer, 0);
-
-		} catch (DriverLoadException e) {
-			Services.getErrorManager().error(
-					"Cannot create the resulting layer of geometry type ", e);
-		} catch (IllegalStateException e) {
-			Services.getErrorManager().error("Cannot get the layer ", e);
-		} catch (LayerException e) {
-			Services.getErrorManager()
-					.error(
-							"Cannot insert resulting layer based on "
-									+ layer.getName(), e);
-		} catch (DriverException e) {
-			Services.getErrorManager().error(
-					"Cannot read the resulting datasource from the layer ", e);
+		public ExecuteProcessing(MapContext mapContext, ILayer layer) {
+			this.mapContext = mapContext;
+			this.layer = layer;
 		}
 
+		public String getTaskName() {
+			return "Overlaps finders";
+		}
+
+		public void run(IProgressMonitor pm) {
+
+			DataManager dataManager = (DataManager) Services
+					.getService(DataManager.class);
+
+			final DataSourceFactory dsf = dataManager.getDSF();
+
+			FeatureCollection fc = new FeatureCollectionAdapter(
+					new SpatialDataSourceDecorator(layer.getDataSource()));
+			InternalOverlapFinder internalOverlapFinder = new InternalOverlapFinder(
+					fc, pm);
+
+			internalOverlapFinder.computeOverlaps();
+
+			FeatureCollection overlapingFeatures = internalOverlapFinder
+					.getOverlappingFeatures();
+
+			FeatureCollection overlapIndicators = internalOverlapFinder
+					.getOverlapIndicators();
+
+			FeatureCollection overlapSizeIndicators = internalOverlapFinder
+					.getOverlapSizeIndicators();
+
+			try {
+				if (overlapingFeatures.size() > 0) {
+					ObjectMemoryDriver overlapingFeaturesdriver = new ObjectMemoryDriver(
+							new FeatureCollectionDatasourceAdapter(
+									overlapingFeatures));
+					String overlapingFeatureslayer = dsf.getSourceManager()
+							.nameAndRegister(overlapingFeaturesdriver);
+
+					ObjectMemoryDriver overlapingIndicatorsdriver = new ObjectMemoryDriver(
+							new FeatureCollectionDatasourceAdapter(
+									overlapIndicators));
+					String overlapingIndicatorslayer = dsf.getSourceManager()
+							.nameAndRegister(overlapingIndicatorsdriver);
+
+					ObjectMemoryDriver overlapingSizeIndicatorsdriver = new ObjectMemoryDriver(
+							new FeatureCollectionDatasourceAdapter(
+									overlapSizeIndicators));
+					String overlapingSizeIndicatorslayer = dsf
+							.getSourceManager().nameAndRegister(
+									overlapingSizeIndicatorsdriver);
+
+					final ILayer overlapingFeaturesLayer = dataManager
+							.createLayer(overlapingFeatureslayer);
+
+					final ILayer overlapingSizeIndicatorsLayer = dataManager
+							.createLayer(overlapingSizeIndicatorslayer);
+					
+					final ILayer overlapingIndicatorsLayer = dataManager
+							.createLayer(overlapingIndicatorslayer);
+										
+					mapContext.getLayerModel().insertLayer(
+							overlapingFeaturesLayer, 0);
+					mapContext.getLayerModel().insertLayer(
+							overlapingIndicatorsLayer, 0);
+					mapContext.getLayerModel().insertLayer(
+							overlapingSizeIndicatorsLayer, 0);
+				}
+
+				else {
+					OutputManager om = (OutputManager) Services
+							.getService(OutputManager.class);
+					Color color = Color.black;
+					om.append("Report ----------------------------------"
+							+ "\n", color);
+
+					color = Color.red;
+					om.append("No overlaping geometries in the layer  : " + layer.getName() + "\n", color);
+					
+					color = Color.black;
+					om.append("----------------------------------" + "\n", color);
+					om.makeVisible();
+
+				}
+
+			} catch (DriverLoadException e) {
+				Services.getErrorManager().error(
+						"Cannot create the resulting layer of geometry type ",
+						e);
+			} catch (IllegalStateException e) {
+				Services.getErrorManager().error("Cannot get the layer ", e);
+			} catch (LayerException e) {
+				Services.getErrorManager().error(
+						"Cannot insert resulting layer based on "
+								+ layer.getName(), e);
+			} catch (DriverException e) {
+				Services.getErrorManager().error(
+						"Cannot read the resulting datasource from the layer ",
+						e);
+			}
+
+		}
 	}
 
 }
