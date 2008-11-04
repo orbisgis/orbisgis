8c43ecc28a896336bfd3ad2dd446a8f3a30df2df
ebocher <ebocher@irstv>
2008-11-04 10:36:23 +0000

    #7 task	Internal gap finder better background job

--- contrib/src/main/java/org/contrib/ui/editorViews/toc/actions/geometry/qa/IntervalGapsFinderAction.java
+++ contrib/src/main/java/org/contrib/ui/editorViews/toc/actions/geometry/qa/IntervalGapsFinderAction.java
@@ -12,8 +12,9 @@ import org.orbisgis.editorViews.toc.action.ILayerAction;
 import org.orbisgis.layerModel.ILayer;
 import org.orbisgis.layerModel.LayerException;
 import org.orbisgis.layerModel.MapContext;
+import org.orbisgis.pluginManager.background.BackgroundJob;
+import org.orbisgis.pluginManager.background.BackgroundManager;
 import org.orbisgis.progress.IProgressMonitor;
-import org.orbisgis.progress.ProgressMonitor;
 
 public class IntervalGapsFinderAction implements ILayerAction {
 
@@ -34,39 +35,61 @@ public class IntervalGapsFinderAction implements ILayerAction {
 
 	public void execute(MapContext mapContext, ILayer layer) {
 
-		DataManager dataManager = (DataManager) Services
-				.getService(DataManager.class);
-
-		final DataSourceFactory dsf = dataManager.getDSF();
-		
-		
-
-		InternalGapFinder internalGapFinder = new InternalGapFinder(
-		
-		new SpatialDataSourceDecorator(layer.getDataSource()));	
-
-
-		try {
-			ObjectMemoryDriver gapDriver = internalGapFinder
-					.getObjectMemoryDriver();
-
-			String gaplayer = dsf.getSourceManager().nameAndRegister(gapDriver);
-
-			final ILayer gapLayer = dataManager.createLayer(gaplayer);
-
-			mapContext.getLayerModel().insertLayer(gapLayer, 0);
-
-		} catch (DriverLoadException e) {
-			Services.getErrorManager().error(
-					"Cannot create the resulting layer of geometry type ", e);
-		} catch (IllegalStateException e) {
-			Services.getErrorManager().error("Cannot get the layer ", e);
-		} catch (LayerException e) {
-			Services.getErrorManager()
-					.error(
-							"Cannot insert resulting layer based on "
-									+ layer.getName(), e);
-		}
+		BackgroundManager bm = (BackgroundManager) Services
+				.getService(BackgroundManager.class);
+		bm.backgroundOperation(new ExecuteProcessing(mapContext, layer));
 
 	}
+
+	private class ExecuteProcessing implements BackgroundJob {
+
+		private ILayer layer;
+
+		private MapContext mapContext;
+
+		public ExecuteProcessing(MapContext mapContext, ILayer layer) {
+			this.mapContext = mapContext;
+			this.layer = layer;
+		}
+
+		public String getTaskName() {
+			return "Gaps finders";
+		}
+
+		public void run(IProgressMonitor pm) {
+
+			DataManager dataManager = (DataManager) Services
+					.getService(DataManager.class);
+
+			final DataSourceFactory dsf = dataManager.getDSF();
+
+			InternalGapFinder internalGapFinder = new InternalGapFinder(
+					new SpatialDataSourceDecorator(layer.getDataSource()), pm);
+
+			try {
+				ObjectMemoryDriver gapDriver = internalGapFinder
+						.getObjectMemoryDriver();
+
+				String gaplayer = dsf.getSourceManager().nameAndRegister(
+						gapDriver);
+
+				final ILayer gapLayer = dataManager.createLayer(gaplayer);
+
+				mapContext.getLayerModel().insertLayer(gapLayer, 0);
+
+			} catch (DriverLoadException e) {
+				Services.getErrorManager().error(
+						"Cannot create the resulting layer of geometry type ",
+						e);
+			} catch (IllegalStateException e) {
+				Services.getErrorManager().error("Cannot get the layer ", e);
+			} catch (LayerException e) {
+				Services.getErrorManager().error(
+						"Cannot insert resulting layer based on "
+								+ layer.getName(), e);
+			}
+
+		}
+	}
+
 }
