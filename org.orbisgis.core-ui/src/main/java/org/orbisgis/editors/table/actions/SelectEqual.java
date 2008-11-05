147e32b114828d247993f89415b8e6bf20b44490
fernando <fernando@irstv>
2008-11-05 12:28:42 +0000

    Select equal values in background process

--- org.orbisgis.core-ui/src/main/java/org/orbisgis/editors/table/actions/SelectEqual.java
+++ org.orbisgis.core-ui/src/main/java/org/orbisgis/editors/table/actions/SelectEqual.java
@@ -9,6 +9,9 @@ import org.orbisgis.Services;
 import org.orbisgis.editors.table.Selection;
 import org.orbisgis.editors.table.action.ITableCellAction;
 import org.orbisgis.errorManager.ErrorManager;
+import org.orbisgis.pluginManager.background.BackgroundJob;
+import org.orbisgis.pluginManager.background.BackgroundManager;
+import org.orbisgis.progress.IProgressMonitor;
 
 public class SelectEqual implements ITableCellAction {
 
@@ -19,27 +22,39 @@ public class SelectEqual implements ITableCellAction {
 	}
 
 	@Override
-	public void execute(DataSource dataSource, Selection selection,
-			int rowIndex, int columnIndex) {
-		try {
-			ArrayList<Integer> newSel = new ArrayList<Integer>();
-			Value ref = dataSource.getFieldValue(rowIndex, columnIndex);
-			for (int i = 0; i < dataSource.getRowCount(); i++) {
-				if (dataSource.getFieldValue(i, columnIndex).equals(ref)
-						.getAsBoolean()) {
-					newSel.add(i);
+	public void execute(final DataSource dataSource, final Selection selection,
+			final int rowIndex, final int columnIndex) {
+		BackgroundManager bm = Services.getService(BackgroundManager.class);
+		bm.backgroundOperation(new BackgroundJob() {
+
+			@Override
+			public void run(IProgressMonitor pm) {
+				try {
+					ArrayList<Integer> newSel = new ArrayList<Integer>();
+					Value ref = dataSource.getFieldValue(rowIndex, columnIndex);
+					for (int i = 0; i < dataSource.getRowCount(); i++) {
+						if (dataSource.getFieldValue(i, columnIndex)
+								.equals(ref).getAsBoolean()) {
+							newSel.add(i);
+						}
+					}
+					int[] sel = new int[newSel.size()];
+					for (int i = 0; i < sel.length; i++) {
+						sel[i] = newSel.get(i);
+					}
+
+					selection.setSelection(sel);
+				} catch (DriverException e) {
+					Services.getService(ErrorManager.class).error(
+							"Cannot read source", e);
 				}
 			}
-			int[] sel = new int[newSel.size()];
-			for (int i = 0; i < sel.length; i++) {
-				sel[i] = newSel.get(i);
-			}
 
-			selection.setSelection(sel);
-		} catch (DriverException e) {
-			Services.getService(ErrorManager.class).error("Cannot read source",
-					e);
-		}
+			@Override
+			public String getTaskName() {
+				return "finding matches";
+			}
+		});
 	}
 
 }
