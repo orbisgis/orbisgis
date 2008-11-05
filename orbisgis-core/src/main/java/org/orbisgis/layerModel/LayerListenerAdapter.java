7dc93a211106bd73352aad3e2497d197a3dc4865
fernando <fernando@irstv>
2008-11-05 12:27:17 +0000

    Added an adapter for those listener implementations that are only interested in one method

---
+++ orbisgis-core/src/main/java/org/orbisgis/layerModel/LayerListenerAdapter.java
@@ -0,0 +1,33 @@
+package org.orbisgis.layerModel;
+
+public abstract class LayerListenerAdapter implements LayerListener {
+
+	@Override
+	public void layerAdded(LayerCollectionEvent e) {
+	}
+
+	@Override
+	public void layerMoved(LayerCollectionEvent e) {
+	}
+
+	@Override
+	public void layerRemoved(LayerCollectionEvent e) {
+	}
+
+	@Override
+	public void nameChanged(LayerListenerEvent e) {
+	}
+
+	@Override
+	public void selectionChanged(SelectionEvent e) {
+	}
+
+	@Override
+	public void styleChanged(LayerListenerEvent e) {
+	}
+
+	@Override
+	public void visibilityChanged(LayerListenerEvent e) {
+	}
+
+}
