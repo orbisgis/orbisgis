60d42c8f9555fd856790f0126173af26cb3b6cf7
fernando <fernando@irstv>
2008-11-05 20:29:50 +0000

    Two selection tools. One for edition that deals with active layer and a general one that deals with the first selected layers (the same as info tool)

---
+++ org.orbisgis.core-ui/src/main/java/org/orbisgis/editors/map/tools/EditionSelectionTool.java
@@ -0,0 +1,24 @@
+package org.orbisgis.editors.map.tools;
+
+import org.orbisgis.editors.map.tool.ToolManager;
+import org.orbisgis.layerModel.ILayer;
+import org.orbisgis.layerModel.MapContext;
+
+public class EditionSelectionTool extends AbstractSelectionTool {
+
+	@Override
+	public boolean isEnabled(MapContext vc, ToolManager tm) {
+		return ToolValidationUtilities.isActiveLayerEditable(vc)
+				&& ToolValidationUtilities.isActiveLayerVisible(vc);
+	}
+
+	@Override
+	public boolean isVisible(MapContext vc, ToolManager tm) {
+		return isEnabled(vc, tm);
+	}
+
+	@Override
+	protected ILayer getLayer(MapContext mc) {
+		return mc.getActiveLayer();
+	}
+}
