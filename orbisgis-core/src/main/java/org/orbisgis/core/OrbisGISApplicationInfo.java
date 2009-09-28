870f36c2dff08cac16056974129bb2a1eb67ffc9
ebocher <ebocher@irstv>
2009-09-28 10:22:55 +0000

    New version 2.2 beta

--- orbisgis-core/src/main/java/org/orbisgis/core/OrbisGISApplicationInfo.java
+++ orbisgis-core/src/main/java/org/orbisgis/core/OrbisGISApplicationInfo.java
@@ -29,12 +29,12 @@ public class OrbisGISApplicationInfo implements ApplicationInfo {
 
 	@Override
 	public String getVersionName() {
-		return "Vienne";
+		return "Lyon";
 	}
 
 	@Override
 	public String getVersionNumber() {
-		return "2.1";
+		return "2.2";
 	}
 
 }
