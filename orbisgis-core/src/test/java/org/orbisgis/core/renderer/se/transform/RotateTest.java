e6e927cf22e6222aee30f94a1cbeb6796b03942c
maxence <maxence@irstv>
2010-04-30 15:13:28 +0000

    Move seTransform tests

---
+++ orbisgis-core/src/test/java/org/orbisgis/core/renderer/se/transform/RotateTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.orbisgis.core.renderer.se.transform;
+
+import org.orbisgis.core.renderer.se.transform.Rotate;
+import junit.framework.TestCase;
+import org.orbisgis.core.renderer.se.parameter.real.RealLiteral;
+
+/**
+ *
+ * @author maxence
+ */
+public class RotateTest extends TestCase {
+    
+    public RotateTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of getRotation method, of class Rotate.
+     */
+    public void testRotate() {
+        Rotate r = new Rotate(new RealLiteral(180.0));
+        assertTrue(r.equals(r));
+    }
+}
