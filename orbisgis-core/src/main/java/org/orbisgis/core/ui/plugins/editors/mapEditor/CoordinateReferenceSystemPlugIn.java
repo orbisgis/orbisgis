0cfa12045e8624967ffaceea5a3e430a3ffea09a
ebocher <ebocher@irstv>
2010-05-28 16:08:41 +0000

    demo gui projection

--- orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/editors/mapEditor/CoordinateReferenceSystemPlugIn.java
+++ orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/editors/mapEditor/CoordinateReferenceSystemPlugIn.java
@@ -6,8 +6,6 @@ import org.orbisgis.core.Services;
 import org.orbisgis.core.images.IconNames;
 import org.orbisgis.core.layerModel.MapContext;
 import org.orbisgis.core.map.projection.ProjectionConfigPanel;
-import org.orbisgis.core.sif.SaveFilePanel;
-import org.orbisgis.core.sif.UIFactory;
 import org.orbisgis.core.ui.editor.IEditor;
 import org.orbisgis.core.ui.pluginSystem.AbstractPlugIn;
 import org.orbisgis.core.ui.pluginSystem.PlugInContext;
@@ -15,16 +13,16 @@ import org.orbisgis.core.ui.pluginSystem.workbench.WorkbenchContext;
 import org.orbisgis.core.ui.plugins.views.MapEditorPlugIn;
 import org.orbisgis.core.ui.plugins.views.editor.EditorManager;
 
-public class CoordinateReferenceSystemPlugIn extends AbstractPlugIn{
+public class CoordinateReferenceSystemPlugIn extends AbstractPlugIn {
 
 	private JButton CRSButton;
-	
+
 	@Override
 	public boolean execute(PlugInContext context) throws Exception {
-		final ProjectionConfigPanel projectionPanel = new ProjectionConfigPanel();
-		if (UIFactory.showDialog(projectionPanel)) {
-			
-		}
+
+		ProjectionConfigPanel projectionPanel = new ProjectionConfigPanel(
+				context.getWorkbenchContext().getWorkbench().getFrame(), true);
+
 		return true;
 	}
 
@@ -32,21 +30,21 @@ public class CoordinateReferenceSystemPlugIn extends AbstractPlugIn{
 	public void initialize(PlugInContext context) throws Exception {
 		CRSButton = new JButton(getIcon(IconNames.MAP_TOOLBAR_PROJECTION));
 		WorkbenchContext wbcontext = context.getWorkbenchContext();
-		
-		wbcontext.getWorkbench().getFrame().getMapEditor()
-							.getProjectionToolBar().addSeparator();
-		wbcontext.getWorkbench().getFrame().getMapEditor()
-							.getProjectionToolBar().addPlugIn(this,CRSButton,context);
-		
+
+		wbcontext.getWorkbench().getFrame().getNavigationToolBar().addPlugIn(
+				this, CRSButton, context);
+
 	}
 
 	@Override
 	public boolean isEnabled() {
 		boolean isVisible = false;
-		IEditor editor = Services.getService(EditorManager.class).getActiveEditor();
-		if (editor != null && editor instanceof MapEditorPlugIn && getPlugInContext().getMapEditor()!=null) {
+		IEditor editor = Services.getService(EditorManager.class)
+				.getActiveEditor();
+		if (editor != null && editor instanceof MapEditorPlugIn
+				&& getPlugInContext().getMapEditor() != null) {
 			MapContext mc = (MapContext) editor.getElement().getObject();
-			isVisible = mc.getLayerModel().getLayerCount() > 0;			
+			isVisible = mc.getLayerModel().getLayerCount() > 0;
 		}
 		CRSButton.setEnabled(isVisible);
 		return isVisible;
@@ -54,7 +52,6 @@ public class CoordinateReferenceSystemPlugIn extends AbstractPlugIn{
 
 	@Override
 	public boolean isSelected() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
