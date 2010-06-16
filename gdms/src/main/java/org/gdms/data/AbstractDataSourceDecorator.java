6a4dc2b1159e7fb76541d4725348fb208ee9a0f0
ebocher <ebocher@irstv>
2010-06-16 22:14:03 +0000

    Clean up

--- gdms/src/main/java/org/gdms/data/AbstractDataSourceDecorator.java
+++ gdms/src/main/java/org/gdms/data/AbstractDataSourceDecorator.java
@@ -1,45 +1,45 @@
 /*
  * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
- * This cross-platform GIS is developed at French IRSTV institute and is able
- * to manipulate and create vector and raster spatial information. OrbisGIS
- * is distributed under GPL 3 license. It is produced  by the geo-informatic team of
- * the IRSTV Institute <http://www.irstv.cnrs.fr/>, CNRS FR 2488:
- *    Erwan BOCHER, scientific researcher,
- *    Thomas LEDUC, scientific researcher,
- *    Fernando GONZALEZ CORTES, computer engineer.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information. OrbisGIS is
+ * distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ *
+ * 
+ *  Team leader Erwan BOCHER, scientific researcher,
+ * 
+ *  User support leader : Gwendall Petit, geomatic engineer.
+ *
  *
  * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
  *
+ * Copyright (C) 2010 Erwan BOCHER, Pierre-Yves FADET, Alexis GUEGANNO, Maxence LAURENT
+ *
  * This file is part of OrbisGIS.
  *
- * OrbisGIS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * OrbisGIS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
  *
- * For more information, please consult:
- *    <http://orbisgis.cerma.archi.fr/>
- *    <http://sourcesup.cru.fr/projects/orbisgis/>
+ * For more information, please consult: <http://www.orbisgis.org/>
  *
  * or contact directly:
- *    erwan.bocher _at_ ec-nantes.fr
- *    fergonco _at_ gmail.com
- *    thomas.leduc _at_ cerma.archi.fr
+ * erwan.bocher _at_ ec-nantes.fr
+ * gwendall.petit _at_ ec-nantes.fr
  */
 /**
  *
  */
 package org.gdms.data;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -58,6 +58,8 @@ import org.gdms.sql.strategies.SemanticException;
 
 public class AbstractDataSourceDecorator extends AbstractDataSource {
 	private DataSource internalDataSource;
+
+	private int index = 0;
 
 	ArrayList<String> fieldsName = new ArrayList<String>();
 
@@ -238,16 +240,6 @@ public class AbstractDataSourceDecorator extends AbstractDataSource {
 	public Number[] getScope(int dimension) throws DriverException,
 			IncompatibleTypesException {
 		return internalDataSource.getScope(dimension);
-	}
-
-	/**
-	 * @return
-	 * @throws IOException
-	 * @see org.gdms.data.DataSource#getWhereFilter()
-	 */
-	@SuppressWarnings("deprecation")
-	public long[] getWhereFilter() throws IOException {
-		return internalDataSource.getWhereFilter();
 	}
 
 	/**
--- gdms/src/main/java/org/gdms/data/SQLSourceDefinition.java
+++ gdms/src/main/java/org/gdms/data/SQLSourceDefinition.java
@@ -1,38 +1,39 @@
 /*
  * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
- * This cross-platform GIS is developed at French IRSTV institute and is able
- * to manipulate and create vector and raster spatial information. OrbisGIS
- * is distributed under GPL 3 license. It is produced  by the geo-informatic team of
- * the IRSTV Institute <http://www.irstv.cnrs.fr/>, CNRS FR 2488:
- *    Erwan BOCHER, scientific researcher,
- *    Thomas LEDUC, scientific researcher,
- *    Fernando GONZALEZ CORTES, computer engineer.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information. OrbisGIS is
+ * distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ *
+ * 
+ *  Team leader Erwan BOCHER, scientific researcher,
+ * 
+ *  User support leader : Gwendall Petit, geomatic engineer.
+ *
  *
  * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
  *
+ * Copyright (C) 2010 Erwan BOCHER, Pierre-Yves FADET, Alexis GUEGANNO, Maxence LAURENT
+ *
  * This file is part of OrbisGIS.
  *
- * OrbisGIS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * OrbisGIS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
  *
- * For more information, please consult:
- *    <http://orbisgis.cerma.archi.fr/>
- *    <http://sourcesup.cru.fr/projects/orbisgis/>
+ * For more information, please consult: <http://www.orbisgis.org/>
  *
  * or contact directly:
- *    erwan.bocher _at_ ec-nantes.fr
- *    fergonco _at_ gmail.com
- *    thomas.leduc _at_ cerma.archi.fr
+ * erwan.bocher _at_ ec-nantes.fr
+ * gwendall.petit _at_ ec-nantes.fr
  */
 package org.gdms.data;
 
@@ -62,6 +63,7 @@ public class SQLSourceDefinition extends AbstractDataSourceDefinition implements
 
 	private Instruction instruction;
 	private String tempSQL;
+	private File file = null;
 
 	public SQLSourceDefinition(Instruction instruction) {
 		this.instruction = instruction;
@@ -69,6 +71,10 @@ public class SQLSourceDefinition extends AbstractDataSourceDefinition implements
 
 	private SQLSourceDefinition(String sql) {
 		this.tempSQL = sql;
+	}
+
+	public File getFile() {
+		return file;
 	}
 
 	public File execute(IProgressMonitor pm) throws DriverException,
@@ -82,7 +88,6 @@ public class SQLSourceDefinition extends AbstractDataSourceDefinition implements
 				throw new IllegalArgumentException(
 						"The query produces no result: " + instruction.getSQL());
 			} else {
-				File file = null;
 				DataSourceFactory dsf = getDataSourceFactory();
 				if (source instanceof DiskBufferDriver) {
 					file = ((DiskBufferDriver) source).getFile();
@@ -97,6 +102,7 @@ public class SQLSourceDefinition extends AbstractDataSourceDefinition implements
 				return file;
 			}
 		}
+
 	}
 
 	public DataSource createDataSource(String tableName, IProgressMonitor pm)
@@ -129,13 +135,14 @@ public class SQLSourceDefinition extends AbstractDataSourceDefinition implements
 	public DefinitionType getDefinition() {
 		SqlDefinitionType ret = new SqlDefinitionType();
 		ret.setSql(instruction.getSQL());
-
 		return ret;
 	}
 
 	public static DataSourceDefinition createFromXML(DataSourceFactory dsf,
 			SqlDefinitionType definitionType) throws DriverException {
-		return new SQLSourceDefinition(definitionType.getSql());
+		SQLSourceDefinition sqlSourceDefinition = new SQLSourceDefinition(
+				definitionType.getSql());
+		return sqlSourceDefinition;
 	}
 
 	@Override
