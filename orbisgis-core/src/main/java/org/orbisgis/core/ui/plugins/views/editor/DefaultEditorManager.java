7b774273daa1edf6c555560fa5434e54e13cd380
ebocher <ebocher@irstv>
2010-06-16 21:37:57 +0000

    Clean up

--- orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/views/editor/DefaultEditorManager.java
+++ orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/views/editor/DefaultEditorManager.java
@@ -1,47 +1,40 @@
-/**
+/*
  * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
  * This cross-platform GIS is developed at French IRSTV institute and is able to
  * manipulate and create vector and raster spatial information. OrbisGIS is
- * distributed under GPL 3 license. It is produced by the geo-informatic team of
+ * distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
  * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
- * 
- *  
- *  Lead Erwan BOCHER, scientific researcher, 
  *
- *  Developer lead : Pierre-Yves FADET, computer engineer. 
- *  
- *  User support lead : Gwendall Petit, geomatic engineer. 
  * 
- * Previous computer developer : Thomas LEDUC, scientific researcher, Fernando GONZALEZ
- * CORTES, computer engineer.
+ *  Team leader Erwan BOCHER, scientific researcher,
  * 
+ *  User support leader : Gwendall Petit, geomatic engineer.
+ *
+ *
  * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
- * 
- * Copyright (C) 2010 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
- * 
+ *
+ * Copyright (C) 2010 Erwan BOCHER, Pierre-Yves FADET, Alexis GUEGANNO, Maxence LAURENT
+ *
  * This file is part of OrbisGIS.
- * 
+ *
  * OrbisGIS is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, please consult: <http://orbisgis.cerma.archi.fr/>
- * <http://sourcesup.cru.fr/projects/orbisgis/>
- * 
- * or contact directly: 
- * erwan.bocher _at_ ec-nantes.fr 
- * Pierre-Yves.Fadet _at_ ec-nantes.fr
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ *
+ * or contact directly:
+ * erwan.bocher _at_ ec-nantes.fr
  * gwendall.petit _at_ ec-nantes.fr
- **/
-
+ */
 package org.orbisgis.core.ui.plugins.views.editor;
 
 import org.apache.log4j.Logger;
@@ -87,10 +80,12 @@ public class DefaultEditorManager implements EditorManager {
 		return editorPanel.getEditors();
 	}
 
+	@Override
 	public boolean hasEditor(EditableElement element) {
 		return EditorPanel.getFirstEditor(element) != null;
 	}
 
+	@Override
 	public void open(EditableElement element, IProgressMonitor pm)
 			throws UnsupportedOperationException {
 		EditorDecorator editor = EditorPanel.getFirstEditor(element);
@@ -118,27 +113,35 @@ public class DefaultEditorManager implements EditorManager {
 		}
 	}
 
+	@Override
 	public IEditor[] getEditor(EditableElement element) {
 		return this.editorPanel.getEditor(element);
 	}
 
+	@Override
 	public String getEditorId(IEditor editor) {
 		return editorPanel.getEditorId(editor);
 	}
 
+	@Override
 	public void addEditorListener(EditorListener listener) {
 		editorPanel.addEditorListener(listener);
 	}
 
+	@Override
 	public void removeEditorListener(EditorListener listener) {
 		editorPanel.removeEditorListener(listener);
 	}
 
+	@Override
 	public IEditor[] getEditors(String editorId, Object object) {
 		return editorPanel.getEditors(editorId, object);
 	}
 
+	@Override
 	public IEditor[] getEditors(String editorId) {
 		return editorPanel.getEditors(editorId);
-	}	
+	}
+
+	
 }
--- orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/views/editor/TransferableEditableElement.java
+++ orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/views/editor/TransferableEditableElement.java
@@ -1,47 +1,40 @@
-/**
+/*
  * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
  * This cross-platform GIS is developed at French IRSTV institute and is able to
  * manipulate and create vector and raster spatial information. OrbisGIS is
- * distributed under GPL 3 license. It is produced by the geo-informatic team of
+ * distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
  * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
- * 
- *  
- *  Lead Erwan BOCHER, scientific researcher, 
  *
- *  Developer lead : Pierre-Yves FADET, computer engineer. 
- *  
- *  User support lead : Gwendall Petit, geomatic engineer. 
  * 
- * Previous computer developer : Thomas LEDUC, scientific researcher, Fernando GONZALEZ
- * CORTES, computer engineer.
+ *  Team leader Erwan BOCHER, scientific researcher,
  * 
+ *  User support leader : Gwendall Petit, geomatic engineer.
+ *
+ *
  * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
- * 
- * Copyright (C) 2010 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
- * 
+ *
+ * Copyright (C) 2010 Erwan BOCHER, Pierre-Yves FADET, Alexis GUEGANNO, Maxence LAURENT
+ *
  * This file is part of OrbisGIS.
- * 
+ *
  * OrbisGIS is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, please consult: <http://orbisgis.cerma.archi.fr/>
- * <http://sourcesup.cru.fr/projects/orbisgis/>
- * 
- * or contact directly: 
- * erwan.bocher _at_ ec-nantes.fr 
- * Pierre-Yves.Fadet _at_ ec-nantes.fr
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ *
+ * or contact directly:
+ * erwan.bocher _at_ ec-nantes.fr
  * gwendall.petit _at_ ec-nantes.fr
- **/
-
+ */
 package org.orbisgis.core.ui.plugins.views.editor;
 
 import java.awt.datatransfer.DataFlavor;
@@ -62,6 +55,7 @@ public class TransferableEditableElement implements Transferable {
 		this.elements = elements;
 	}
 
+	@Override
 	public Object getTransferData(DataFlavor flavor)
 			throws UnsupportedFlavorException, IOException {
 		Object ret = null;
@@ -80,13 +74,16 @@ public class TransferableEditableElement implements Transferable {
 		return ret;
 	}
 
+	@Override
 	public DataFlavor[] getTransferDataFlavors() {
 		return (new DataFlavor[] { editableElementFlavor,
 				DataFlavor.stringFlavor });
 	}
 
+	@Override
 	public boolean isDataFlavorSupported(DataFlavor flavor) {
 		return (flavor.equals(editableElementFlavor) || flavor
 				.equals(DataFlavor.stringFlavor));
 	}
+
 }
