56678ae2ddd6aa8e42df0a66407783687f050e05
ebocher <ebocher@irstv>
2010-06-16 22:19:25 +0000

    Clean up

--- gdms/src/main/java/org/gdms/sql/customQuery/spatial/geometry/crs/ST_Transform.java
+++ gdms/src/main/java/org/gdms/sql/customQuery/spatial/geometry/crs/ST_Transform.java
@@ -1,3 +1,40 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information. OrbisGIS is
+ * distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ *
+ * 
+ *  Team leader Erwan BOCHER, scientific researcher,
+ * 
+ *  User support leader : Gwendall Petit, geomatic engineer.
+ *
+ *
+ * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
+ *
+ * Copyright (C) 2010 Erwan BOCHER, Pierre-Yves FADET, Alexis GUEGANNO, Maxence LAURENT
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ *
+ * or contact directly:
+ * erwan.bocher _at_ ec-nantes.fr
+ * gwendall.petit _at_ ec-nantes.fr
+ */
 package org.gdms.sql.customQuery.spatial.geometry.crs;
 
 import java.util.List;
@@ -64,24 +101,28 @@ public class ST_Transform implements CustomQuery {
 					.getMetadata());
 
 			GeometryTransformer gt = new GeometryTransformer() {
-	            protected CoordinateSequence transformCoordinates(CoordinateSequence cs, Geometry geom){
-	                Coordinate[] cc = geom.getCoordinates();
-	                CoordinateSequence newcs = new CoordinateArraySequence(cc);
-	                for (int i = 0 ; i < cc.length ; i++) {
-	                    Coordinate c = cc[i];
-	                    try {	                    	
-	                        double[] xyz = cos.transform(new double[]{c.x, c.y, c.z});
-	                        newcs.setOrdinate(i,0,xyz[0]);
-	                        newcs.setOrdinate(i,1,xyz[1]);
-	                        if(xyz.length > 2)
-	                        	newcs.setOrdinate(i,2,xyz[2]);
-	                        else
-	                        	newcs.setOrdinate(i,2,Double.NaN);
-	                    } catch(IllegalCoordinateException ice) {ice.printStackTrace();}
-	                }
-	                return newcs;
-	            }
-	        };
+				protected CoordinateSequence transformCoordinates(
+						CoordinateSequence cs, Geometry geom) {
+					Coordinate[] cc = geom.getCoordinates();
+					CoordinateSequence newcs = new CoordinateArraySequence(cc);
+					for (int i = 0; i < cc.length; i++) {
+						Coordinate c = cc[i];
+						try {
+							double[] xyz = cos.transform(new double[] { c.x,
+									c.y, c.z });
+							newcs.setOrdinate(i, 0, xyz[0]);
+							newcs.setOrdinate(i, 1, xyz[1]);
+							if (xyz.length > 2)
+								newcs.setOrdinate(i, 2, xyz[2]);
+							else
+								newcs.setOrdinate(i, 2, Double.NaN);
+						} catch (IllegalCoordinateException ice) {
+							ice.printStackTrace();
+						}
+					}
+					return newcs;
+				}
+			};
 
 			int geomIndex = sds.getSpatialFieldIndex();
 			for (int i = 0; i < sds.getRowCount(); i++) {
@@ -131,8 +172,7 @@ public class ST_Transform implements CustomQuery {
 
 	@Override
 	public String getSqlOrder() {
-		// TODO Auto-generated method stub
-		return null;
+		return "SELECT ST_TRANSFORM(the_geom, '4326') from myTable";
 	}
 
 }
