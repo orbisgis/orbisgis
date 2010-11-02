efb01c4b2901da933953010a9196e959bb59149e
ebocher <ebocher@irstv>
2010-11-02 13:59:55 +0000

    TASK #279

---
+++ gdms/src/main/java/org/gdms/data/system/SystemSource.java
@@ -0,0 +1,16 @@
+package org.gdms.data.system;
+
+import java.io.File;
+
+public class SystemSource {
+
+	private File file;
+
+	public SystemSource(File file) {
+		this.file = file;
+	}
+
+	public File getFile() {
+		return file;
+	}
+}
