bc35b64ac01b4fe380233eb09ded63e622d2f2f5
ebocher <ebocher@irstv>
2010-11-02 13:45:31 +0000

    TASK #278

--- orbisgis-core/src/test/java/org/orbisgis/core/ui/plugins/demo/extensions/tools/HelloToolExtension.java
+++ orbisgis-core/src/test/java/org/orbisgis/core/ui/plugins/demo/extensions/tools/HelloToolExtension.java
@@ -1,9 +1,9 @@
 package org.orbisgis.core.ui.plugins.demo.extensions.tools;
 
-import org.orbisgis.core.images.IconLoader;
 import org.orbisgis.core.ui.pluginSystem.Extension;
 import org.orbisgis.core.ui.pluginSystem.PlugInContext;
 import org.orbisgis.core.ui.pluginSystem.workbench.WorkbenchContext;
+import org.orbisgis.core.ui.preferences.lookandfeel.images.IconLoader;
 
 public class HelloToolExtension extends Extension {
 
