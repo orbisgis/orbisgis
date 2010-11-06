2c0a56d0f4801078344b663a9ceddf59ba0809b9
ebocher <ebocher@irstv>
2010-11-06 14:17:24 +0000

    DOC

--- orbisgis-core/src/main/java/org/orbisgis/core/OrbisGISApplicationInfo.java
+++ orbisgis-core/src/main/java/org/orbisgis/core/OrbisGISApplicationInfo.java
@@ -13,7 +13,7 @@
  *
  * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
  *
- * Copyright (C) 2010 Erwan BOCHER, Pierre-Yves FADET, Alexis GUEGANNO, Maxence LAURENT
+ * Copyright (C) 2010 Erwan BOCHER, Alexis GUEGANNO, Maxence LAURENT
  *
  * This file is part of OrbisGIS.
  *
@@ -32,8 +32,7 @@
  * For more information, please consult: <http://www.orbisgis.org/>
  *
  * or contact directly:
- * erwan.bocher _at_ ec-nantes.fr
- * gwendall.petit _at_ ec-nantes.fr
+ * info _at_ orbisgis.org
  */
 
 package org.orbisgis.core;
