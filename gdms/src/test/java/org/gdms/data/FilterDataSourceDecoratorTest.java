7c009dc70b82760a6bd890875edbcbd1ccb7d6ac
ebocher <ebocher@irstv>
2010-11-15 11:34:45 +0000

    TASK #247 added a junit test for st_intersects.

--- gdms/src/test/java/org/gdms/data/FilterDataSourceDecoratorTest.java
+++ gdms/src/test/java/org/gdms/data/FilterDataSourceDecoratorTest.java
@@ -41,46 +41,94 @@ package org.gdms.data;
 
 import java.io.File;
 import java.util.List;
+
 import org.gdms.SourceTest;
+
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.WKTReader;
 
 public class FilterDataSourceDecoratorTest extends SourceTest {
 
-    public void testFilterDecorator() throws Exception {
-        dsf.getSourceManager().register("landcover2000",
+	public void testFilterDecorator() throws Exception {
+		dsf.getSourceManager().register("landcover2000",
 				new File(internalData + "landcover2000.shp"));
-        DataSource original = dsf.getDataSource("landcover2000");
-        FilterDataSourceDecorator decorator = new FilterDataSourceDecorator(original);
-        decorator.setFilter("type = 'cereals'");
+		DataSource original = dsf.getDataSource("landcover2000");
+		FilterDataSourceDecorator decorator = new FilterDataSourceDecorator(
+				original);
+		decorator.setFilter("type = 'cereals'");
 
-        original.open();
-        decorator.open();
+		original.open();
+		decorator.open();
 
-        assertTrue(original.getFieldCount() == decorator.getFieldCount());
+		assertTrue(original.getFieldCount() == decorator.getFieldCount());
 
-        for (int i = 0; i < original.getMetadata().getFieldCount(); i++) {
-            assertTrue(original.getFieldName(i).equals(decorator.getFieldName(i)));
-        }
+		for (int i = 0; i < original.getMetadata().getFieldCount(); i++) {
+			assertTrue(original.getFieldName(i).equals(
+					decorator.getFieldName(i)));
+		}
 
-        int cols = original.getFieldCount();
+		int cols = original.getFieldCount();
 
-        for (int i = 0; i < decorator.getRowCount() && i < 10000; i++) {
-            long o = decorator.getOriginalIndex(i);
-            assertTrue(decorator.getFieldValue(i, decorator.getFieldIndexByName("type")).toString().equals("cereals"));
-            for (int j = 0; j < cols; j++) {
-                assertTrue(decorator.getFieldValue(i, j).doEquals(original.getFieldValue(o, j)));
-            }
-        }
+		for (int i = 0; i < decorator.getRowCount() && i < 10000; i++) {
+			long o = decorator.getOriginalIndex(i);
+			assertTrue(decorator.getFieldValue(i,
+					decorator.getFieldIndexByName("type")).toString().equals(
+					"cereals"));
+			for (int j = 0; j < cols; j++) {
+				assertTrue(decorator.getFieldValue(i, j).doEquals(
+						original.getFieldValue(o, j)));
+			}
+		}
 
-        List<Integer> map = decorator.getIndexMap();
-        for (int i = 0; i < map.size(); i++) {
-            assertTrue(decorator.getFieldValue(i, decorator.getFieldIndexByName("type")).toString().equals("cereals"));
-            for (int j = 0; j < cols; j++) {
-                assertTrue(decorator.getFieldValue(i, j).doEquals(original.getFieldValue(map.get(i), j)));
-            }
-        }
+		List<Integer> map = decorator.getIndexMap();
+		for (int i = 0; i < map.size(); i++) {
+			assertTrue(decorator.getFieldValue(i,
+					decorator.getFieldIndexByName("type")).toString().equals(
+					"cereals"));
+			for (int j = 0; j < cols; j++) {
+				assertTrue(decorator.getFieldValue(i, j).doEquals(
+						original.getFieldValue(map.get(i), j)));
+			}
+		}
 
-        decorator.close();
-        original.close();
-    }
+		decorator.close();
+		original.close();
+	}
 
+	public void testSpatialFilter() throws Exception {
+
+		dsf.getSourceManager().register("landcover2000",
+				new File(internalData + "landcover2000.shp"));
+
+		WKTReader wktReader = new WKTReader();
+		Geometry geomExtent = wktReader
+				.read("POLYGON ((183456.16879270627 2428883.34989648 0, 183461.0194286128 2428262.4685004433 0, 184467.5263792192 2428233.364685004 0, 184477.22765103227 2428883.34989648 0, 183456.16879270627 2428883.34989648 0))");
+
+		int waintingResult = 77;
+		
+		Envelope extent = geomExtent.getEnvelopeInternal();
+		DataSource original = dsf.getDataSource("landcover2000");
+
+		FilterDataSourceDecorator filterDataSourceDecorator = new FilterDataSourceDecorator(
+				original);
+
+		String filter = "ST_Intersects(ST_GeomFromText('POLYGON(("
+				+ extent.getMinX() + " " + extent.getMinY() + ","
+				+ extent.getMinX() + " " + extent.getMaxY() + ","
+				+ extent.getMaxX() + " " + extent.getMaxY() + ","
+				+ extent.getMaxX() + " " + extent.getMinY() + ","
+				+ extent.getMinX() + " " + extent.getMinY() + "))'), "
+				+ "the_geom" + ")";
+		filterDataSourceDecorator.setFilter(filter);
+		
+		long filterCount = filterDataSourceDecorator.getRowCount();
+		
+		assertTrue(filterCount==waintingResult);
+		
+		
+		
+		
+
+	}
 }
