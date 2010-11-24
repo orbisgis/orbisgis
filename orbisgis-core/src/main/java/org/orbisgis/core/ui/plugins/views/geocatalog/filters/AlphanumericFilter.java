1b26793e6f56a9251572ab806a752f01095d715d
Antoine Gourlay <antoine@gourlay.fr>
2010-11-24 13:27:27 +0000

    FIX #316 Added SYSTEM_TABLE as a non-accepted type in the AlphanumericFilter

--- orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/views/geocatalog/filters/AlphanumericFilter.java
+++ orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/views/geocatalog/filters/AlphanumericFilter.java
@@ -51,7 +51,7 @@ public class AlphanumericFilter implements IFilter {
 	public boolean accepts(SourceManager sm, String sourceName) {
 		int type = sm.getSource(sourceName).getType();
 		int spatial = SourceManager.VECTORIAL | SourceManager.RASTER
-				| SourceManager.WMS;
+				| SourceManager.WMS | SourceManager.SYSTEM_TABLE;
 		return (type & spatial) == 0;
 	}
 
