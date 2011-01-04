0b29ed20dc528d451bad86135379016fbb898eff
Antoine Gourlay <antoine@gourlay.fr>
2011-01-04 08:00:19 +0000

    removed useless private inner-class in SQLConsoleViewPlugIn: it is a duplicate of sqlConsole.actions.ExecuteScriptProcess.
    This class was being used only when executing the script with the Execute button, and the other (public) one when using Ctrl+Enter... now both use the public one.

--- orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/views/sqlConsole/SQLConsoleViewPlugIn.java
+++ orbisgis-core/src/main/java/org/orbisgis/core/ui/plugins/views/sqlConsole/SQLConsoleViewPlugIn.java
@@ -51,46 +51,26 @@ import javax.swing.JButton;
 import javax.swing.JMenuItem;
 
 import org.apache.log4j.Logger;
-import org.gdms.data.DataSource;
-import org.gdms.data.DataSourceCreationException;
-import org.gdms.data.DataSourceFactory;
-import org.gdms.data.ExecutionException;
-import org.gdms.data.metadata.Metadata;
-import org.gdms.data.types.Type;
-import org.gdms.driver.DriverException;
 import org.gdms.sql.customQuery.CustomQuery;
 import org.gdms.sql.customQuery.QueryManager;
 import org.gdms.sql.function.Function;
 import org.gdms.sql.function.FunctionManager;
-import org.gdms.sql.parser.ParseException;
-import org.gdms.sql.parser.TokenMgrError;
-import org.gdms.sql.strategies.Instruction;
-import org.gdms.sql.strategies.SQLProcessor;
-import org.gdms.sql.strategies.SemanticException;
-import org.orbisgis.core.DataManager;
 import org.orbisgis.core.OrbisGISPersitenceConfig;
 import org.orbisgis.core.Services;
-import org.orbisgis.core.background.BackgroundJob;
 import org.orbisgis.core.background.BackgroundManager;
 import org.orbisgis.core.geocognition.GeocognitionElement;
-import org.orbisgis.core.layerModel.ILayer;
-import org.orbisgis.core.layerModel.LayerException;
-import org.orbisgis.core.layerModel.MapContext;
 import org.orbisgis.core.sif.OpenFilePanel;
 import org.orbisgis.core.sif.SaveFilePanel;
 import org.orbisgis.core.sif.UIFactory;
-import org.orbisgis.core.ui.editors.map.MapContextManager;
 import org.orbisgis.core.ui.geocognition.TransferableGeocognitionElement;
 import org.orbisgis.core.ui.pluginSystem.PlugInContext;
 import org.orbisgis.core.ui.pluginSystem.ViewPlugIn;
 import org.orbisgis.core.ui.pluginSystem.workbench.Names;
 import org.orbisgis.core.ui.pluginSystem.workbench.WorkbenchContext;
-import org.orbisgis.core.ui.plugins.views.OutputManager;
 import org.orbisgis.core.ui.plugins.views.sqlConsole.actions.ConsoleListener;
+import org.orbisgis.core.ui.plugins.views.sqlConsole.actions.ExecuteScriptProcess;
 import org.orbisgis.core.ui.plugins.views.sqlConsole.ui.SQLConsolePanel;
-import org.orbisgis.core.ui.plugins.views.sqlConsole.util.CodeErrors;
 import org.orbisgis.core.ui.preferences.lookandfeel.OrbisGISIcon;
-import org.orbisgis.progress.IProgressMonitor;
 
 public class SQLConsoleViewPlugIn extends ViewPlugIn {
 
@@ -134,9 +114,9 @@ public class SQLConsoleViewPlugIn extends ViewPlugIn {
 					final BufferedReader in = new BufferedReader(
 							new FileReader(selectedFile));
 					String line;
-					StringBuffer ret = new StringBuffer();
+					StringBuilder ret = new StringBuilder();
 					while ((line = in.readLine()) != null) {
-						ret.append(line + EOL);
+						ret.append(line).append(EOL);
 					}
 					in.close();
 
@@ -147,8 +127,7 @@ public class SQLConsoleViewPlugIn extends ViewPlugIn {
 			}
 
 			public void execute(String text) {
-				BackgroundManager bm = (BackgroundManager) Services
-						.getService(BackgroundManager.class);
+				BackgroundManager bm = Services.getService(BackgroundManager.class);
 				bm.backgroundOperation(new ExecuteScriptProcess(text));
 			}
 
@@ -210,157 +189,6 @@ public class SQLConsoleViewPlugIn extends ViewPlugIn {
 	public boolean execute(PlugInContext context) throws Exception {
 		getPlugInContext().loadView(getId());
 		return true;
-	}
-
-	private class ExecuteScriptProcess implements BackgroundJob {
-
-		private String script;
-
-		public ExecuteScriptProcess(String script) {
-			this.script = script;
-		}
-
-		public String getTaskName() {
-			return "Executing script";
-		}
-
-		public void run(IProgressMonitor pm) {
-
-			DataManager dataManager = (DataManager) Services
-					.getService(DataManager.class);
-			DataSourceFactory dsf = dataManager.getDataSourceFactory();
-			SQLProcessor sqlProcessor = new SQLProcessor(dsf);
-			String[] instructions = new String[0];
-
-			long t1 = System.currentTimeMillis();
-			try {
-				logger.debug("Preparing script: " + script);
-				try {
-					instructions = sqlProcessor.getScriptInstructions(script);
-				} catch (SemanticException e) {
-					Services.getErrorManager().error(
-							"Semantic error in the script", e);
-				} catch (ParseException e) {
-					Services.getErrorManager().error("Cannot parse script", e);
-					panel.updateCodeError(CodeErrors.getCodeError(e, script));
-				} catch (TokenMgrError e) {
-					Services.getErrorManager().error("Cannot parse script", e);
-				}
-
-				MapContext vc = ((MapContextManager) Services
-						.getService(MapContextManager.class))
-						.getActiveMapContext();
-
-				for (int i = 0; i < instructions.length; i++) {
-
-					logger.debug("Preparing instruction: " + instructions[i]);
-					try {
-						Instruction instruction = sqlProcessor
-								.prepareInstruction(instructions[i]);
-
-						Metadata metadata = instruction.getResultMetadata();
-						if (metadata != null) {
-							boolean spatial = false;
-							for (int k = 0; k < metadata.getFieldCount(); k++) {
-								int typeCode = metadata.getFieldType(k)
-										.getTypeCode();
-								if ((typeCode == Type.GEOMETRY)
-										|| (typeCode == Type.RASTER)) {
-									spatial = true;
-								}
-							}
-
-							DataSource ds = dsf.getDataSource(instruction,
-									DataSourceFactory.DEFAULT, pm);
-
-							if (pm.isCancelled()) {
-								break;
-							}
-
-							if (spatial && vc != null) {
-
-								try {
-									final ILayer layer = dataManager
-											.createLayer(ds);
-
-									vc.getLayerModel().insertLayer(layer, 0);
-
-								} catch (LayerException e) {
-									Services.getErrorManager().error(
-											"Cannot create the layer:"
-													+ ds.getName(), e);
-									break;
-								}
-							} else {
-
-								ds.open();
-								StringBuilder aux = new StringBuilder();
-								int fc = ds.getMetadata().getFieldCount();
-								int rc = (int) ds.getRowCount();
-
-								for (int j = 0; j < fc; j++) {
-									aux.append(ds.getFieldName(j)).append("\t");
-								}
-								aux.append("\n");
-								for (int row = 0; row < rc; row++) {
-									for (int j = 0; j < fc; j++) {
-										aux.append(ds.getFieldValue(row, j))
-												.append("\t");
-									}
-									aux.append("\n");
-									if (row > 100) {
-										aux.append("and more... total " + rc
-												+ " rows");
-										break;
-									}
-								}
-								ds.close();
-
-								OutputManager om = Services
-										.getService(OutputManager.class);
-								om.println(aux.toString());
-							}
-						} else {
-							instruction.execute(pm);
-
-							if (pm.isCancelled()) {
-								break;
-							}
-
-						}
-					} catch (ExecutionException e) {
-						Services.getErrorManager().error(
-								"Error executing the instruction:"
-										+ instructions[i], e);
-						break;
-					} catch (SemanticException e) {
-						Services.getErrorManager().error(
-								"Semantic error in instruction:"
-										+ instructions[i], e);
-						break;
-					} catch (DataSourceCreationException e) {
-						Services.getErrorManager().error(
-								"Cannot create the DataSource:"
-										+ instructions[i], e);
-						break;
-					} catch (ParseException e) {
-						Services.getErrorManager().error(
-								"Parse error in statement:" + instructions[i],
-								e);
-						break;
-					}
-
-					pm.progressTo(100 * i / instructions.length);
-				}
-
-			} catch (DriverException e) {
-				Services.getErrorManager().error("Data access error :", e);
-			}
-
-			long t2 = System.currentTimeMillis();
-			logger.debug("Execution time: " + ((t2 - t1) / 1000.0));
-			panel.setStatusMessage("Execution time: " + ((t2 - t1) / 1000.0));
-		}
 	}
 
 	public boolean isEnabled() {
