a672ff756f4d219895af8ea81e00dc09d149eae2
agueganno <a9emen@gmail.com>
2011-07-29 13:36:28 +0000

    Adding documentation, and ensuring code indentation.

--- orbisgis-core/src/main/java/org/orbisgis/core/renderer/se/CompositeSymbolizer.java
+++ orbisgis-core/src/main/java/org/orbisgis/core/renderer/se/CompositeSymbolizer.java
@@ -35,8 +35,6 @@
  * erwan.bocher _at_ ec-nantes.fr
  * gwendall.petit _at_ ec-nantes.fr
  */
-
-
 package org.orbisgis.core.renderer.se;
 
 import java.util.ArrayList;
@@ -50,126 +48,149 @@ import org.orbisgis.core.renderer.se.SeExceptions.InvalidStyle;
 import org.orbisgis.core.renderer.se.common.Uom;
 
 /**
- *
- * This is the entry point
- *
+ * This is the entry point of the <code>Symbolizer</code>'s structure in a <code>Rule</code>
+ * A <code>CompositeSymbolize</code> is a collection that embedded other <code>Symbolizer</code>s,
+ * and that is not directly associated to any rendering or representation hint.
  * @author maxence
  */
 public final class CompositeSymbolizer implements SymbolizerNode {
 
-    public JAXBElement<? extends SymbolizerType> getJAXBElement(){
-        if (symbolizers.size() == 1){
-            return symbolizers.get(0).getJAXBElement();
-        }
-        else if (symbolizers.size() > 1){
-            ObjectFactory of = new ObjectFactory();
-            CompositeSymbolizerType cs = of.createCompositeSymbolizerType();
-            List<JAXBElement<? extends SymbolizerType>> sList = cs.getSymbolizer();
-            for (Symbolizer s : symbolizers){
-                sList.add(s.getJAXBElement());
-            }
-
-            return of.createCompositeSymbolizer(cs);
-        }
-        else{
-            return null;
-        }
-    }
-
-
-    public CompositeSymbolizer(JAXBElement<? extends SymbolizerType> st) throws InvalidStyle {
-        symbolizers = new ArrayList<Symbolizer>();
+        private ArrayList<Symbolizer> symbolizers;
+        private SymbolizerNode parent;
         
-        if (st.getDeclaredType() == net.opengis.se._2_0.core.CompositeSymbolizerType.class){
-            JAXBElement<CompositeSymbolizerType> jcs = (JAXBElement<CompositeSymbolizerType>)st;
+        /**
+         * Get a Jaxb representation of this <code>CompositeSymbolizer</code>.
+         * @return 
+         */
+        public JAXBElement<? extends SymbolizerType> getJAXBElement() {
+                if (symbolizers.size() == 1) {
+                        return symbolizers.get(0).getJAXBElement();
+                } else if (symbolizers.size() > 1) {
+                        ObjectFactory of = new ObjectFactory();
+                        CompositeSymbolizerType cs = of.createCompositeSymbolizerType();
+                        List<JAXBElement<? extends SymbolizerType>> sList = cs.getSymbolizer();
+                        for (Symbolizer s : symbolizers) {
+                                sList.add(s.getJAXBElement());
+                        }
 
-            for (JAXBElement<? extends SymbolizerType> s : jcs.getValue().getSymbolizer()){
-                if (s.getDeclaredType() == net.opengis.se._2_0.core.CompositeSymbolizerType.class){
-                    // If the sub-symbolizer is another collection : inline all
-                    CompositeSymbolizer cs2 = new CompositeSymbolizer(s);
-                    for (Symbolizer s2 : cs2.symbolizers){
-                        this.addSymbolizer(s2);
-                    }
+                        return of.createCompositeSymbolizer(cs);
+                } else {
+                        return null;
                 }
-                else{
-                    Symbolizer symb = Symbolizer.createSymbolizerFromJAXBElement(s);
-                    this.addSymbolizer(symb);
-                }
-            }
         }
-        else{
-            this.addSymbolizer(Symbolizer.createSymbolizerFromJAXBElement(st));
-        }
-    }
 
-    /*
-    public void draw(Graphics2D g2, SpatialDataSourceDecorator sds, long fid, boolean selected, MapTransform mt) throws ParameterException, IOException, DriverException{
+        /**
+         * Build a new <code>CompositeSymbolizer</code>, finding its attributes and inner
+         * elements in a JAXBElement.
+         * @param st
+         * @throws org.orbisgis.core.renderer.se.SeExceptions.InvalidStyle 
+         */
+        public CompositeSymbolizer(JAXBElement<? extends SymbolizerType> st) throws InvalidStyle {
+                symbolizers = new ArrayList<Symbolizer>();
+
+                if (st.getDeclaredType() == net.opengis.se._2_0.core.CompositeSymbolizerType.class) {
+                        JAXBElement<CompositeSymbolizerType> jcs = (JAXBElement<CompositeSymbolizerType>) st;
+
+                        for (JAXBElement<? extends SymbolizerType> s : jcs.getValue().getSymbolizer()) {
+                                if (s.getDeclaredType() == net.opengis.se._2_0.core.CompositeSymbolizerType.class) {
+                                        // If the sub-symbolizer is another collection : inline all
+                                        CompositeSymbolizer cs2 = new CompositeSymbolizer(s);
+                                        for (Symbolizer s2 : cs2.symbolizers) {
+                                                this.addSymbolizer(s2);
+                                        }
+                                } else {
+                                        Symbolizer symb = Symbolizer.createSymbolizerFromJAXBElement(s);
+                                        this.addSymbolizer(symb);
+                                }
+                        }
+                } else {
+                        this.addSymbolizer(Symbolizer.createSymbolizerFromJAXBElement(st));
+                }
+        }
+
+        /*
+        public void draw(Graphics2D g2, SpatialDataSourceDecorator sds, long fid, boolean selected, MapTransform mt) throws ParameterException, IOException, DriverException{
         for (Symbolizer s : this.symbolizers){
-            if (s instanceof VectorSymbolizer){
-                ((VectorSymbolizer)s).draw(g2, sds, fid, selected, mt, null, null);
-            }
+        if (s instanceof VectorSymbolizer){
+        ((VectorSymbolizer)s).draw(g2, sds, fid, selected, mt, null, null);
         }
-    }*/
+        }
+        }*/
+        /**
+         * Build a new, empty, CompositeSymbolizer.
+         */
+        public CompositeSymbolizer() {
+                symbolizers = new ArrayList<Symbolizer>();
+        }
 
-    public CompositeSymbolizer(){
-        symbolizers = new ArrayList<Symbolizer>();
-    }
+        /**
+         * Get the list of <code>Symbolizer</code>s contained in this <code>CompositeSymbolizer</code>
+         * @return 
+         */
+        public List<Symbolizer> getSymbolizerList() {
+                return this.symbolizers;
+        }
 
-    public List<Symbolizer> getSymbolizerList(){
-        return this.symbolizers;
-    }
+        /**
+         * Add a <code>Symbolizer</code> to the list contained in this <code>CompositeSymbolizer</code>
+         * @param s 
+         */
+        public void addSymbolizer(Symbolizer s) {
+                symbolizers.add(s);
+                s.setParent(this);
+                if (s.getLevel() < 0) {
+                        s.setLevel(symbolizers.size());
+                }
+        }
 
-    public void addSymbolizer(Symbolizer s){
-        symbolizers.add(s);
-        s.setParent(this);
-		if (s.getLevel() < 0){
-			s.setLevel(symbolizers.size());
-		}
-    }
+        /**
+         * @deprecated
+         */
+        public void moveSymbolizerDown(Symbolizer s) {
+                int index = symbolizers.indexOf(s);
+                if (index > -1 && index < symbolizers.size() - 1) {
+                        symbolizers.remove(index);
+                        symbolizers.add(index + 1, s);
+                }
+        }
 
-	/**
-	 * @deprecated
-	 */
-	public void moveSymbolizerDown(Symbolizer s){
-		int index = symbolizers.indexOf(s);
-		if (index > -1 && index < symbolizers.size()-1){
-			symbolizers.remove(index);
-			symbolizers.add(index+1, s);
-		}
-	}
+        /**
+         * @deprecated
+         */
+        public void moveSymbolizerUp(Symbolizer s) {
+                int index = symbolizers.indexOf(s);
+                if (index > 0) {
+                        symbolizers.remove(index);
+                        symbolizers.add(index - 1, s);
+                }
+        }
 
+        /**
+         * Remove the <code>Symbolizer s</code> to the list contained in this <code>CompositeSymbolizer</code>
+         * @param s 
+         */
+        public void removeSymbolizer(Symbolizer s) {
+                symbolizers.remove(s);
+        }
 
-	/**
-	 * @deprecated
-	 */
-	public void moveSymbolizerUp(Symbolizer s){
-		int index = symbolizers.indexOf(s);
-		if (index > 0){
-			symbolizers.remove(index);
-			symbolizers.add(index-1, s);
-		}
-	}
+        /**
+         * As a collection of <code>Symbolizer</code>s, a <code>CompositeSymbolize</code>
+         * is not associated to any Uom. Returns always <code>null</code>
+         * @return 
+         *      null
+         */
+        @Override
+        public Uom getUom() {
+                return null;
+        }
 
-	public void removeSymbolizer(Symbolizer s){
-		symbolizers.remove(s);
-	}
+        @Override
+        public SymbolizerNode getParent() {
+                return parent;
+        }
 
-    private ArrayList<Symbolizer> symbolizers;
-
-    @Override
-    public Uom getUom() {
-        return null;
-    }
-
-    @Override
-    public SymbolizerNode getParent() {
-        return parent;
-    }
-
-    @Override
-    public void setParent(SymbolizerNode rule) {
-        this.parent = rule;
-    }
-
-    SymbolizerNode parent;
+        @Override
+        public void setParent(SymbolizerNode rule) {
+                this.parent = rule;
+        }
 }
