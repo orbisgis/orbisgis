75c02edb706e9b3fcecbc01d01403e31e6701055
ebocher <ebocher@irstv>
2011-08-25 07:53:25 +0000

    Task #463 Add two  SQL functions to interact with OrbisGIS ui MC_ADDLayer(toto) add table in the current mapcontext MC_BBox() returns the current mapcontext bounding box. How to use it : create table result as SELECT * from mytable where st_intersects(the_geom, BBox()); Execute MC_ADDLayer(result);

---
+++ orbisgis-ui/src/main/java/org/orbisgis/core/ui/plugins/sql/SQLFunctionsPlugIn.java
@@ -0,0 +1,62 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information. OrbisGIS is
+ * distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ *
+ *
+ *  Team leader Erwan BOCHER, scientific researcher,
+ *
+ *
+ *
+ * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
+ *
+ * Copyright (C) 2010 Erwan BOCHER,  Alexis GUEGANNO, Antoine GOURLAY, Adelin PIAU, Gwendall PETIT
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ *
+ * or contact directly:
+ * info _at_ orbisgis.org
+ */
+package org.orbisgis.core.ui.plugins.sql;
+
+import org.orbisgis.core.ui.pluginSystem.AbstractPlugIn;
+import org.orbisgis.core.ui.pluginSystem.PlugInContext;
+
+/**
+ *
+ * @author ebocher
+ */
+public class SQLFunctionsPlugIn extends AbstractPlugIn {
+
+        @Override
+        public void initialize(PlugInContext context) throws Exception {
+                context.getFeatureInstaller().addRegisterFunction(MapContext_BBox.class);
+                context.getFeatureInstaller().addRegisterFunction(MapContext_AddLayer.class);
+        }
+
+        @Override
+        public boolean execute(PlugInContext context) throws Exception {
+                return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+                return true;
+        }
+}
