689dcda20e1adceee7f2599348a76f31b44495b9
agueganno <a9emen@gmail.com>
2011-11-29 14:12:47 +0000

    removing useless artifacts in orbisgis-se

--- gdms/src/test/resources/removeAllTypes.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE "alltypes";
-delete from geometry_columns where f_table_name='alltypes';
--- gdms/src/test/resources/simpleScript.sql
+++
@@ -1,3 +0,0 @@
-select register('/home/ebocher/Documents/data/BD_Topo_Nantes_shp/BATI_surface.shp','landcover');
-select register('/tmp/test.shp','landcoverBuffer');
-create table landcoverBuffer as select ST_Buffer(the_geom, 20) as the_geom from landcover where ST_area(the_geom) > 9000;
