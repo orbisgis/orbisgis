88eadbe784f130ceae065b559720d34997fb84f6
Antoine Gourlay <antoine@gourlay.fr>
2011-12-06 13:46:14 +0000

    fix #581 Removed re-use of instances of WKB/WKT Readers/Writers.  * a GeometryFactory is kept (it is thread-safe).  * new Readers/Writers are created upon every request (after looking inside the classes, their construction does not seem to be a heavy enough operation to necessitate using a pool or a ThreadLocal).

--- gdms/src/main/java/org/gdms/data/values/WKBUtil.java
+++ gdms/src/main/java/org/gdms/data/values/WKBUtil.java
@@ -38,62 +38,38 @@
  */
 package org.gdms.data.values;
 
+import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.io.WKBReader;
 import com.vividsolutions.jts.io.WKBWriter;
 import com.vividsolutions.jts.io.WKTReader;
 import com.vividsolutions.jts.io.WKTWriter;
 
 final class WKBUtil {
-
-	private static volatile WKBWriter writer3D = null;
-	private static volatile WKBWriter writer2D = null;
-
-	private static volatile WKTWriter textWriter3D = null;
-	private static volatile WKTWriter textWriter2D = null;
-
-	private static volatile WKBReader wkbReader = null;
-	private static volatile WKTReader wktReader = null;
+        
+        private static final GeometryFactory GEOMFACT = new GeometryFactory();
 
 	static WKBReader getWKBReaderInstance() {
-		if (wkbReader == null) {
-			wkbReader = new WKBReader();
-		}
-		return wkbReader;
+		return new WKBReader(GEOMFACT);
 	}
 
 	static WKBWriter getWKBWriter2DInstance() {
-		if (writer2D == null) {
-			writer2D = new WKBWriter();
-		}
-		return writer2D;
+		return new WKBWriter();
 	}
 
 	static WKBWriter getWKBWriter3DInstance() {
-		if (writer3D == null) {
-			writer3D = new WKBWriter(3);
-		}
-		return writer3D;
+		return new WKBWriter(3);
 	}
 
 	static WKTReader getWKTReaderInstance() {
-		if (wktReader == null) {
-			wktReader = new WKTReader();
-		}
-		return wktReader;
+		return new WKTReader(GEOMFACT);
 	}
 
 	static WKTWriter getTextWKTWriter2DInstance() {
-		if (textWriter2D == null) {
-			textWriter2D = new WKTWriter();
-		}
-		return textWriter2D;
+		return new WKTWriter();
 	}
 
 	static WKTWriter getTextWKTWriter3DInstance() {
-		if (textWriter3D == null) {
-			textWriter3D = new WKTWriter(3);
-		}
-		return textWriter3D;
+		return new WKTWriter(3);
 	}
 
         private WKBUtil() {
