4c7df7bab2c84c7234b6e8faf7270189da0cff18
agueganno <a9emen@gmail.com>
2011-12-06 15:28:30 +0000

    add a new sample file, that contains a recode instance, and a test to check that we are able to read AND write SE files that contain Recode instances.

--- orbisgis-core/src/test/java/org/orbisgis/core/renderer/se/parameter/color/Recode2ColorTest.java
+++ orbisgis-core/src/test/java/org/orbisgis/core/renderer/se/parameter/color/Recode2ColorTest.java
@@ -1,8 +1,21 @@
 package org.orbisgis.core.renderer.se.parameter.color;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import junit.framework.TestCase;
+import net.opengis.se._2_0.core.LineSymbolizerType;
+import net.opengis.se._2_0.core.PenStrokeType;
+import net.opengis.se._2_0.core.RecodeType;
+import net.opengis.se._2_0.core.RuleType;
+import net.opengis.se._2_0.core.SolidFillType;
+import net.opengis.se._2_0.core.StyleType;
+import org.orbisgis.core.renderer.se.Style;
 import org.orbisgis.core.renderer.se.parameter.ParameterException;
 import org.orbisgis.core.renderer.se.parameter.string.StringLiteral;
 import org.orbisgis.core.renderer.se.parameter.string.StringParameter;
@@ -12,6 +25,24 @@ import org.orbisgis.core.renderer.se.parameter.string.StringParameter;
  * @author maxence
  */
 public class Recode2ColorTest extends TestCase {
+
+    protected Recode2Color recode;
+
+    String key1;
+    String key2;
+    String key3;
+    String key4;
+    String key5;
+
+    ColorParameter c1;
+    ColorLiteral c2;
+    ColorLiteral c3;
+    ColorLiteral c4;
+    ColorLiteral c5;
+
+    ColorLiteral fb;
+
+    StringParameter lookup;
     
     public Recode2ColorTest(String testName) {
         super(testName);
@@ -99,23 +130,25 @@ public class Recode2ColorTest extends TestCase {
         assertTrue(recode.getNumMapItem() == 0);
     }
 
-    protected Recode2Color recode;
-
-    String key1;
-    String key2;
-    String key3;
-    String key4;
-    String key5;
-
-    ColorParameter c1;
-    ColorLiteral c2;
-    ColorLiteral c3;
-    ColorLiteral c4;
-    ColorLiteral c5;
- 
-    ColorLiteral fb;
-
-    StringParameter lookup;
-
+    public void testMarshalUnmarshal() throws Exception {
+            String xml = "src/test/resources/org/orbisgis/core/renderer/se/colorRecode.se";
+                JAXBContext jaxbContext = JAXBContext.newInstance(StyleType.class);
+                Unmarshaller u = jaxbContext.createUnmarshaller();
+                JAXBElement<StyleType> ftsElem = (JAXBElement<StyleType>) u.unmarshal(
+                        new FileInputStream(xml));
+                Marshaller m = jaxbContext.createMarshaller();
+                m.marshal(ftsElem, new FileOutputStream("target/c2routput.se"));
+                Style st = new Style(ftsElem, null);
+                JAXBElement<StyleType> elem = st.getJAXBElement();
+                m.marshal(elem, new FileOutputStream("target/c2routput.se"));
+                //Let's search in the created JAXBElement
+                StyleType stype = elem.getValue();
+                RuleType rt = stype.getRule().get(0);
+                LineSymbolizerType lst = ((LineSymbolizerType)(rt.getSymbolizer().getValue()));
+                SolidFillType sf = ((SolidFillType)((PenStrokeType)lst.getStroke().getValue()).getFill().getValue());
+                JAXBElement je = (JAXBElement) (sf.getColor().getContent().get(0));
+                RecodeType rect = ((RecodeType)(je.getValue()));
+                assertTrue(rect.getMapItem().size() == 4);
+    }
 
 }
