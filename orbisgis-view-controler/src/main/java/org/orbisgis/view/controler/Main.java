05414f8b4be4a1abf195c2693f4a813216e0ecc1
nicolas-f <nicolas-f@irstv>
2012-01-30 14:11:00 +0000

    Add view controler package, entry point of OrbisGis UI

---
+++ orbisgis-view-controler/src/main/java/org/orbisgis/view/controler/Main.java
@@ -0,0 +1,13 @@
+package org.orbisgis.view.controler;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
