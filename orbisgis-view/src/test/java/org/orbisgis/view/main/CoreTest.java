d2fbe3fa175fd28998ae3afa2cc49e7a1dfb9d5f
nicolas-f <nicolas-f@irstv>
2012-02-03 13:00:14 +0000

    Add unit test of ui

---
+++ orbisgis-view/src/test/java/org/orbisgis/view/main/CoreTest.java
@@ -0,0 +1,67 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information. OrbisGIS is
+ * distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ * 
+ *
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ *
+ * or contact directly:
+ * info _at_ orbisgis.org
+ */
+package org.orbisgis.view.main;
+
+import java.util.Collection;
+import junit.framework.TestCase;
+import org.junit.Before;
+/**
+ * Unit Test of org.orbisgis.view.main.Core
+ * @author fortin
+ */
+public class CoreTest extends TestCase {
+    Core instance;
+    public CoreTest(String testName) {
+        super(testName);
+        
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        instance = new Core();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        instance.shutdown();
+    }
+
+    /**
+     * Test of startup method, of class Core.
+     */
+    @Before
+    public void testStartup() {
+        System.out.println("startup");
+        assertTrue(instance.getMainFrame()==null);
+        instance.startup();
+        assertTrue(instance.getMainFrame()!=null);
+    }
+}
