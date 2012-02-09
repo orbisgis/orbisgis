50fb5846b40f1e4b2079ce663d7f583bea6140fd
ebocher <ebocher@irstv>
2012-02-09 12:25:41 +0000

    Just add a Thread.sleep() to display the wonderfull result

--- orbisgis-core/src/test/java/org/orbisgis/core/beanshell/BeanShellScriptTest.java
+++ orbisgis-core/src/test/java/org/orbisgis/core/beanshell/BeanShellScriptTest.java
@@ -2,30 +2,26 @@ package org.orbisgis.core.beanshell;
 
 import org.junit.Test;
 
-
 /**
  *
  * @author ebocher
  */
+public class BeanShellScriptTest {
 
-
-public class BeanShellScriptTest{
-        
         @Test
         public void testNullFileScript() throws Exception {
-               BeanshellScript.main(new String[]{"-f", ""});
+                BeanshellScript.main(new String[]{"-f", ""});
         }
-        
+
         @Test
         public void testSimpleFileScript() throws Exception {
-                
-               BeanshellScript.main(new String[]{"-f", "src/test/resources/beanshell/helloWorld.bsh"});
+
+                BeanshellScript.main(new String[]{"-f", "src/test/resources/beanshell/helloWorld.bsh"});
         }
-        
+
         @Test
-        public void testMapDisplayScript() throws Exception {                
-               BeanshellScript.main(new String[]{"-f", "src/test/resources/beanshell/mapDisplayDatasource.bsh"});
+        public void testMapDisplayScript() throws Exception {
+                BeanshellScript.main(new String[]{"-f", "src/test/resources/beanshell/mapDisplayDatasource.bsh"});
+                Thread.sleep(3000);
         }
-        
-        
 }
