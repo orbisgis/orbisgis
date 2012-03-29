84eab39460e830e37cff3a93af8a397b9e506daf
Antoine Gourlay <antoine@gourlay.fr>
2012-03-29 13:41:17 +0000

    improved automatic packaging of orbisgis: added a binary zip package (with launchers & empty 'lib/ext') and a source zip package with the whole maven project (including pom.xml, excluding 'target/')

---
+++ orbisgis-ui/src/bin/zip/orbisgis.sh
@@ -0,0 +1,1 @@
+java -Xmx1024M -jar orbisgis-ui-4.0-SNAPSHOT-jar-with-dependencies.jar $*
---
+++ orbisgis-ui/src/bin/zip/orbishell.sh
@@ -0,0 +1,1 @@
+java -Xmx1024M -cp "orbisgis-ui-4.0-SNAPSHOT-jar-with-dependencies.jar" org.orbisgis.core.beanshell.BeanshellScript $*
