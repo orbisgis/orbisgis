59538bbe792d14cc1826369b659c6d0a3fdee117
agueganno <a9emen@gmail.com>
2012-03-29 16:05:06 +0000

    As I'll use this class again in order to manage rules and their instances, it is not deprecated anymore.
    Comment temporarily some checks that are made to set or not the
    checkbox. As rules are not managed yet, it can't be added now.
    
    Add Javadoc, too, as the role of this class is clear.

--- orbisgis-ui/src/main/java/org/orbisgis/core/ui/editorViews/toc/actions/cui/LegendListRenderer.java
+++ orbisgis-ui/src/main/java/org/orbisgis/core/ui/editorViews/toc/actions/cui/LegendListRenderer.java
@@ -5,7 +5,12 @@ import java.awt.Component;
 import java.awt.Rectangle;
 import javax.swing.*;
 
-@Deprecated
+/**
+ * As a {@code CellRenderer}, this class is used in {@code LegendList} to render
+ * the elements of the list of {@code Legend}. Each time a change occur in the
+ * list organization, it is called to perform the rendering.
+ * @author alexis
+ */
 public class LegendListRenderer implements ListCellRenderer {
 
 	private LegendListRenderPanel ourJPanel = null;
@@ -15,6 +20,11 @@ public class LegendListRenderer implements ListCellRenderer {
 
 	private static final Color SELECTED = Color.red;
 
+        /**
+         * Build a new {@code ListCellRenderer} dedicated to the rendering of
+         * {@code Legend} elements and affiliated.
+         * @param legendsPanel
+         */
 	public LegendListRenderer(LegendsPanel legendsPanel) {
 		this.legendsPanel = legendsPanel;
 	}
@@ -29,11 +39,21 @@ public class LegendListRenderer implements ListCellRenderer {
 
 	}
 
+        /**
+         * The {@code CellRenderer} is used by its owning {@code LegendList} to
+         * produce instances of {@code Component} each time it's needed. This
+         * goal is achieved by {@code LegendListRenderer} by using this inner
+         * class.
+         */
 	public class LegendListRenderPanel extends JPanel {
 
 		private JCheckBox jCheckBox;
 		private JLabel label;
 
+                /**
+                 * Prepare the {@code Component} that will be returned to the
+                 * {@code LegendList}.
+                 */
 		public LegendListRenderPanel() {
 
 			jCheckBox = new JCheckBox();
@@ -46,15 +66,24 @@ public class LegendListRenderer implements ListCellRenderer {
 
 		}
 
+                /**
+                 * Configure the {@code Component} that will be returned to the
+                 * {@code LegendList}.
+                 * @param list
+                 * @param value
+                 * @param legendIndex
+                 * @param isSelected
+                 * @param cellHasFocus
+                 */
 		public void setNodeCosmetic(JList list, Object value, int legendIndex,
 				boolean isSelected, boolean cellHasFocus) {
 			jCheckBox.setVisible(true);
 
-			if (legendsPanel.getLegends()[legendIndex].isVisible()) {
+//			if (legendsPanel.getLegends()[legendIndex].isVisible()) {
 				jCheckBox.setSelected(true);
-			} else {
-				jCheckBox.setSelected(false);
-			}
+//			} else {
+//				jCheckBox.setSelected(false);
+//			}
 
 			if (isSelected) {
 				label.setForeground(SELECTED);
@@ -74,10 +103,12 @@ public class LegendListRenderer implements ListCellRenderer {
 		}
 	}
 
+        /**
+         * Get the bounds of the last produced {@code Component}.
+         * @return
+         */
 	public Rectangle getCheckBoxBounds() {
-
 		return ourJPanel.getCheckBoxBounds();
-
 	}
 
 }
