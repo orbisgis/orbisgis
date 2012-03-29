a8dfa205e2a43c6dfd2474414b4e285fdd153f44
lpang <lpang@irstv>
2012-03-29 16:08:24 +0000

    Set WMSClient instanciation in WMSDriver.

--- gdms/src/main/java/org/gdms/driver/stream/SimpleWMSDriver.java
+++ gdms/src/main/java/org/gdms/driver/stream/SimpleWMSDriver.java
@@ -2,6 +2,7 @@ package org.gdms.driver.stream;
 
 import com.vividsolutions.jts.geom.Envelope;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.util.ArrayList;
@@ -25,6 +26,7 @@ import org.gdms.driver.StreamReadWriteDriver;
 import org.gdms.driver.driverManager.DriverManager;
 import org.gdms.source.SourceManager;
 import org.gvsig.remoteClient.utils.BoundaryBox;
+import org.gvsig.remoteClient.wms.ICancellable;
 import org.gvsig.remoteClient.wms.WMSClient;
 import org.gvsig.remoteClient.wms.WMSStatus;
 //import org.orbisgis.core.layerModel.WMSClientPool;
@@ -33,6 +35,9 @@ import org.orbisgis.progress.ProgressMonitor;
 /**
  *
  * @author Vincent Dépériers
+ */
+/** The driver who gets the information about a flux WMS(host,srs,format,),
+ *  it can provide an acces to the GDMS, and return a value about the WMSclient to the layer.
  */
 public final class SimpleWMSDriver extends AbstractDataSet implements StreamReadWriteDriver {
 
@@ -57,18 +62,33 @@ public final class SimpleWMSDriver extends AbstractDataSet implements StreamRead
         this.m_Schema = new DefaultSchema(DRIVER_NAME + this.hashCode());
         this.m_Schema.addTable(DriverManager.DEFAULT_SINGLE_TABLE_NAME, metadata);
     }
-
+    /**
+     * Open the WMS driver.
+     * @param streamSource
+     * @throws DriverException 
+     */
     @Override
     public void open(StreamSource streamSource) throws DriverException {
         this.m_StreamSource = streamSource;
 
     }
-
+    
+    /**
+     * Close the flux source.
+     * @throws DriverException 
+     */
     @Override
     public void close() throws DriverException {
         this.m_StreamSource = null;
     }
 
+    /**
+     * Gets the value(host,src,layer name,format) in each field.
+     * @param rowIndex
+     * @param fieldId
+     * @return
+     * @throws DriverException 
+     */
     @Override
     public Value getFieldValue(long rowIndex, int fieldId) throws DriverException {
         //Pour le moment mais c'est a changer xD
@@ -88,6 +108,13 @@ public final class SimpleWMSDriver extends AbstractDataSet implements StreamRead
         return null;
     }
 
+    /**
+     * With the host of the flux wms, creates a WMS client.
+     * @return
+     * @throws ConnectException
+     * @throws IOException 
+     */
+    // c'est pas très claire cette méthode, il y a des choses dont on n'a pas besoin...
     public WMSClient getWMSClient() throws ConnectException, IOException {
 
         String host = m_StreamSource.getHost(); //$NON-NLS-1$
@@ -111,12 +138,26 @@ public final class SimpleWMSDriver extends AbstractDataSet implements StreamRead
         return m_WMSClient;
     }
     
+    /**
+     * Gets the envelope of the flux.
+     * @return 
+     */
     public Envelope getEnvelope() {
         return this.m_Envelope;
     }
     
+    /**
+     * Gets the status of the flux.
+     * @return 
+     */
     public WMSStatus getWMSStatus(){
         return this.m_WMSStatus;
+    }
+    
+    public File getMap(int width, int height,ICancellable cancel){
+        m_WMSStatus.setHeight(height);
+        m_WMSStatus.setExtent(new Rectangle2D.Double(m_Envelope.getMinX(), m_Envelope.getMinY(), m_Envelope.getWidth(), m_Envelope.getHeight()));
+        throw new RuntimeException("Compiled Code");
     }
 
     private org.gvsig.remoteClient.wms.WMSLayer find(String layerName, org.gvsig.remoteClient.wms.WMSLayer layer) {
@@ -136,6 +177,14 @@ public final class SimpleWMSDriver extends AbstractDataSet implements StreamRead
         return null;
     }
 
+    /**
+     * Gets the boundary box of the layer with the srs and the layer name.
+     * @param layerName
+     * @param layer
+     * @param srs
+     * @return 
+     */
+    // cette méthode est plutôt setLayerBoundingBox?
     private BoundaryBox getLayerBoundingBox(String layerName, org.gvsig.remoteClient.wms.WMSLayer layer, String srs) {
         org.gvsig.remoteClient.wms.WMSLayer wmsLayer = find(layerName, layer);
         // Obtain the bbox at current level
@@ -167,6 +216,11 @@ public final class SimpleWMSDriver extends AbstractDataSet implements StreamRead
         return null;
     }
 
+    /**
+     * Gets the metadata stored in the schema of the driver.
+     * @return
+     * @throws DriverException 
+     */
     @Override
     public Metadata getMetadata() throws DriverException {
         return this.m_Schema.getTableByName(DriverManager.DEFAULT_SINGLE_TABLE_NAME);
@@ -177,26 +231,49 @@ public final class SimpleWMSDriver extends AbstractDataSet implements StreamRead
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * Sets the adresse URL of the flux.
+     * @param url
+     * @throws DriverException 
+     */
     @Override
     public void setURL(String url) throws DriverException {
         this.m_url = url;
     }
 
+    /**
+     * Checks if the driver is currently open.
+     * @return 
+     */
     @Override
     public boolean isOpen() {
         return true;
     }
 
+    /**
+     * Gets the prefix of the driver, returns "wms" for a wms driver.
+     * @return 
+     */
     @Override
     public String[] getPrefixes() {
         return new String[]{"wms"};
     }
 
+    /**
+     * Gets the schema of the flux.
+     * @return
+     * @throws DriverException 
+     */
     @Override
     public Schema getSchema() throws DriverException {
         return this.m_Schema;
     }
 
+    /**
+     * Gets the DataSet  of the flux.
+     * @param name
+     * @return 
+     */
     @Override
     public DataSet getTable(String name) {
         if (!name.equals(DriverManager.DEFAULT_SINGLE_TABLE_NAME)) {
@@ -205,41 +282,73 @@ public final class SimpleWMSDriver extends AbstractDataSet implements StreamRead
         return this;
     }
 
+    /**
+     * Gets the data source factory of this flux.
+     * @param dsf 
+     */
     @Override
     public void setDataSourceFactory(DataSourceFactory dsf) {
         this.m_DataSourceFactory = dsf;
     }
 
+    /**
+     * Gets the type of flux that can be supported by the driver. For now, WMS driver can support Raster and Memory.
+     * @return 
+     */
     @Override
     public int getSupportedType() {
         return SourceManager.MEMORY | SourceManager.RASTER;
     }
 
+    /**
+     * Gets the type of the sources that this driver can read, for a WMS driver, it can read the flux WMS.
+     * @return 
+     */
     @Override
     public int getType() {
         return SourceManager.WMS;
     }
 
+    /**
+    * Returns a short representation of the source types that this driver accesses.
+    * @return 
+    */
     @Override
     public String getTypeName() {
         return "Stream WMS";
     }
 
+    /**
+     * Gets the definitions of the actual types that this driver can handle.
+     * @return 
+     */
     @Override
     public String getTypeDescription() {
         return "Simple WMS Stream Driver";
     }
 
+    /**
+     * Gets a description of the driver.
+     * @return 
+     */
     @Override
     public String getDriverId() {
         return this.DRIVER_NAME;
     }
 
+    /**
+     * Returns true if the driver can write contents to the source.
+     * @return 
+     */
     @Override
     public boolean isCommitable() {
         return this.m_Commitable;
     }
 
+    /**
+     * Sets if the driver can or can not wirte contents to the source.
+     * @param commitable 
+     */
     public void setCommitable(boolean commitable) {
         this.m_Commitable = commitable;
     }
@@ -249,6 +358,12 @@ public final class SimpleWMSDriver extends AbstractDataSet implements StreamRead
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * Checks if the metadata can be stored in this driver.
+     * @param metadata
+     * @return
+     * @throws DriverException 
+     */
     @Override
     public String validateMetadata(Metadata metadata) throws DriverException {
         if (metadata.getFieldCount() != 1) {
