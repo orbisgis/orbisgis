cd93c3c65bffbf716da386b02393eee6c14efdb0
nicolas-f <nicolas-f@irstv>
2012-04-04 13:36:33 +0000

    Move mnemonic locale common function to Sif

---
+++ orbisgis-sif/src/main/java/org/orbisgis/sif/common/menuCommonFunctions.java
@@ -0,0 +1,60 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information. OrbisGIS is
+ * distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ * 
+ *
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ *
+ * or contact directly:
+ * info _at_ orbisgis.org
+ */
+package org.orbisgis.sif.common;
+
+import javax.swing.JMenuItem;
+
+/**
+ * Common tools to Swing Menu
+ */
+public class menuCommonFunctions {
+    /**
+     * Static class
+     */
+    private menuCommonFunctions() {
+        
+    }
+    
+        /**
+     * Translate the characted following & in the label by a mnemonic
+     * @param menuItem The menu item
+     */
+    public static void setMnemonic(JMenuItem menuItem) {
+        String menuLabel = menuItem.getText();
+        if(menuLabel.contains("&")) {
+            int charpos=menuLabel.indexOf("&");
+            if(menuLabel.length()>charpos+1) {
+                if(!menuLabel.substring(charpos+1,charpos+2).equals("&")) {
+                    menuItem.setMnemonic(menuLabel.substring(charpos+1,charpos+2).toCharArray()[0]);
+                }
+                menuItem.setText(menuLabel.replaceFirst("&",""));
+            }
+        }
+    }
+}
