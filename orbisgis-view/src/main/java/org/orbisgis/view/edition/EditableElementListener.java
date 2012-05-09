dae92e31d2b760b05f2146d4a00a5fd03e107c88
nicolas-f <nicolas-f@irstv>
2012-05-09 06:35:55 +0000

    Copy Map Editor editable element base

---
+++ orbisgis-view/src/main/java/org/orbisgis/view/edition/EditableElementListener.java
@@ -0,0 +1,54 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information. OrbisGIS is
+ * distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ * 
+ *
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ *
+ * or contact directly:
+ * info _at_ orbisgis.org
+ */
+package org.orbisgis.view.edition;
+
+public interface EditableElementListener {
+
+	/**
+	 * Called when the id of the element has changed
+	 * 
+	 * @param element
+	 */
+	void idChanged(EditableElement element);
+
+	/**
+	 * Called when the contents of the element has been edited
+	 * 
+	 * @param element
+	 */
+	void contentChanged(EditableElement element);
+
+	/**
+	 * Called when the element has been saved
+	 * 
+	 * @param element
+	 */
+	void saved(EditableElement element);
+
+}
