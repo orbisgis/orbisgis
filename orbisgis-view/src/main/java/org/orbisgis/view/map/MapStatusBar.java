7716388d3f5e6421ded288809c6b947ae1d84ae6
nicolas-f <github@nettrader.fr>
2012-07-13 11:01:22 +0200

    Fix error in the scale validation

--- orbisgis-view/src/main/java/org/orbisgis/view/map/MapStatusBar.java
+++ orbisgis-view/src/main/java/org/orbisgis/view/map/MapStatusBar.java
@@ -221,7 +221,6 @@ public class MapStatusBar extends JPanel {
                                                         setUserDefinedScaleDenominator(ft.parse(scaleParts[1]).longValue());
                                                         LOGGER.debug("User scale input accepted..");
                                                 } else {
-                                                        invalidateUserInput();
                                                         throw new ParseException(I18N.tr("The only accepted value on nominator is 1"), 0);
                                                 }
                                         } catch( ParseException ex) {
