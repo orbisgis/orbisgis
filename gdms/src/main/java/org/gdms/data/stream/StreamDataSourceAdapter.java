eb55ef453a8a4b05996957e2e602132ca14f48cd
Antoine Gourlay <antoine@gourlay.fr>
2012-07-20 09:13:28 +0200

    formatting and update for DataSource -> DataSet.

--- gdms/src/main/java/org/gdms/data/stream/StreamDataSourceAdapter.java
+++ gdms/src/main/java/org/gdms/data/stream/StreamDataSourceAdapter.java
@@ -1,7 +1,43 @@
+/**
+ * The GDMS library (Generic Datasource Management System)
+ * is a middleware dedicated to the management of various kinds of
+ * data-sources such as spatial vectorial data or alphanumeric. Based
+ * on the JTS library and conform to the OGC simple feature access
+ * specifications, it provides a complete and robust API to manipulate
+ * in a SQL way remote DBMS (PostgreSQL, H2...) or flat files (.shp,
+ * .csv...).
+ *
+ * Gdms is distributed under GPL 3 license. It is produced by the "Atelier SIG"
+ * team of the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ *
+ * Copyright (C) 2007-2012 IRSTV FR CNRS 2488
+ *
+ * This file is part of Gdms.
+ *
+ * Gdms is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Gdms is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Gdms. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ *
+ * or contact directly:
+ * info@orbisgis.org
+ */
 package org.gdms.data.stream;
 
 import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.orbisgis.progress.NullProgressMonitor;
+
 import org.gdms.data.DataSource;
 import org.gdms.data.DriverDataSource;
 import org.gdms.data.edition.Commiter;
@@ -12,7 +48,6 @@ import org.gdms.driver.*;
 import org.gdms.source.CommitListener;
 import org.gdms.source.DefaultSourceManager;
 import org.gdms.source.Source;
-import org.orbisgis.progress.NullProgressMonitor;
 
 /**
  * Adapter to the DataSource interface for stream drivers
@@ -60,11 +95,9 @@ public class StreamDataSourceAdapter extends DriverDataSource implements Commite
     }
 
     @Override
-    public void saveData(DataSource ds) throws DriverException {
+    public void saveData(DataSet ds) throws DriverException {
         LOG.trace("Saving Data");
-        ds.open();
         ((StreamReadWriteDriver) driver).write(ds, new NullProgressMonitor());
-        ds.close();
     }
 
     @Override
