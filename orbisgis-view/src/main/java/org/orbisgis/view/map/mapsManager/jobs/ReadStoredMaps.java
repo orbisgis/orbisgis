fcdd5503fb3bfbc0adcc3a5442ee66b969bef6c7
nicolas-f <github@nettrader.fr>
2012-08-31 17:04:22 +0200

    Make some nodes

---
+++ orbisgis-view/src/main/java/org/orbisgis/view/map/mapsManager/jobs/ReadStoredMaps.java
@@ -0,0 +1,53 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information. 
+ * 
+ * OrbisGIS is distributed under GPL 3 license. It is produced by the "Atelier SIG"
+ * team of the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ * 
+ * Copyright (C) 2007-2012 IRSTV (FR CNRS 2488)
+ * 
+ * This file is part of OrbisGIS.
+ * 
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * For more information, please consult: <http://www.orbisgis.org/>
+ * or contact directly:
+ * info_at_ orbisgis.org
+ */
+package org.orbisgis.view.map.mapsManager.jobs;
+
+import org.orbisgis.progress.ProgressMonitor;
+import org.orbisgis.view.background.BackgroundJob;
+import org.xnap.commons.i18n.I18n;
+import org.xnap.commons.i18n.I18nFactory;
+
+/**
+ * Update the tree node to replace the filename by the good title and description
+ * @author Nicolas Fortin
+ */
+public class ReadStoredMaps implements BackgroundJob {
+        private static final I18n I18N = I18nFactory.getI18n(ReadStoredMaps.class);
+        
+        @Override
+        public void run(ProgressMonitor pm) {
+                //
+        }
+
+        @Override
+        public String getTaskName() {
+                return I18N.tr("Parse maps title and description");
+        }
+        
+}
