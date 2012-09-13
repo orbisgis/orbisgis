02421fe91d1f49621ebc53e0ffd9a1b5881226c7
unknown <julien.senn@si-ein-d24-jse.ing.intra.eiaj.ch>
2012-09-13 12:13:18 +0200

    Modification Speed up open Choropleth

--- orbisgis-view/src/main/java/org/orbisgis/view/toc/actions/cui/freqChart/chartListener/FreqChartListener.java
+++ orbisgis-view/src/main/java/org/orbisgis/view/toc/actions/cui/freqChart/chartListener/FreqChartListener.java
@@ -1,5 +1,6 @@
 package org.orbisgis.view.toc.actions.cui.freqChart.chartListener;
 
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
@@ -9,7 +10,13 @@ import java.util.List;
 import org.jfree.chart.ChartMouseEvent;
 import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.ui.RectangleEdge;
+import org.orbisgis.core.Services;
+import org.orbisgis.progress.ProgressMonitor;
+import org.orbisgis.view.background.BackgroundJob;
+import org.orbisgis.view.background.BackgroundManager;
 import org.orbisgis.view.toc.actions.cui.freqChart.dataModel.FreqChartDataModel;
 import org.orbisgis.view.toc.actions.cui.freqChart.render.FreqChartRender;
 
@@ -20,7 +27,7 @@ import org.orbisgis.view.toc.actions.cui.freqChart.render.FreqChartRender;
 public class FreqChartListener implements ChartMouseListener,
         MouseMotionListener {
 
-     /** The frequence chart panel */
+    /** The frequence chart panel */
     private ChartPanel chartPanel;
     /** The frequence chart data model */
     private FreqChartDataModel freqChartDataModel;
@@ -28,6 +35,8 @@ public class FreqChartListener implements ChartMouseListener,
     private FreqChartRender freqChartRender;
     /** The old selected threshold */
     private int selectThresholdOld;
+    /** The mouse screen position */
+    private Point screen;
 
     /**
      * FreqChartListener constructor
@@ -44,54 +53,81 @@ public class FreqChartListener implements ChartMouseListener,
 
     @Override
     public void chartMouseClicked(ChartMouseEvent event) {
-        Point2D p = chartPanel.translateScreenToJava2D(event.getTrigger().getPoint());
-        XYPlot plot = (XYPlot) chartPanel.getChart().getPlot();
-        Rectangle2D plotArea = chartPanel.getScreenDataArea();
-        double chartX = plot.getDomainAxis().java2DToValue(p.getX(), plotArea,
-                plot.getDomainAxisEdge());
-        double chartY = plot.getRangeAxis().java2DToValue(p.getY(), plotArea,
-                plot.getRangeAxisEdge());
 
-        Point2D point = new Point2D.Double(chartX, chartY);
+        screen = event.getTrigger().getPoint();
 
-        List<List<Double>> threshold = freqChartDataModel.getThresholdList();
+        BackgroundManager bm = Services.getService(BackgroundManager.class);
+        bm.backgroundOperation(new BackgroundJob() {
 
-        double thresholdMax;
-        int selectThreshold = 0;
-        boolean found = false;
+            @Override
+            public void run(ProgressMonitor pm) {
+                Point2D p = chartPanel.translateScreenToJava2D(screen);
+                XYPlot plot = (XYPlot) chartPanel.getChart().getPlot();
+                Rectangle2D plotArea = chartPanel.getScreenDataArea();
+                double chartX = plot.getDomainAxis().java2DToValue(p.getX(), plotArea,
+                        plot.getDomainAxisEdge());
+                double chartY = plot.getRangeAxis().java2DToValue(p.getY(), plotArea,
+                        plot.getRangeAxisEdge());
 
-        for (int i = 1; i < threshold.size(); i++) {
-            thresholdMax = threshold.get(i - 1).get(1);
-            if (thresholdMax <= chartX + 1 && thresholdMax >= chartX - 1) {
-                selectThreshold = i;
-                found = true;
+                Point2D point = new Point2D.Double(chartX, chartY);
+
+                List<List<Double>> threshold = freqChartDataModel.getThresholdList();
+
+                double thresholdMax;
+                int selectThreshold = 0;
+                boolean found = false;
+
+                for (int i = 1; i < threshold.size(); i++) {
+                    thresholdMax = threshold.get(i - 1).get(1);
+                    if (thresholdMax <= chartX + 1 && thresholdMax >= chartX - 1) {
+                        selectThreshold = i;
+                        found = true;
+                    }
+                }
+                if (!found) {
+                    selectThreshold = 0;
+                }
+                if (found) {
+                    freqChartDataModel.fireEvent(new AxisChanged(this,
+                            AxisChangedType.RANGEPRESSED, selectThreshold, point));
+                }
+                freqChartDataModel.fireEvent(new AxisChanged(this,
+                        AxisChangedType.CHARTPRESSED, selectThreshold, point));
             }
-        }
-        if (!found) {
-            selectThreshold = 0;
-        }
-        if (found) {
-            freqChartDataModel.fireEvent(new AxisChanged(this,
-                    AxisChangedType.RANGEPRESSED, selectThreshold, point));
-        }
-        freqChartDataModel.fireEvent(new AxisChanged(this,
-                AxisChangedType.CHARTPRESSED, selectThreshold, point));
+
+            @Override
+            public String getTaskName() {
+                return "ClicMouseEvent";
+            }
+        });
     }
 
     @Override
     public void mouseDragged(MouseEvent event) {
+        screen = event.getPoint();
+        BackgroundManager bm = Services.getService(BackgroundManager.class);
+        bm.backgroundOperation(new BackgroundJob() {
 
-        Point2D p = chartPanel.translateScreenToJava2D(event.getPoint());
-        XYPlot plot = (XYPlot) chartPanel.getChart().getPlot();
-        Rectangle2D plotArea = chartPanel.getScreenDataArea();
-        double chartX = plot.getDomainAxis().java2DToValue(p.getX(), plotArea,
-                plot.getDomainAxisEdge());
-        double chartY = plot.getRangeAxis().java2DToValue(p.getY(), plotArea,
-                plot.getRangeAxisEdge());
+            @Override
+            public void run(ProgressMonitor pm) {
+                Point2D p = chartPanel.translateScreenToJava2D(screen);
+                XYPlot plot = (XYPlot) chartPanel.getChart().getPlot();
+                Rectangle2D plotArea = chartPanel.getScreenDataArea();
+                double chartX = plot.getDomainAxis().java2DToValue(p.getX(), plotArea,
+                        plot.getDomainAxisEdge());
+                double chartY = plot.getRangeAxis().java2DToValue(p.getY(), plotArea,
+                        plot.getRangeAxisEdge());
 
-        Point2D point = new Point2D.Double(chartX, chartY);
-        freqChartDataModel.fireEvent(new AxisChanged(this,
-                AxisChangedType.CHARTDRAG, selectThresholdOld, point));
+                Point2D point = new Point2D.Double(chartX, chartY);
+                freqChartDataModel.fireEvent(new AxisChanged(this,
+                        AxisChangedType.CHARTDRAG, selectThresholdOld, point));
+            }
+
+            @Override
+            public String getTaskName() {
+                return "DragMouseEvent";
+            }
+        });
     }
 
     @Override
@@ -100,53 +136,64 @@ public class FreqChartListener implements ChartMouseListener,
 
     @Override
     public void chartMouseMoved(ChartMouseEvent event) {
-        Point2D p = chartPanel.translateScreenToJava2D(event.getTrigger().getPoint());
-        XYPlot plot = (XYPlot) chartPanel.getChart().getPlot();
-        Rectangle2D plotArea = chartPanel.getScreenDataArea();
-        double chartX = plot.getDomainAxis().java2DToValue(p.getX(), plotArea,
-                plot.getDomainAxisEdge());
-        double chartXMin = plot.getDomainAxis().java2DToValue(
-                p.getX() - freqChartDataModel.getPixRangeDelta(), plotArea,
-                plot.getDomainAxisEdge());
-        double chartXMax = plot.getDomainAxis().java2DToValue(
-                p.getX() + freqChartDataModel.getPixRangeDelta(), plotArea,
-                plot.getDomainAxisEdge());
-        double chartY = plot.getRangeAxis().java2DToValue(p.getY(), plotArea,
-                plot.getRangeAxisEdge());
 
-        Point2D point = new Point2D.Double(chartX, chartY);
+        screen = event.getTrigger().getPoint();
+        BackgroundManager bm = Services.getService(BackgroundManager.class);
+        bm.backgroundOperation(new BackgroundJob() {
 
-        List<List<Double>> threshold = freqChartDataModel.getThresholdList();
+            @Override
+            public void run(ProgressMonitor pm) {
+                Point2D p = chartPanel.translateScreenToJava2D(screen);
+                XYPlot plot = (XYPlot) chartPanel.getChart().getPlot();
+                Rectangle2D plotArea = chartPanel.getScreenDataArea();
+                ValueAxis domAxis = plot.getDomainAxis();
+                RectangleEdge domAxisEdge = plot.getDomainAxisEdge();
+                int pixRange = freqChartDataModel.getPixRangeDelta();
+                double chartX = domAxis.java2DToValue(p.getX(), plotArea, domAxisEdge);
+                double chartXMin = domAxis.java2DToValue(p.getX() - pixRange, plotArea, domAxisEdge);
+                double chartXMax = domAxis.java2DToValue(p.getX() + pixRange, plotArea, domAxisEdge);
+                double chartY = plot.getRangeAxis().java2DToValue(p.getY(), plotArea, plot.getRangeAxisEdge());
 
-        double thresholdMax;
-        int selectThreshold = 0;
-        boolean found = false;
+                Point2D point = new Point2D.Double(chartX, chartY);
 
-        for (int i = 1; i < threshold.size(); i++) {
-            thresholdMax = threshold.get(i - 1).get(1);
-            if (thresholdMax <= chartXMax && thresholdMax >= chartXMin) {
-                selectThreshold = i;
-                found = true;
-            }
-        }
-        if (!found) {
-            selectThreshold = 0;
-        }
+                List<List<Double>> threshold = freqChartDataModel.getThresholdList();
 
-        freqChartRender.setSelectedThreshold(selectThreshold);
-        if (selectThresholdOld != selectThreshold) {
-            freqChartRender.drawAxes(plot, freqChartDataModel);
-            if (found) {
+                double thresholdMax;
+                int selectThreshold = 0;
+                boolean found = false;
+
+                for (int i = 1; i < threshold.size(); i++) {
+                    thresholdMax = threshold.get(i - 1).get(1);
+                    if (thresholdMax <= chartXMax && thresholdMax >= chartXMin) {
+                        selectThreshold = i;
+                        found = true;
+                    }
+                }
+                if (!found) {
+                    selectThreshold = 0;
+                }
+
+                freqChartRender.setSelectedThreshold(selectThreshold);
+                if (selectThresholdOld != selectThreshold) {
+                    freqChartRender.drawAxes(plot, freqChartDataModel);
+                    if (found) {
+                        freqChartDataModel.fireEvent(new AxisChanged(this,
+                                AxisChangedType.RANGESUP, selectThreshold, point));
+                    } else {
+                        freqChartDataModel.fireEvent(new AxisChanged(this,
+                                AxisChangedType.RANGESDOWN, selectThresholdOld, point));
+                    }
+                }
                 freqChartDataModel.fireEvent(new AxisChanged(this,
-                        AxisChangedType.RANGESUP, selectThreshold, point));
-            } else {
-                freqChartDataModel.fireEvent(new AxisChanged(this,
-                        AxisChangedType.RANGESDOWN, selectThresholdOld, point));
+                        AxisChangedType.CHARTMOVE, selectThreshold, point));
+                selectThresholdOld = selectThreshold;
             }
-        }
-        freqChartDataModel.fireEvent(new AxisChanged(this,
-                AxisChangedType.CHARTMOVE, selectThreshold, point));
-        selectThresholdOld = selectThreshold;
+
+            @Override
+            public String getTaskName() {
+                return "MoveMouseEvent";
+            }
+        });
     }
 
     /**
