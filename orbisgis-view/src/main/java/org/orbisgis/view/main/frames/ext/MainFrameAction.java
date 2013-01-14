00e8b7fc4c39f9f1b0c7a67e2a9216d6230e09f7
nicolas-f <github@nettrader.fr>
2013-01-14 11:06:29 +0100

    remove useless osgi package declaration, already exported by felix framework. Add package version declaration.

--- orbisgis-view/src/main/java/org/orbisgis/view/main/frames/ext/MainFrameAction.java
+++ orbisgis-view/src/main/java/org/orbisgis/view/main/frames/ext/MainFrameAction.java
@@ -3,6 +3,8 @@ package org.orbisgis.view.main.frames.ext;
 import org.orbisgis.view.components.actions.ActionFactoryService;
 
 /**
+ * Implement this interface to define additional menu items.
+ * If this interface is modified, increment the package version trough package-info.java .
  * @author Nicolas Fortin
  */
 public interface MainFrameAction  extends ActionFactoryService<MainWindow> {
@@ -11,6 +13,4 @@ public interface MainFrameAction  extends ActionFactoryService<MainWindow> {
     public static final String MENU_EXIT = "exitapp";
     public static final String MENU_TOOLS = "tools";
     public static final String MENU_CONFIGURE = "configure";
-    public static final String MENU_LOOKANDFEEL = "lookAndFeel";
-    public static final String MENU_WINDOWS = "windows";
 }
---
+++ orbisgis-view/src/main/java/org/orbisgis/view/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Class related to Graphical User Interface.
+ * @since 4.0
+ */
+package org.orbisgis.view;
