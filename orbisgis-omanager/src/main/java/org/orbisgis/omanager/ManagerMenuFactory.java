267f5362cf5d4913262530b72a5e62e3cc7a22b1
nicolas-f <github@nettrader.fr>
2013-01-15 14:12:46 +0100

    getPanel() method in main window extension point. omanager panel list.

--- orbisgis-omanager/src/main/java/org/orbisgis/omanager/ManagerMenuFactory.java
+++ orbisgis-omanager/src/main/java/org/orbisgis/omanager/ManagerMenuFactory.java
@@ -1,14 +1,18 @@
 package org.orbisgis.omanager;
 
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.beans.EventHandler;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.Action;
 import javax.swing.ImageIcon;
+import org.apache.log4j.Logger;
+import org.orbisgis.omanager.ui.MainPanel;
 import org.orbisgis.view.components.actions.DefaultAction;
 import org.orbisgis.view.main.frames.ext.MainFrameAction;
 import org.orbisgis.view.main.frames.ext.MainWindow;
+import org.osgi.framework.BundleContext;
 import org.xnap.commons.i18n.I18n;
 import org.xnap.commons.i18n.I18nFactory;
 
@@ -16,16 +20,26 @@ import org.xnap.commons.i18n.I18nFactory;
  * @author Nicolas Fortin
  */
 public class ManagerMenuFactory implements MainFrameAction {
-    public static final String A_MANAGE_PLUGINS = "A_MANAGE_PLUGINS";
+    private static final Logger LOGGER = Logger.getLogger("gui."+ManagerMenuFactory.class);
+    public static final String MENU_MANAGE_PLUGINS = "A_MANAGE_PLUGINS";
     private static final I18n I18N = I18nFactory.getI18n(ManagerMenuFactory.class);
-
-
+    private BundleContext bundleContext;
+    private MainPanel mainPanel;
+    private MainWindow target; // There is only one main window in the application, the it can be stored here.
+    /**
+     * @param bundleContext Used to track OSGi bundle repository service, and to manage bundles.
+     */
+    public ManagerMenuFactory(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+        LOGGER.info("Init manager");
+    }
 
     public List<Action> createActions(MainWindow target) {
+        this.target = target;
         List<Action> actions = new ArrayList<Action>();
-        actions.add(new DefaultAction(A_MANAGE_PLUGINS,I18N.tr("&Manage plug-ins"),
+        actions.add(new DefaultAction(MENU_MANAGE_PLUGINS,I18N.tr("&Manage plug-ins"),
                 new ImageIcon(ManagerMenuFactory.class.getResource("panel_icon.png")),
-                EventHandler.create(ActionListener.class,this,"showManager")).setParent(MENU_TOOLS));
+                EventHandler.create(ActionListener.class,this,"showManager")).setParent(MENU_TOOLS).setInsertFirst(true));
         return actions;
     }
 
@@ -33,10 +47,18 @@ public class ManagerMenuFactory implements MainFrameAction {
      * Make and show the plug-ins manager
      */
     public void showManager() {
-
+        LOGGER.info("Show manager..");
+        if(mainPanel==null) {
+            mainPanel = new MainPanel(target.getMainFrame());
+        }
+        mainPanel.setModal(false);
+        mainPanel.setVisible(true);
     }
 
     public void disposeActions(MainWindow target, List<Action> actions) {
-
+        // Close the Dialog if created
+        if(mainPanel != null) {
+            mainPanel.dispose();
+        }
     }
 }
--- orbisgis-omanager/src/main/java/org/orbisgis/omanager/ui/MainPanel.java
+++ orbisgis-omanager/src/main/java/org/orbisgis/omanager/ui/MainPanel.java
@@ -1,9 +1,40 @@
 package org.orbisgis.omanager.ui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Frame;
+import javax.swing.BorderFactory;
+import javax.swing.JDialog;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import org.orbisgis.view.components.list.PanelList;
 
 /**
+ * Dialog that handle bundles.
  * @author Nicolas Fortin
  */
-public class MainPanel extends JPanel {
+public class MainPanel extends JDialog {
+    private static final Dimension DEFAULT_DIMENSION = new Dimension(640,480);
+    private static final Dimension DEFAULT_DETAILS_DIMENSION = new Dimension(200,-1);
+    // Bundle Category filter
+    private JList bundleCategory = new JList(new String[] {"All","DataBase","Network","SQL Functions"});
+    private JTextArea bundleDetails = new JTextArea();
+    // PanelList can be replaced by a standard JList.
+    private PanelList bundleList = new PanelList();
+
+    public MainPanel(Frame frame) {
+        super(frame);
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
+        // Put components
+        JPanel leftOfSplitGroup = new JPanel(new BorderLayout());
+        bundleCategory.setBorder(BorderFactory.createEtchedBorder());
+        leftOfSplitGroup.add(bundleCategory,BorderLayout.WEST);
+        leftOfSplitGroup.add(bundleList,BorderLayout.CENTER);
+        bundleDetails.setPreferredSize(DEFAULT_DETAILS_DIMENSION);
+        JSplitPane contentPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,leftOfSplitGroup,bundleDetails);
+        setContentPane(contentPane);
+        setSize(DEFAULT_DIMENSION);
+    }
 }
