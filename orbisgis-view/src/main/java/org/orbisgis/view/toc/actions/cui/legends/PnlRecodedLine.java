ddf5b34a405972241534378f2e63a8e069f0085e
agueganno <alexis.gueganno@ec-nantes.fr>
2013-03-05 11:27:44 +0100

    First version of the panel for unique value lines. It's jsut a configuration viewer for now : it does not listen to any event so it is not possible to edit the configuration from there. It comes with a dedicated TableModel and a dedicated CellRenderer for the previews embedded in the table.

---
+++ orbisgis-view/src/main/java/org/orbisgis/view/toc/actions/cui/legends/PnlRecodedLine.java
@@ -0,0 +1,264 @@
+/*
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information.
+ *
+ * OrbisGIS is distributed under GPL 3 license. It is produced by the "Atelier SIG"
+ * team of the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ *
+ * Copyright (C) 2007-2012 IRSTV (FR CNRS 2488)
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ * or contact directly:
+ * info_at_ orbisgis.org
+ */
+package org.orbisgis.view.toc.actions.cui.legends;
+
+import org.apache.log4j.Logger;
+import org.gdms.data.DataSource;
+import org.orbisgis.core.layerModel.ILayer;
+import org.orbisgis.legend.Legend;
+import org.orbisgis.legend.thematic.LineParameters;
+import org.orbisgis.legend.thematic.constant.UniqueSymbolLine;
+import org.orbisgis.legend.thematic.recode.RecodedLine;
+import org.orbisgis.view.toc.actions.cui.LegendContext;
+import org.orbisgis.view.toc.actions.cui.SimpleGeometryType;
+import org.orbisgis.view.toc.actions.cui.components.CanvasSE;
+import org.orbisgis.view.toc.actions.cui.legend.ILegendPanel;
+import org.orbisgis.view.toc.actions.cui.legend.ISELegendPanel;
+import org.orbisgis.view.toc.actions.cui.legends.model.PreviewCellRenderer;
+import org.orbisgis.view.toc.actions.cui.legends.model.TableModelRecodedLine;
+import org.xnap.commons.i18n.I18n;
+import org.xnap.commons.i18n.I18nFactory;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.beans.EventHandler;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This panel must be used to manage all the parameters of a line symbolizer
+ * which is configured thanks to a "simple" recoded PenStroke. All the parameters
+ * of the PenStroke must be configured either with a Recode or a Literal, all
+ * the Recode must be done with the same analysis field.
+ *
+ * @author Alexis Guéganno
+ */
+public class PnlRecodedLine extends AbstractFieldPanel implements ILegendPanel {
+    private static final Logger LOGGER = Logger.getLogger("gui." + PnlProportionalPointSE.class);
+    private static I18n I18N = I18nFactory.getI18n(PnlRecodedLine.class);
+    private String id;
+    private RecodedLine legend;
+    private DataSource ds;
+
+    @Override
+    public Component getComponent() {
+        initializeLegendFields();
+        return this;
+    }
+
+    @Override
+    public void initialize(LegendContext lc) {
+        if (legend == null) {
+            setLegend(new RecodedLine());
+        }
+        setGeometryType(lc.getGeometryType());
+        ILayer layer = lc.getLayer();
+        if (layer != null && layer.getDataSource() != null) {
+            ds = layer.getDataSource();
+        }
+    }
+
+    @Override
+    public ISELegendPanel newInstance() {
+        return new PnlRecodedLine();
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(String newId) {
+        id = newId;
+    }
+
+    @Override
+    public String validateInput() {
+        return "";
+    }
+
+    /**
+     * Creates and fill the combo box that will be used to compute the
+     * analysis.
+     *
+     * @return  A ComboBox linked to the underlying MappedLegend that configures the analysis field.
+     */
+    private JComboBox getFieldComboBox() {
+        if (ds != null) {
+            JComboBox jcc = getFieldCombo(ds);
+            ActionListener acl2 = EventHandler.create(ActionListener.class,
+                        this, "updateField", "source.selectedItem");
+            String field = legend.getLookupFieldName();
+            if (field != null && !field.isEmpty()) {
+                jcc.setSelectedItem(field);
+            }
+            jcc.addActionListener(acl2);
+            updateField((String) jcc.getSelectedItem());
+            return jcc;
+        } else {
+            return new JComboBox();
+        }
+    }
+
+    /**
+     * Build the panel used to select the classification field.
+     *
+     * @return
+     */
+    private JPanel getFieldLine() {
+        JPanel jp = new JPanel();
+        jp.add(new JLabel(I18N.tr("Classification field : ")));
+        jp.add(getFieldComboBox());
+        return jp;
+    }
+
+    /**
+     * Get a preview for the fallback configuration
+     * @return The preview as a CanvasSE.
+     */
+    private JPanel getPreview() {
+        UniqueSymbolLine usl = new UniqueSymbolLine(legend.getFallbackParameters());
+        CanvasSE cse = new CanvasSE(usl.getSymbolizer());
+        return cse;
+    }
+
+    /**
+     * Builds the panel used to display and configure the fallback symbol
+     *
+     * @return
+     */
+    private JPanel getFallback() {
+        JPanel jp = new JPanel();
+        jp.add(new JLabel(I18N.tr("Fallback Symbol")));
+        jp.add(getPreview());
+        return jp;
+    }
+
+    private JPanel getTablePanel() {
+        JPanel jp = new JPanel();
+        jp.setBorder(BorderFactory.createTitledBorder(I18N.tr("Unique value classification")));
+        //we build the table here
+//        TableColumnModel cm = new DefaultTableColumnModel();
+        JTable table = new JTable(new TableModelRecodedLine(legend));
+        table.setRowHeight(CanvasSE.HEIGHT);
+        final int previewWidth = CanvasSE.WIDTH;
+        TableColumn previews = table.getColumnModel().getColumn(TableModelRecodedLine.PREVIEW_COLUMN);
+        previews.setWidth(previewWidth);
+        previews.setMinWidth(previewWidth);
+        previews.setMaxWidth(previewWidth);
+        previews.setCellRenderer(new PreviewCellRenderer(table, String.class, legend));
+        JScrollPane jsp = new JScrollPane(table);
+        jsp.setMaximumSize(new Dimension(200,200));
+        table.setMaximumSize(new Dimension(200,200));
+        table.setPreferredScrollableViewportSize(new Dimension(400,200));
+        jp.add(jsp, BorderLayout.CENTER);
+        table.doLayout();
+        return jp;
+    }
+
+    private void initializeLegendFields() {
+        this.removeAll();
+        JPanel glob = new JPanel();
+        GridBagLayout grid = new GridBagLayout();
+        glob.setLayout(grid);
+        //Field chooser
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        glob.add(getFieldLine(), gbc);
+        //Fallback symbol
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        glob.add(getFallback(), gbc);
+        //Table for the recoded configurations
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        glob.add(getTablePanel(), gbc);
+        this.add(glob);
+    }
+
+    /**
+     * Used when the field against which the analysis is made changes.
+     *
+     * @param obj
+     */
+    public void updateField(String obj) {
+        legend.setLookupFieldName(obj);
+    }
+
+    @Override
+    public Legend getLegend() {
+        return legend;
+    }
+
+    @Override
+    public void setLegend(Legend legend) {
+        if (legend instanceof RecodedLine) {
+            this.legend = (RecodedLine) legend;
+            this.initializeLegendFields();
+        } else {
+            throw new IllegalArgumentException(I18N.tr("You must use recognized RecodedLine instances in"
+                        + "this panel."));
+        }
+    }
+
+    @Override
+    public void setGeometryType(int type) {
+    }
+
+    @Override
+    public boolean acceptsGeometryType(int geometryType) {
+                return geometryType == SimpleGeometryType.LINE ||
+                        geometryType == SimpleGeometryType.POLYGON||
+                        geometryType == SimpleGeometryType.ALL;
+    }
+
+    @Override
+    public Legend copyLegend() {
+        RecodedLine rl = new RecodedLine();
+        Set<Map.Entry<String,LineParameters>> entries = legend.entrySet();
+        for(Map.Entry<String,LineParameters> entry : entries){
+            rl.put(entry.getKey(),entry.getValue());
+        }
+        rl.setFallbackParameters(legend.getFallbackParameters());
+        rl.setLookupFieldName(legend.getLookupFieldName());
+        return rl;
+    }
+}
---
+++ orbisgis-view/src/main/java/org/orbisgis/view/toc/actions/cui/legends/model/PreviewCellRenderer.java
@@ -0,0 +1,71 @@
+/**
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at French IRSTV institute and is able to
+ * manipulate and create vector and raster spatial information.
+ *
+ * OrbisGIS is distributed under GPL 3 license. It is produced by the "Atelier SIG"
+ * team of the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ *
+ * Copyright (C) 2007-2012 IRSTV (FR CNRS 2488)
+ *
+ * This file is part of OrbisGIS.
+ *
+ * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ * or contact directly:
+ * info_at_ orbisgis.org
+ */
+package org.orbisgis.view.toc.actions.cui.legends.model;
+
+import org.orbisgis.legend.thematic.map.MappedLegend;
+import org.orbisgis.view.components.renderers.TableLaFCellRenderer;
+import org.orbisgis.view.toc.actions.cui.components.CanvasSE;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author alexis
+ */
+public class PreviewCellRenderer extends TableLaFCellRenderer {
+    private final MappedLegend symbolizer;
+
+    /**
+     * Set listener to L&F events
+     * {@link javax.swing.JTable#getDefaultRenderer}
+     *
+     * @param table Where the listener has to be installed
+     * @param type  Default cell renderer for this columnClass
+     */
+    public PreviewCellRenderer(JTable table, Class<?> type, MappedLegend sym) {
+        super(table, type);
+        symbolizer = sym;
+    }
+
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        if(value instanceof String){
+            CanvasSE cse = new CanvasSE(symbolizer.getSymbolizer());
+            Map<String,Object> map = new HashMap<String,Object>();
+            map.put(symbolizer.getLookupFieldName(), value);
+            cse.setSampleDatasource(map);
+            return cse;
+        } else {
+            return lookAndFeelRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        }
+    }
+}
