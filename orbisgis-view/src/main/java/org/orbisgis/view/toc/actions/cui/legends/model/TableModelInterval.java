5ec184d2744707e8a63eac3fb7d21c2bc9a693c0
Alexis Guéganno <alexis.gueganno@ec-nantes.fr>
2013-06-26 17:19:23 +0200

    Adds a label in the table of the interval classification UIs

--- orbisgis-view/src/main/java/org/orbisgis/view/toc/actions/cui/legends/model/TableModelInterval.java
+++ orbisgis-view/src/main/java/org/orbisgis/view/toc/actions/cui/legends/model/TableModelInterval.java
@@ -14,8 +14,52 @@ import java.util.SortedSet;
  * @author Alexis Guéganno
  */
 public class TableModelInterval<U extends LineParameters> extends AbstractLegendTableModel<Double,U> {
+    public static final int INTERVAL_COLUMN = 2;
+    private static final int TABLE_COLUMN_COUNT = 3;
     private AbstractCategorizedLegend<U> cat;
     private final static I18n I18N = I18nFactory.getI18n(TableModelInterval.class);
+
+    @Override
+    public int getColumnCount() {
+        return TABLE_COLUMN_COUNT;
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex){
+        return columnIndex < 2;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        if(columnIndex == KEY_COLUMN) {
+            return getKeyAt(rowIndex);
+        } else if(columnIndex == PREVIEW_COLUMN){
+            return getPreviewAt(rowIndex);
+        } else if(columnIndex == INTERVAL_COLUMN){
+            SortedSet<Double> ts = getMappedLegend().keySet();
+            Double d = getKeyAt(rowIndex);
+            SortedSet<Double> tail = ts.tailSet(d);
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            if(Double.isInfinite(d)){
+                sb.append("-").append(Character.toString('\u221e')) ;
+            }else {
+                sb.append(d);
+            }
+            sb.append(";");
+            if(tail.size() == 1){
+                sb.append("+").append(Character.toString('\u221e'));
+            } else {
+                Iterator<Double> it = tail.iterator();
+                //next is d, we want the one next to d.
+                it.next();
+                sb.append(+it.next());
+            }
+            sb.append("[");
+            return sb.toString();
+        }
+        throw new IndexOutOfBoundsException("We did not found a column at index "+columnIndex+" !");
+    }
 
     @Override
     public String getColumnName(int col){
@@ -23,6 +67,8 @@ public class TableModelInterval<U extends LineParameters> extends AbstractLegend
             return I18N.tr("Threshold");
         } else if(col == PREVIEW_COLUMN){
             return I18N.tr("Preview");
+        } else if(col == INTERVAL_COLUMN){
+            return I18N.tr("Label");
         }
         throw new IndexOutOfBoundsException("We did not found a column at index "+col+" !");
     }
