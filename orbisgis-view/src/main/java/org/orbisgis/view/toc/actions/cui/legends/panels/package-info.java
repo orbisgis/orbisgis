26d2fd7c2a7744da65f281be28301ae6e2511420
Adam Gouge <adam.gouge@ec-nantes.fr>
2013-08-09 13:46:09 +0200

    Add package descriptions to javadoc

---
+++ orbisgis-view/src/main/java/org/orbisgis/view/toc/actions/cui/legends/panels/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Individual UI panels used in the Simple Style Editor.
+ *
+ * @see org.orbisgis.view.toc.actions.cui.legends.ui
+ */
+package org.orbisgis.view.toc.actions.cui.legends.panels;
