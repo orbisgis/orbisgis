7242e429b890cef26eefc838c2602054aa1cfc3a
nicolas-f <github@nettrader.fr>
2014-03-21 15:23:19 +0100

    Fix package name

--- bundles/core-map/src/test/java/org/orbisgis/coremap/OwsMapContextTest.java
+++ bundles/core-map/src/test/java/org/orbisgis/coremap/OwsMapContextTest.java
@@ -26,7 +26,7 @@
  * or contact directly:
  * info_at_ orbisgis.org
  */
-package org.orbisgis.core;
+package org.orbisgis.coremap;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -42,20 +42,53 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.h2gis.drivers.shp.SHPDriverFunction;
+import org.h2gis.h2spatial.ut.SpatialH2UT;
 import org.h2gis.h2spatialapi.EmptyProgressVisitor;
+import org.h2gis.h2spatialext.CreateSpatialExtension;
 import org.h2gis.utilities.TableLocation;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.orbisgis.corejdbc.DataManager;
+import org.orbisgis.corejdbc.internal.DataManagerImpl;
 import org.orbisgis.coremap.layerModel.ILayer;
 import org.orbisgis.coremap.layerModel.MapContext;
 import org.orbisgis.coremap.layerModel.OwsMapContext;
-import org.orbisgis.core_export.MapImageWriter;
 import org.orbisgis.coremap.renderer.se.common.Description;
 import org.orbisgis.progress.NullProgressMonitor;
+
+import javax.sql.DataSource;
 
 /**
  *
  */
-public class OwsMapContextTest extends AbstractTest  {
+public class OwsMapContextTest  {
+    private static Connection connection;
+    private static DataManager dataManager;
+
+    @BeforeClass
+    public static void tearUpClass() throws Exception {
+        DataSource dataSource = SpatialH2UT.createDataSource(OwsMapContextTest.class.getSimpleName(), false);
+        connection = dataSource.getConnection();
+        CreateSpatialExtension.initSpatialExtension(connection);
+        dataManager = new DataManagerImpl(dataSource);
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        connection.close();
+        dataManager.dispose();
+    }
+
+    private DataManager getDataManager() {
+        return dataManager;
+    }
+
+    private Connection getConnection() {
+        return connection;
+    }
+
+    private MapContext mc = new OwsMapContext(dataManager);
 
         
     @Test
@@ -99,23 +132,6 @@ public class OwsMapContextTest extends AbstractTest  {
         // Test the abstract with the provided locale
         assertTrue(mc2.getDescription().getAbstract(locale).equals(mapAbstract));
         mc2.close(null);
-    }
-
-    /**
-     * Method to export the mapcontext as a file image
-     * @param imagePath
-     * @param format
-     * @throws Exception
-     */
-    private void saveAs(String imagePath, MapImageWriter.Format format) throws Exception {
-        MapContext mc = new OwsMapContext(getDataManager());
-        mc.open(null);
-        ILayer layer = mc.createLayer(URI.create("../src/test/resources/data/landcover2000.shp"));
-        mc.getLayerModel().addLayer(layer);
-        MapImageWriter mapImageWriter = new MapImageWriter(mc.getLayerModel());
-        FileOutputStream out = new FileOutputStream(new File(imagePath));
-        mapImageWriter.setFormat(format);
-        mapImageWriter.write(out, new NullProgressMonitor());
     }
 
     @Test
@@ -172,25 +188,5 @@ public class OwsMapContextTest extends AbstractTest  {
         assertEquals(1, newMapContext.getLayers().length);
         assertTrue(newMapContext.getLayers()[0].getDataUri().toString().contains("table=LANDCOVER2000"));
         assertEquals(TableLocation.parse("LANDCOVER2000").getTable(), TableLocation.parse(newMapContext.getLayers()[0].getTableReference()).getTable());
-    }
-
-    @Test
-    public void exportToPNG() throws Exception {
-        saveAs("mapExportTest.png", MapImageWriter.Format.PNG);
-    }
-
-    @Test
-    public void exportToJEPG() throws Exception {
-        saveAs("mapExportTest.jpg", MapImageWriter.Format.JPEG);
-    }
-
-    @Test
-    public void exportToTIFF() throws Exception {
-        saveAs("mapExportTest.tiff", MapImageWriter.Format.TIFF);
-    }
-
-    @Test
-    public void exportToPDF() throws Exception {
-        saveAs("mapExportTest.pdf", MapImageWriter.Format.PDF);
     }
 }
