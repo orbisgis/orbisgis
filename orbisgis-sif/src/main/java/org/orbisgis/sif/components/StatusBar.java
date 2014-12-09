a1f29936acd6a2fdaf7ae983d2a116a39e54f089
nicolas-f <github@nettrader.fr>
2014-12-09 09:11:50 +0100

    Move statusbar

--- orbisgis-sif/src/main/java/org/orbisgis/sif/components/StatusBar.java
+++ orbisgis-sif/src/main/java/org/orbisgis/sif/components/StatusBar.java
@@ -26,7 +26,7 @@
  * or contact directly:
  * info_at_ orbisgis.org
  */
-package org.orbisgis.view.components.statusbar;
+package org.orbisgis.sif.components;
 
 import java.awt.BorderLayout;
 import javax.swing.BorderFactory;
