6eb7de99810810ccd651e34355f952b99d6a31ea
nicolas-f <github@nettrader.fr>
2014-12-19 15:16:09 +0100

    Move to private package

--- main-frame/src/main/java/org/orbisgis/mainframe/impl/MainFrame.java
+++ main-frame/src/main/java/org/orbisgis/mainframe/impl/MainFrame.java
@@ -26,7 +26,7 @@
  * or contact directly:
  * info_at_ orbisgis.org
  */
-package org.orbisgis.view.main.frames;
+package org.orbisgis.mainframe.impl;
 
 import java.awt.*;
 import java.awt.event.ActionListener;
