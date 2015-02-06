6ea33d6c74e9d03f064d968027404ae6c9610964
nicolas-f <github@nettrader.fr>
2015-02-06 17:03:45 +0100

    Move TreeNodeBusy

--- bundles/map-editor/src/main/java/org/orbisgis/mapeditor/map/mapsManager/TreeNodeWorkspace.java
+++ bundles/map-editor/src/main/java/org/orbisgis/mapeditor/map/mapsManager/TreeNodeWorkspace.java
@@ -40,6 +40,8 @@ import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.TransferHandler.TransferSupport;
 import javax.swing.tree.MutableTreeNode;
+
+import org.orbisgis.sif.components.resourceTree.TreeNodeBusy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.orbisgis.coremap.layerModel.MapContext;
