ac1ce7c557d9a12643a7e227bad77c8bdef41ae0
Palominos Sylvain <sylvain.palominos@ec-nantes.fr>
2015-06-09 10:16:05 +0200

    Fully implements the Output class and its test.

--- bundles/orbistoolbox/src/main/java/org/orbisgis/orbistoolbox/process/Output.java
+++ bundles/orbistoolbox/src/main/java/org/orbisgis/orbistoolbox/process/Output.java
@@ -20,62 +20,112 @@
 package org.orbisgis.orbistoolbox.process;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Outputs are the return values of a process. Outputs have a cardinality of one.
+ * Output elements may be simple (i.e. the output has no sub-outputs attached)
+ * or aggregate (i.e. the output has one or more sub-output elements attached).
+ * A simple output includes a realization of the DataDescription element.
+ * An aggregate output contains one or more sub-outputs.
+ *
+ * For more information : http://docs.opengeospatial.org/is/14-065/14-065.html#30
+ *
  * @author Sylvain PALOMINOS
  */
 
-public class Output
-        extends DescriptionType {
-    private DataDescription
-            dataDescription;
-    private List<Output>
-            output;
+public class Output extends DescriptionType {
+    /** Data type and domain of this output. */
+    private DataDescription dataDescription;
+    /** Nested Output. It is recommended to keep the nesting level as low as possible. */
+    private List<Output> output;
+
+
 
     /**
      * Unique constructor providing the necessary attributes according to the WPS specification.
+     * Sets the data type and domain of this output (DataDescription argument) and sets the output list to null.
+     * The default values of minOccurs and maxOccurs is 1.
      * All the parameters should not be null.
      *
-     * @param title      Not null title of a process, input, output.
-     * @param identifier Not null unambiguous identifier of a process, input, and output.
+     * @param title      Not null title of a process, output, output.
+     * @param identifier Not null unambiguous identifier of a process, output, and output.
+     * @param dataDescription Not null DataDescription of this output.
      * @throws IllegalArgumentException Exception thrown if one of the parameters is null.
      */
-    public Output(String title,
-                  URI identifier)
-            throws
-            IllegalArgumentException {
-        super(title,
-                identifier);
-    }
-
-    public void setDataDescription(DataDescription dataDescription) {
-        if (dataDescription ==
-                null) {
-            return;
+    public Output(String title, URI identifier, DataDescription dataDescription) throws IllegalArgumentException {
+        super(title, identifier);
+        if(dataDescription == null ){
+            throw new IllegalArgumentException("The argument \"dataDescription\" should not be null");
         }
-        output =
-                null;
-        this.dataDescription =
-                dataDescription;
+        this.dataDescription = dataDescription;
+        this.output = null;
     }
 
+    /**
+     * Unique constructor providing the necessary attributes according to the WPS specification.
+     * Sets the output list with the List<Output> argument and sets the DataDescription to null;
+     * The default values of minOccurs and maxOccurs is 1.
+     * All the parameters should not be null or empty.
+     *
+     * @param title      Not null title of a process, output, output.
+     * @param identifier Not null unambiguous identifier of a process, output, and output.
+     * @param outputList Not null DataDescription of this output.
+     * @throws IllegalArgumentException Exception thrown if one of the parameters is null.
+     */
+    public Output(String title, URI identifier, List<Output> outputList) throws IllegalArgumentException {
+        super(title, identifier);
+        if(outputList == null || outputList.isEmpty() || outputList.contains(null)){
+            throw new IllegalArgumentException("The argument \"outputList\" should not be null or empty or " +
+                    "containing a null value");
+        }
+        this.dataDescription = null;
+        this.output = outputList;
+    }
+
+    /**
+     * Sets the DataDescription of this output (the argument should not be null) and set to null the output list.
+     * @param dataDescription New DataDescription.
+     */
+    public void setDataDescription(DataDescription dataDescription) throws IllegalArgumentException {
+        if (dataDescription == null) {
+            throw new IllegalArgumentException("The argument \"dataDescription\" should not be null");
+        }
+        output = null;
+        this.dataDescription = dataDescription;
+    }
+
+    /**
+     * Returns the DataDescription of this output.
+     * @return The DataDescription of this output.
+     */
     public DataDescription getDataDescription() {
         return dataDescription;
     }
 
-    public void setOutput(List<Output> outputList) {
-        if (outputList ==
-                null ||
-                outputList.isEmpty()) {
-            return;
+
+    /**
+     * Sets the output list with the not null/empty list and set the dataDescription to null.
+     * The list should not contain null value.
+     * @param outputList List of nested output.
+     */
+    public void setOutput(List<Output> outputList) throws IllegalArgumentException {
+        if (outputList == null || outputList.isEmpty() || outputList.contains(null)) {
+            throw new IllegalArgumentException("The argument \"outputList\" should not be null or empty or " +
+                    "containing a null value");
         }
-        output =
-                null;
-        this.output =
-                outputList;
+        output = new ArrayList<>();
+        for(Output i : outputList) {
+            this.output.add(i);
+        }
+        this.dataDescription = null;
     }
 
+    /**
+     * Returns the nested output.
+     * @return The nested output.
+     */
     public List<Output> getOutput() {
         return output;
     }
---
+++ bundles/orbistoolbox/src/test/java/org/orbisgis/orbistoolbox/process/OutputTest.java
@@ -0,0 +1,131 @@
+/**
+ * OrbisToolBox is an OrbisGIS plugin dedicated to create and manage processing.
+ *
+ * OrbisToolBox is distributed under GPL 3 license. It is produced by CNRS <http://www.cnrs.fr/> as part of the
+ * MApUCE project, funded by the French Agence Nationale de la Recherche (ANR) under contract ANR-13-VBDU-0004.
+ *
+ * OrbisToolBox is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * OrbisToolBox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for moredetails.
+ *
+ * You should have received a copy of the GNU General Public License along with OrbisToolBox. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/> or contact directly: info_at_orbisgis.org
+ */
+
+package org.orbisgis.orbistoolbox.process;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sylvain PALOMINOS
+ */
+
+public class OutputTest {
+    
+    /**
+     * Tests if the constructor with a null title returns an IllegalArgumentException.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void nullTitleConstructorTest() throws URISyntaxException, MalformedURLException {
+        Format f = new Format("test", new URL("http://orbisgis.org"));
+        f.setDefaultFormat(true);
+        new Output(null, new URI("test"), new RawData(f));
+    }
+
+    /**
+     * Tests if the constructor with a null identifier returns an IllegalArgumentException.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void nullURIConstructorTest() throws MalformedURLException {
+        Format f = new Format("test", new URL("http://orbisgis.org"));
+        f.setDefaultFormat(true);
+        new Output("test", null, new RawData(f));
+    }
+
+    /**
+     * Tests if the constructor with a null dataDescription returns an IllegalArgumentException.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void nullDataDescriptionConstructorTest() throws URISyntaxException {
+        new Output("test", new URI("test"), (List<Output>)null);
+    }
+
+    /**
+     * Tests if the constructor with a null output list returns an IllegalArgumentException.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void nullOutputConstructorTest() throws URISyntaxException {
+        new Output("test", new URI("test"), (DataDescription)null);
+    }
+
+    /**
+     * Tests if the constructor with an empty output list returns an IllegalArgumentException.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void emptyOutputConstructorTest() throws URISyntaxException {
+        List<Output> emptyList = new ArrayList<>();
+        new Output("test", new URI("test"), emptyList);
+    }
+
+    /**
+     * Tests if the constructor with a output list containing a null output returns an IllegalArgumentException.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public final void containingNullOutputConstructorTest() throws URISyntaxException, MalformedURLException {
+        Format f = new Format("test", new URL("http://orbisgis.org"));
+        f.setDefaultFormat(true);
+        Output output = new Output("test", new URI("test"), new RawData(f));
+        List<Output> nullList = new ArrayList<>();
+        nullList.add(output);
+        nullList.add(null);
+        new Output("test", new URI("test"), nullList);
+    }
+
+    /**
+     * Tests if setting the output list sets the data description to null.
+     */
+    @Test()
+    public final void setOutputTest() throws URISyntaxException, MalformedURLException {
+        Format f = new Format("test", new URL("http://orbisgis.org"));
+        f.setDefaultFormat(true);
+        Output output1 = new Output("test", new URI("test"), new RawData(f));
+        Output output2 = new Output("test", new URI("test"), new RawData(f));
+
+        List<Output> list = new ArrayList<>();
+        list.add(output2);
+
+        output1.setOutput(list);
+
+        Assert.assertEquals("The data description should be null.", null, output1.getDataDescription());
+        Assert.assertEquals("The output list is not the same as the one given.", list, output1.getOutput());
+    }
+
+    /**
+     * Tests if setting the data description sets the output list to null.
+     */
+    @Test()
+    public final void setDataDescriptionTest() throws URISyntaxException, MalformedURLException {
+        Format f = new Format("test", new URL("http://orbisgis.org"));
+        f.setDefaultFormat(true);
+        Output output = new Output("test", new URI("test"), new RawData(f));
+
+        DataDescription dataDescription = new RawData(f);
+        output.setDataDescription(dataDescription);
+
+        Assert.assertEquals("The data description should be null.", null, output.getOutput());
+        Assert.assertEquals("The output list is not the same as the one given.", dataDescription, output.getDataDescription());
+    }
+}
