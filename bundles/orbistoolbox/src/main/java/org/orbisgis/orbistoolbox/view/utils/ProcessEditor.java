6f1ca663f0741115d76c2e03c0b4c55958c21478
Palominos Sylvain <sylvain.palominos@gmail.com>
2015-12-17 14:07:56 +0100

    Open the panel of configuration of an input/output on clicking on the text and arrow, Scroll to an attribute configuration UI if it is not fully visible.

--- bundles/orbistoolbox/src/main/java/org/orbisgis/orbistoolbox/view/utils/ProcessEditor.java
+++ bundles/orbistoolbox/src/main/java/org/orbisgis/orbistoolbox/view/utils/ProcessEditor.java
@@ -37,6 +37,7 @@ import javax.swing.*;
 import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
 import java.beans.EventHandler;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -193,6 +194,7 @@ public class ProcessEditor extends JPanel implements EditorDockable, PropertyCha
      */
     private JComponent buildUIConf(){
         JPanel panel = new JPanel(new MigLayout("fill"));
+        JScrollPane scrollPane = new JScrollPane(panel);
         //For each input, display its title, its abstract and gets its UI from the dataUIManager
         for(Input i : pee.getProcess().getInput()){
 
@@ -205,18 +207,22 @@ public class ProcessEditor extends JPanel implements EditorDockable, PropertyCha
                     inputPanel.setBorder(BorderFactory.createTitledBorder(i.getTitle()));
                     JPanel upPanel = new JPanel(new MigLayout());
                     JButton showButton = new JButton(ToolBoxIcon.getIcon("btnright"));
-                    showButton.addActionListener(EventHandler.create(ActionListener.class, this, "onClickButton", "source"));
-                    showButton.putClientProperty("body", comp);
-                    showButton.putClientProperty("parent", inputPanel);
                     showButton.setBorderPainted(false);
                     showButton.setMargin(new Insets(0, 0, 0, 0));
                     showButton.setContentAreaFilled(false);
                     showButton.setOpaque(false);
                     showButton.setFocusable(false);
+                    showButton.putClientProperty("upPanel", upPanel);
+                    showButton.addMouseListener(EventHandler.create(MouseListener.class, this, "onClickButton", "source", "mouseClicked"));
                     upPanel.add(showButton);
                     JLabel inputAbstrac = new JLabel(i.getResume());
                     inputAbstrac.setFont(inputAbstrac.getFont().deriveFont(Font.ITALIC));
                     upPanel.add(inputAbstrac, "wrap");
+                    upPanel.putClientProperty("body", comp);
+                    upPanel.putClientProperty("parent", inputPanel);
+                    upPanel.putClientProperty("button", showButton);
+                    upPanel.putClientProperty("scrollPane", scrollPane);
+                    upPanel.addMouseListener(EventHandler.create(MouseListener.class, this, "onClickHeader", "source", "mouseClicked"));
                     inputPanel.add(upPanel, "growx, span");
                     panel.add(inputPanel, "growx, span");
                 }
@@ -234,18 +240,22 @@ public class ProcessEditor extends JPanel implements EditorDockable, PropertyCha
                     outputPanel.setBorder(BorderFactory.createTitledBorder(o.getTitle()));
                     JPanel upPanel = new JPanel(new MigLayout());
                     JButton showButton = new JButton(ToolBoxIcon.getIcon("btnright"));
-                    showButton.addActionListener(EventHandler.create(ActionListener.class, this, "onClickButton", "source"));
-                    showButton.putClientProperty("body", component);
-                    showButton.putClientProperty("parent", outputPanel);
                     showButton.setBorderPainted(false);
                     showButton.setMargin(new Insets(0, 0, 0, 0));
                     showButton.setContentAreaFilled(false);
                     showButton.setOpaque(false);
                     showButton.setFocusable(false);
+                    showButton.putClientProperty("upPanel", upPanel);
+                    showButton.addMouseListener(EventHandler.create(MouseListener.class, this, "onClickButton", "source", "mouseClicked"));
                     upPanel.add(showButton);
                     JLabel outputAbstrac = new JLabel(o.getResume());
                     outputAbstrac.setFont(outputAbstrac.getFont().deriveFont(Font.ITALIC));
                     upPanel.add(outputAbstrac, "wrap");
+                    upPanel.putClientProperty("body", component);
+                    upPanel.putClientProperty("parent", outputPanel);
+                    upPanel.putClientProperty("button", showButton);
+                    upPanel.putClientProperty("scrollPane", scrollPane);
+                    upPanel.addMouseListener(EventHandler.create(MouseListener.class, this, "onClickHeader", "source", "mouseClicked"));
                     outputPanel.add(upPanel, "growx, span");
                     panel.add(outputPanel, "growx, span");
                 }
@@ -254,15 +264,22 @@ public class ProcessEditor extends JPanel implements EditorDockable, PropertyCha
         JButton runButton = new JButton("Run");
         runButton.addActionListener(EventHandler.create(ActionListener.class, this, "runProcess"));
         panel.add(runButton, "growx, wrap");
-        JScrollPane scrollPane = new JScrollPane(panel);
         scrollPane.getVerticalScrollBar().setUnitIncrement(SCROLLBAR_UNIT_INCREMENT);
+        scrollPane.getHorizontalScrollBar().setUnitIncrement(SCROLLBAR_UNIT_INCREMENT);
         return scrollPane;
     }
 
-    public void onClickButton(Object source){
-        JButton showButton = (JButton)source;
-        JComponent body = (JComponent)showButton.getClientProperty("body");
-        JComponent parent = (JComponent)showButton.getClientProperty("parent");
+    public void onClickButton(Object source) {
+        JButton button = (JButton)source;
+        onClickHeader(button.getClientProperty("upPanel"));
+    }
+
+    public void onClickHeader(Object source){
+        JPanel panel = (JPanel)source;
+        JButton showButton = (JButton)panel.getClientProperty("button");
+        final JComponent body = (JComponent)panel.getClientProperty("body");
+        JComponent parent = (JComponent)panel.getClientProperty("parent");
+        final JScrollPane scrollPane = (JScrollPane)panel.getClientProperty("scrollPane");
         boolean isVisible = body.isVisible();
         if(isVisible) {
             body.setVisible(false);
@@ -273,6 +290,14 @@ public class ProcessEditor extends JPanel implements EditorDockable, PropertyCha
             body.setVisible(true);
             parent.add(body, "growx, span");
             showButton.setIcon(ToolBoxIcon.getIcon("btndown"));
+            //Later scrollDown to the element
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    scrollPane.scrollRectToVisible(body.getBounds());
+                }
+            });
+            scrollPane.scrollRectToVisible(body.getBounds());
         }
         parent.revalidate();
     }
