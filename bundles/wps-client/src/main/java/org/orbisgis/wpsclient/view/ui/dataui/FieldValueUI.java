3fdba979d9340520d38a2ebe3169dbd70e2d463a
Palominos Sylvain <sylvain.palominos@univ-ubs.fr>
2016-03-14 10:10:12 +0100

    On loading the field list, display a layer indicating that the load is in progress.

--- bundles/wps-client/src/main/java/org/orbisgis/wpsclient/view/ui/dataui/FieldValueUI.java
+++ bundles/wps-client/src/main/java/org/orbisgis/wpsclient/view/ui/dataui/FieldValueUI.java
@@ -20,19 +20,26 @@
 package org.orbisgis.wpsclient.view.ui.dataui;
 
 import net.miginfocom.swing.MigLayout;
-import org.orbisgis.corejdbc.ReadRowSet;
+import org.orbisgis.commons.progress.SwingWorkerPM;
 import org.orbisgis.wpsclient.WpsClient;
-import org.orbisgis.wpsclient.view.utils.FieldValueListModel;
 import org.orbisgis.wpsclient.view.utils.ToolBoxIcon;
 import org.orbisgis.wpsservice.model.*;
 
 import javax.swing.*;
+import javax.swing.Timer;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.plaf.LayerUI;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.beans.EventHandler;
+import java.beans.PropertyChangeEvent;
 import java.net.URI;
 import java.util.*;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 /**
  * DataUI implementation for FieldValue.
@@ -47,11 +54,6 @@ public class FieldValueUI implements DataUI{
     /** Size constants **/
     private static final int MAX_JLIST_ROW_COUNT = 10;
     private static final int MIN_JLIST_ROW_COUNT = 1;
-    //Set the minimal cell width to 1 because the JList will be in a MigLayout with a 'growx' attribute that will
-    //make if take the whole parent component width.
-    private static final int MIN_WIDTH_CELL = 1;
-    //TODO dynamically calculate it
-    private static final int MAX_HEIGHT_CELL = 15;
 
     /** Constant used to pass object as client property throw JComponents **/
     private static final String DATA_MAP_PROPERTY = "DATA_MAP_PROPERTY";
@@ -60,6 +62,7 @@ public class FieldValueUI implements DataUI{
     private static final String IS_OPTIONAL_PROPERTY = "IS_OPTIONAL_PROPERTY";
     private static final String INITIAL_DELAY_PROPERTY = "INITIAL_DELAY_PROPERTY";
     private static final String TOOLTIP_TEXT_PROPERTY = "TOOLTIP_TEXT_PROPERTY";
+    private static final String LAYERUI_PROPERTY = "LAYERUI_PROPERTY";
 
     /** WpsClient using the generated UI. */
     private WpsClient wpsClient;
@@ -98,8 +101,6 @@ public class FieldValueUI implements DataUI{
         }
         list.setLayoutOrientation(JList.VERTICAL);
         list.setVisibleRowCount(MIN_JLIST_ROW_COUNT);
-        JScrollPane listScroller = new JScrollPane(list);
-        panel.add(listScroller, "growx, wrap");
         list.putClientProperty(URI_PROPERTY, inputOrOutput.getIdentifier());
         list.putClientProperty(FIELD_VALUE_PROPERTY, fieldValue);
         list.putClientProperty(DATA_MAP_PROPERTY, dataMap);
@@ -108,6 +109,13 @@ public class FieldValueUI implements DataUI{
         list.addMouseListener(EventHandler.create(MouseListener.class, this, "onComboBoxExited", "source", "mouseExited"));
         list.addListSelectionListener(EventHandler.create(ListSelectionListener.class, this, "onListSelection", "source"));
         list.setToolTipText(inputOrOutput.getResume());
+
+        //Adds a WaitLayerUI which will be displayed when the toolbox is loading the data
+        JScrollPane listScroller = new JScrollPane(list);
+        WaitLayerUI layerUI = new WaitLayerUI();
+        JLayer<JComponent> layer = new JLayer<>(listScroller, layerUI);
+        panel.add(layer, "growx, wrap");
+        list.putClientProperty(LAYERUI_PROPERTY, layerUI);
 
         return panel;
     }
@@ -144,83 +152,14 @@ public class FieldValueUI implements DataUI{
      * @param source the source JList.
      */
     public void refreshList(Object source){
-        JList<String> list = (JList<String>)source;
-        FieldValue fieldValue = (FieldValue)list.getClientProperty(FIELD_VALUE_PROPERTY);
-        HashMap<URI, Object> dataMap = (HashMap<URI, Object>)list.getClientProperty(DATA_MAP_PROPERTY);
-        //If the DataField related to the FieldValue has been modified, reload the dataField values
-        if(fieldValue.isDataFieldModified()) {
-            fieldValue.setDataFieldModified(false);
-            String tableName = null;
-            String fieldName = null;
-            if(fieldValue.getDataFieldIdentifier().toString().contains("$")){
-                String[] split = fieldValue.getDataFieldIdentifier().toString().split("\\$");
-                if(split.length == 4) {
-                    tableName = split[1]+"."+split[2];
-                    fieldName = split[3];
-                }
-                else if(split.length == 3){
-                    tableName = split[1];
-                    fieldName = split[2];
-                }
-                else{
-                    return;
-                }
-            }
-            else if (dataMap.get(fieldValue.getDataStoreIdentifier()) != null){
-                tableName = ((URI) dataMap.get(fieldValue.getDataStoreIdentifier())).getSchemeSpecificPart();
-                fieldName = dataMap.get(fieldValue.getDataFieldIdentifier()).toString();
-            }
-            if(tableName != null && fieldName != null) {
-                //First retrieve the good field name with the good case.
-                List<String> fieldList = wpsClient.getWpsService().getTableFieldList(tableName,
-                        new ArrayList<DataType>(), new ArrayList<DataType>());
-                for(String field : fieldList){
-                    if(field.equalsIgnoreCase(fieldName)){
-                        fieldName = field;
-                    }
-                }
-                //Retrieve the rowSet reading the table from the wpsService.
-                ReadRowSet readRowSet = wpsClient.getWpsService().getTableReadRowSet(tableName);
-                if(readRowSet != null) {
-                    //Sets the list with a custom model using the rowSet.
-                    FieldValueListModel model = new FieldValueListModel(readRowSet, fieldName);
-                    list.setModel(model);
-                    list.setFixedCellWidth(MIN_WIDTH_CELL);
-                    list.setFixedCellHeight(MAX_HEIGHT_CELL);
-
-                    if (model.getSize() < MAX_JLIST_ROW_COUNT) {
-                        list.setVisibleRowCount(model.getSize());
-                    } else {
-                        list.setVisibleRowCount(MAX_JLIST_ROW_COUNT);
-                    }
-                }
-            }
+        //Instantiate a worker which will do the list update in a separeted swing thread
+        FieldValueWorker worker = new FieldValueWorker((JList)source);
+        ExecutorService executorService = wpsClient.getExecutorService();
+        if(executorService != null){
+            executorService.execute(worker);
         }
-
-        //If the jList doesn't contains any values, it mean that the DataField hasn't been well selected.
-        //So show a tooltip text to warn the user.
-        if(list.getModel().getSize() == 0) {
-            list.putClientProperty(INITIAL_DELAY_PROPERTY, ToolTipManager.sharedInstance().getInitialDelay());
-            list.putClientProperty(TOOLTIP_TEXT_PROPERTY, list.getToolTipText());
-            ToolTipManager.sharedInstance().setInitialDelay(0);
-            ToolTipManager.sharedInstance().setDismissDelay(2500);
-            String fieldValueStr = fieldValue.getDataFieldIdentifier().toString();
-            if(fieldValueStr.contains("$")){
-                String[] split = fieldValueStr.split("\\$");
-                if(split.length == 3){
-                    fieldValueStr = split[1]+"."+split[2];
-                }
-                else if(split.length == 4){
-                    fieldValueStr = split[1]+"."+split[2]+"."+split[3];
-                }
-                list.setToolTipText("First configure the DataField : " + fieldValueStr);
-            }
-            else {
-                list.setToolTipText("First configure the DataField : " +
-                        fieldValueStr.substring(fieldValueStr.lastIndexOf(":") + 1));
-            }
-            ToolTipManager.sharedInstance().mouseMoved(
-                    new MouseEvent(list,MouseEvent.MOUSE_MOVED,System.currentTimeMillis(),0,0,0,0,false));
+        else{
+            worker.execute();
         }
     }
 
@@ -240,5 +179,215 @@ public class FieldValueUI implements DataUI{
             }
         }
         dataMap.put(uri, listValues.toArray(new String[listValues.size()]));
+    }
+
+    /**
+     * SwingWorker doing the list update in a separated Swing Thread
+     */
+    private class FieldValueWorker extends SwingWorkerPM{
+        private JList<String> list;
+
+        public FieldValueWorker(JList<String> list){
+            this.list = list;
+        }
+
+        @Override
+        protected Object doInBackground() throws Exception {
+            WaitLayerUI layerUI = (WaitLayerUI)list.getClientProperty(LAYERUI_PROPERTY);
+            layerUI.start();
+            FieldValue fieldValue = (FieldValue)list.getClientProperty(FIELD_VALUE_PROPERTY);
+            HashMap<URI, Object> dataMap = (HashMap<URI, Object>)list.getClientProperty(DATA_MAP_PROPERTY);
+            boolean isOptional = (boolean)list.getClientProperty(IS_OPTIONAL_PROPERTY);
+            //If the DataField related to the FieldValue has been modified, reload the dataField values
+            if(fieldValue.isDataFieldModified()) {
+                fieldValue.setDataFieldModified(false);
+                String tableName = null;
+                String fieldName = null;
+                if(fieldValue.getDataFieldIdentifier().toString().contains("$")){
+                    String[] split = fieldValue.getDataFieldIdentifier().toString().split("\\$");
+                    if(split.length == 4) {
+                        tableName = split[1]+"."+split[2];
+                        fieldName = split[3];
+                    }
+                    else if(split.length == 3){
+                        tableName = split[1];
+                        fieldName = split[2];
+                    }
+                    else{
+                        layerUI.stop();
+                        return null;
+                    }
+                }
+                else if (dataMap.get(fieldValue.getDataStoreIdentifier()) != null){
+                    tableName = ((URI) dataMap.get(fieldValue.getDataStoreIdentifier())).getSchemeSpecificPart();
+                    fieldName = dataMap.get(fieldValue.getDataFieldIdentifier()).toString();
+                }
+                if(tableName != null && fieldName != null) {
+                    //First retrieve the good field name with the good case.
+                    List<String> fieldList = wpsClient.getWpsService().getTableFieldList(tableName,
+                            new ArrayList<DataType>(), new ArrayList<DataType>());
+                    for(String field : fieldList){
+                        if(field.equalsIgnoreCase(fieldName)){
+                            fieldName = field;
+                        }
+                    }
+                    //Retrieve the rowSet reading the table from the wpsService.
+                    DefaultListModel<String> model = (DefaultListModel<String>) list.getModel();
+                    model.removeAllElements();
+                    List<String> listFields = wpsClient.getWpsService().getFieldValueList(tableName, fieldName);
+                    Collections.sort(listFields);
+                    for (String field : listFields) {
+                        model.addElement(field);
+                    }
+                    if (listFields.size() < MAX_JLIST_ROW_COUNT) {
+                        list.setVisibleRowCount(listFields.size());
+                    } else {
+                        list.setVisibleRowCount(MAX_JLIST_ROW_COUNT);
+                    }
+                    if (!isOptional && list.getModel().getSize() > 0) {
+                        list.setSelectedIndex(0);
+                    }
+                }
+            }
+
+            //If the jList doesn't contains any values, it mean that the DataField hasn't been well selected.
+            //So show a tooltip text to warn the user.
+            if(list.getModel().getSize() == 0) {
+                list.putClientProperty(INITIAL_DELAY_PROPERTY, ToolTipManager.sharedInstance().getInitialDelay());
+                list.putClientProperty(TOOLTIP_TEXT_PROPERTY, list.getToolTipText());
+                ToolTipManager.sharedInstance().setInitialDelay(0);
+                ToolTipManager.sharedInstance().setDismissDelay(2500);
+                String fieldValueStr = fieldValue.getDataFieldIdentifier().toString();
+                if(fieldValueStr.contains("$")){
+                    String[] split = fieldValueStr.split("\\$");
+                    if(split.length == 3){
+                        fieldValueStr = split[1]+"."+split[2];
+                    }
+                    else if(split.length == 4){
+                        fieldValueStr = split[1]+"."+split[2]+"."+split[3];
+                    }
+                    list.setToolTipText("First configure the DataField : " + fieldValueStr);
+                }
+                else {
+                    list.setToolTipText("First configure the DataField : " +
+                            fieldValueStr.substring(fieldValueStr.lastIndexOf(":") + 1));
+                }
+                ToolTipManager.sharedInstance().mouseMoved(
+                        new MouseEvent(list,MouseEvent.MOUSE_MOVED,System.currentTimeMillis(),0,0,0,0,false));
+            }
+            layerUI.stop();
+            return null;
+        }
+    }
+
+    /**
+     * Wait layer displayed on doing an update on the list.
+     */
+    class WaitLayerUI extends LayerUI<JComponent> implements ActionListener {
+        private boolean mIsRunning;
+        private boolean mIsFadingOut;
+        private Timer mTimer;
+
+        private int mAngle;
+        private int mFadeCount;
+        private int mFadeLimit = 15;
+
+        @Override
+        public void paint(Graphics g, JComponent c) {
+            int w = c.getWidth();
+            int h = c.getHeight();
+
+            // Paint the view.
+            super.paint (g, c);
+
+            if (!mIsRunning) {
+                return;
+            }
+
+            Graphics2D g2 = (Graphics2D)g.create();
+
+            float fade = (float)mFadeCount / (float)mFadeLimit;
+            // Gray it out.
+            Composite urComposite = g2.getComposite();
+            g2.setComposite(AlphaComposite.getInstance(
+                    AlphaComposite.SRC_OVER, .5f * fade));
+            g2.fillRect(0, 0, w, h);
+            g2.setComposite(urComposite);
+
+            // Paint the wait indicator.
+            int s = Math.min(w, h) / 5;
+            int cx = w / 2;
+            int cy = h / 2;
+            g2.setPaint(Color.white);
+            //"Loading source" painting
+            Font font = g2.getFont().deriveFont(Font.PLAIN, s / 3);
+            g2.setFont(font);
+            FontMetrics metrics = g2.getFontMetrics(font);
+            int w1 = metrics.stringWidth("Loading");
+            int w2 = metrics.stringWidth("fields");
+            int h1 = metrics.getHeight();
+            g2.drawString("Loading", cx - w1 / 2, cy - h1 / 2);
+            g2.drawString("source", cx - w2 / 2, cy + h1 / 2);
+            //waiter painting
+            g2.setStroke(new BasicStroke(s / 4, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+
+            g2.rotate(Math.PI * mAngle / 180, cx, cy);
+            for (int i = 1; i < 12; i++) {
+                float scale = (11.0f - (float)i) / 11.0f;
+                g2.drawLine(cx + s, cy, cx + s * 2, cy);
+                g2.rotate(-Math.PI / 6, cx, cy);
+                g2.setComposite(AlphaComposite.getInstance(
+                        AlphaComposite.SRC_OVER, scale * fade));
+            }
+            Toolkit.getDefaultToolkit().sync();
+            g2.dispose();
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            if (mIsRunning) {
+                firePropertyChange("tick", 0, 1);
+                mAngle += 3;
+                if (mAngle >= 360) {
+                    mAngle = 0;
+                }
+                if (mIsFadingOut) {
+                    if (--mFadeCount <= 0) {
+                        mIsRunning = false;
+                        mTimer.stop();
+                    }
+                }
+                else if (mFadeCount < mFadeLimit) {
+                    mFadeCount++;
+                }
+            }
+        }
+
+        public void start() {
+            if (mIsRunning) {
+                return;
+            }
+
+            // Run a thread for animation.
+            mIsRunning = true;
+            mIsFadingOut = false;
+            mFadeCount = 0;
+            int fps = 24;
+            int tick = 1000 / fps;
+            mTimer = new Timer(tick, this);
+            mTimer.start();
+        }
+
+        public void stop() {
+            mIsFadingOut = true;
+        }
+
+        @Override
+        public void applyPropertyChange(PropertyChangeEvent pce, JLayer l) {
+            if ("tick".equals(pce.getPropertyName())) {
+                l.repaint();
+            }
+        }
     }
 }
