1fb31d097c2d15573953fa9f16d6bc6bf3018690
nicolas-f <[omitted]>
2018-05-18 10:28:45 +0200

    wrong reformat

--- bundles/sqlconsole/src/main/java/org/orbisgis/sqlconsole/ui/SQLFunctionsPanel.java
+++ bundles/sqlconsole/src/main/java/org/orbisgis/sqlconsole/ui/SQLFunctionsPanel.java
@@ -2,34 +2,34 @@
  * OrbisGIS is a java GIS application dedicated to research in GIScience.
  * OrbisGIS is developed by the GIS group of the DECIDE team of the
  * Lab-STICC CNRS laboratory, see <http://www.lab-sticc.fr/>.
- * <p>
+ *
  * The GIS group of the DECIDE team is located at :
- * <p>
+ *
  * Laboratoire Lab-STICC – CNRS UMR 6285
  * Equipe DECIDE
  * UNIVERSITÉ DE BRETAGNE-SUD
  * Institut Universitaire de Technologie de Vannes
  * 8, Rue Montaigne - BP 561 56017 Vannes Cedex
- * <p>
+ *
  * OrbisGIS is distributed under GPL 3 license.
- * <p>
+ *
  * Copyright (C) 2007-2014 CNRS (IRSTV FR CNRS 2488)
  * Copyright (C) 2015-2017 CNRS (Lab-STICC UMR CNRS 6285)
- * <p>
+ *
  * This file is part of OrbisGIS.
- * <p>
+ *
  * OrbisGIS is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * <p>
+ *
  * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License along with
  * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
- * <p>
+ *
  * For more information, please consult: <http://www.orbisgis.org/>
  * or contact directly:
  * info_at_ orbisgis.org
