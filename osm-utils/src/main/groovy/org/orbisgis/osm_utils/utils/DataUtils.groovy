a51da2238a8bc180f170aaf7a8ea2ddac69dbc5b
Palominos Sylvain <sylvain.palominos@univ-ubs.fr>
2020-11-18 14:13:00 +0100

    Add headers.

--- osm-utils/src/main/groovy/org/orbisgis/osm_utils/utils/DataUtils.groovy
+++ osm-utils/src/main/groovy/org/orbisgis/osm_utils/utils/DataUtils.groovy
@@ -1,8 +1,43 @@
+/*
+ * Bundle OSM is part of the OrbisGIS platform
+ *
+ * OrbisGIS is a java GIS application dedicated to research in GIScience.
+ * OrbisGIS is developed by the GIS group of the DECIDE team of the
+ * Lab-STICC CNRS laboratory, see <http://www.lab-sticc.fr/>.
+ *
+ * The GIS group of the DECIDE team is located at :
+ *
+ * Laboratoire Lab-STICC – CNRS UMR 6285
+ * Equipe DECIDE
+ * UNIVERSITÉ DE BRETAGNE-SUD
+ * Institut Universitaire de Technologie de Vannes
+ * 8, Rue Montaigne - BP 561 56017 Vannes Cedex
+ *
+ * OSM is distributed under LGPL 3 license.
+ *
+ * Copyright (C) 2019 CNRS (Lab-STICC UMR CNRS 6285)
+ *
+ *
+ * OSM is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OSM is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * OSM. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
+ * or contact directly:
+ * info_at_ orbisgis.org
+ */
 package org.orbisgis.osm_utils.utils
 
 import org.h2gis.api.EmptyProgressVisitor
 import org.h2gis.functions.io.osm.OSMDriverFunction
-import org.h2gis.utilities.JDBCUtilities
 import org.h2gis.utilities.TableLocation
 
 import java.sql.Connection
