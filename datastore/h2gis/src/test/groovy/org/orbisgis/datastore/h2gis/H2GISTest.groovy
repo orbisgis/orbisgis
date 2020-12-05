dd900d1314920a466af67795d5a32fbee558a14c
ebocher <erwan.bocher@univ-ubs.fr>
2020-12-05 17:10:15 +0100

    Add unit tests for H2GIS and POSTGIS datastore Fix dependencies

--- datastore/h2gis/src/test/groovy/org/orbisgis/datastore/h2gis/H2GISTest.groovy
+++ datastore/h2gis/src/test/groovy/org/orbisgis/datastore/h2gis/H2GISTest.groovy
@@ -1,7 +1,10 @@
 package org.orbisgis.datastore.h2gis
 
 import org.apache.commons.dbcp.BasicDataSource
+import org.geotools.data.Query
 import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.assertEquals
 
 /**
  * Test class dedicated to {@link org.orbisgis.datastore.h2gis.H2GIS}
@@ -58,5 +61,68 @@ class H2GISTest {
         assert "jdbc:h2:./target/database2;AUTO_SERVER=TRUE" == bds.url.toString()
         assert 100 == ds.fetchSize
         assert 50 == ds.batchInsertSize
+    }
+
+    @Test
+    void openH2GISFromPath() {
+        def ds = H2GIS.open("./target/db_h2gis")
+        assert ds
+    }
+
+    @Test
+    void createReadFeatureSource() {
+        def ds = H2GIS.open("./target/db_h2gis")
+        assert ds
+        ds.execute("""
+                DROP TABLE IF EXISTS geotable;
+                CREATE TABLE geotable (id int, the_geom geometry(point));
+                INSERT INTO geotable VALUES (1, 'POINT(10 10)'::GEOMETRY), (2, 'POINT(1 1)'::GEOMETRY);
+        """)
+        def fs = ds.getFeatureSource("GEOTABLE")
+        assert fs
+        assert fs.getCount(Query.ALL)==2
+    }
+
+    @Test
+    void eachRowSQL() {
+        def ds = H2GIS.open("./target/db_h2gis")
+        assert ds
+        ds.execute("""
+                DROP TABLE IF EXISTS geotable;
+                CREATE TABLE geotable (id int, the_geom geometry(point));
+                INSERT INTO geotable VALUES (1, 'POINT(10 10)'::GEOMETRY), (2, 'POINT(1 1)'::GEOMETRY);
+        """)
+        def concat = ""
+        ds.eachRow "SELECT THE_GEOM FROM geotable", { row -> concat += "$row.the_geom\n" }
+        assertEquals("POINT (10 10)\nPOINT (1 1)\n", concat)
+    }
+
+    @Test
+    void firstRows() {
+        def ds = H2GIS.open("./target/db_h2gis")
+        assert ds
+        ds.execute("""
+                DROP TABLE IF EXISTS geotable;
+                CREATE TABLE geotable (id int, the_geom geometry(point));
+                INSERT INTO geotable VALUES (1, 'POINT(10 10)'::GEOMETRY), (2, 'POINT(1 1)'::GEOMETRY);
+        """)
+        def concat = ""
+        println ds.firstRow("select count(*) as nb from geotable").nb
+    }
+
+    @Test
+    void queryH2GISMetaData() {
+        def ds = H2GIS.open("./target/db_h2gis")
+        assert ds
+        ds.execute("""
+                DROP TABLE IF EXISTS geotable;
+                CREATE TABLE geotable (id int, the_geom geometry(point));
+                INSERT INTO geotable VALUES (1, 'POINT(10 10)'::GEOMETRY), (2, 'POINT(1 1)'::GEOMETRY);
+        """)
+        def concat = ""
+        ds.rows "SELECT * FROM geotable", { meta ->
+            concat += "${meta.getTableName(1)} $meta.columnCount\n"
+        }
+        assertEquals("GEOTABLE 2\n", concat)
     }
 }
